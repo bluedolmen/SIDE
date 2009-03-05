7efc2b71ce5891f88513152a38390369c19992c8
bch <bch@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-03-05 20:50:23 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@5180 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Workflow/trunk/com.bluexml.side.Workflow.modeler/src/com/bluexml/side/Workflow/modeler/actions/WorkflowEditorActionBarContributor.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * No CopyrightText Defined in the configurator file.
+ ******************************************************************************/
+package com.bluexml.side.Workflow.modeler.actions;
+
+import org.topcased.modeler.actions.ModelerActionBarContributor;
+
+/**
+ * Generated Actions
+ *
+ * @generated
+ */
+public class WorkflowEditorActionBarContributor extends
+		ModelerActionBarContributor {
+	// TODO defined customized actions
+}
