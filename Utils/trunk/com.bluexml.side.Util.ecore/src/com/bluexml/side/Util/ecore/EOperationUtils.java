dd6fdc4e82eb7ac2e4012238c651aa2b431d8efb
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-03-18 10:12:16 +0000

    fix BasicIndexOutOfBoundsException on EOperation compare
    git-svn-id: http://svn.bluexml.com/private/S-IDE@5425 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Utils/trunk/com.bluexml.side.Util.ecore/src/com/bluexml/side/Util/ecore/EOperationUtils.java
+++ Utils/trunk/com.bluexml.side.Util.ecore/src/com/bluexml/side/Util/ecore/EOperationUtils.java
@@ -27,64 +27,77 @@ import org.eclipse.emf.ecore.impl.EClassImpl;
 
 /**
  * @author Constantin Madola
- *
+ * 
  */
 public class EOperationUtils {
 
 	/**
-	 * This method retrun strue if otehrEop is similar to eop
-	 * method is transitive (normally)
-	 * EOperations are similar if 
-	 *  <ol><p>EOperations are similar if </p>
-	 *  <li>They have exactly the same return type</li>
-	 *  <li>They have the same name</li>
-	 *  <li>they have the same number of parameters</li>
-	 *  <li><code>each parameter</code> from an <code>EOperation</code> is of same type or is subType from the same parameter at the same
-	 *  position in the other <code>EOperation</code></li>
-	 *  </ol>
-	 * @param eop     first Eoperation
-	 * @param otherEop other Eoperation
+	 * This method retrun strue if otehrEop is similar to eop method is
+	 * transitive (normally) EOperations are similar if
+	 * <ol>
+	 * <p>
+	 * EOperations are similar if
+	 * </p>
+	 * <li>They have exactly the same return type</li>
+	 * <li>They have the same name</li>
+	 * <li>they have the same number of parameters</li>
+	 * <li><code>each parameter</code> from an <code>EOperation</code> is of
+	 * same type or is subType from the same parameter at the same position in
+	 * the other <code>EOperation</code></li>
+	 * </ol>
+	 * 
+	 * @param eop
+	 *            first Eoperation
+	 * @param otherEop
+	 *            other Eoperation
 	 * @return
 	 */
-	protected static boolean isTheSameEoperation(EOperation eop,EOperation otherEop){
+	protected static boolean isTheSameEoperation(EOperation eop, EOperation otherEop) {
 		boolean result = true;
 		result &= eop.getName().equals(otherEop.getName());
 		result &= eop.getEType().getName().equals(otherEop.getEType().getName());
-		EList<EParameter> eopParamList =eop.getEParameters();
+		EList<EParameter> eopParamList = eop.getEParameters();
 		EList<EParameter> otherEopParamList = otherEop.getEParameters();
 		result &= eopParamList.size() == otherEopParamList.size();
 		int limit = eopParamList.size();
 		int cpt = 0;
 		boolean stop = false;
-		while((cpt < limit) && !stop ){
+		while ((cpt < limit) && !stop) {
 			stop = true;
-			EParameter eopParam = eopParamList.get(cpt);
-			EParameter otherEopParam = otherEopParamList.get(cpt);
-			if(isSubType(eopParam, otherEopParam) || isSubType(otherEopParam,eopParam)){
-				stop = false;
+			// compare parameters number before made advanced compare this to avoid BasicIndexOutOfBoundsException
+			if (eopParamList.size() == otherEopParamList.size()) {
+				EParameter eopParam = eopParamList.get(cpt);
+				EParameter otherEopParam = otherEopParamList.get(cpt);
+				if (isSubType(eopParam, otherEopParam) || isSubType(otherEopParam, eopParam)) {
+					stop = false;
+				}
 			}
 			cpt++;
 		}
 		result &= !stop;
 		return result;
 	}
+
 	/**
-	 * returns true, if second method argument is the same type or a subType of first method argument
+	 * returns true, if second method argument is the same type or a subType of
+	 * first method argument
+	 * 
 	 * @param epaType
 	 * @param epaSubType
 	 * @return
 	 */
-	protected static boolean isSubType(EParameter epaType, EParameter epaSubType ){
+	protected static boolean isSubType(EParameter epaType, EParameter epaSubType) {
 		EClassifier eClassifierSubType = epaSubType.getEType();
-		
+
 		boolean result = epaType.getEType().getName().equals(epaSubType.getEType().getName());
-		
-		// cast in EClassImpl because its impossible to get SuperType from an EClassifier
-		if(eClassifierSubType instanceof EClassImpl && !result){
-			EClassImpl eci = (EClassImpl)eClassifierSubType;
+
+		// cast in EClassImpl because its impossible to get SuperType from an
+		// EClassifier
+		if (eClassifierSubType instanceof EClassImpl && !result) {
+			EClassImpl eci = (EClassImpl) eClassifierSubType;
 			EList<EClass> elistSubTypeSuperType = eci.getEAllSuperTypes();
 			Iterator<EClass> iterListSuperType = elistSubTypeSuperType.iterator();
-			while(iterListSuperType.hasNext() && !result){
+			while (iterListSuperType.hasNext() && !result) {
 				result = iterListSuperType.next().getName().equals(epaType.getEType().getName());
 
 			}
