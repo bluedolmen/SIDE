55a2feacaffd558223e1e9546e0ef89b1d24c97b
bch <bch@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-03-20 14:05:26 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@5497 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Application/trunk/com.bluexml.side.Application.generator/src/com/bluexml/side/application/generator/AbstractGenerator.java
@@ -0,0 +1,36 @@
+package com.bluexml.side.application.generator;
+
+import java.util.Map;
+
+import com.bluexml.side.application.StaticConfigurationParameters;
+
+public abstract class AbstractGenerator implements IGenerator {
+
+	protected String[] generationParameters;
+	protected String[] generatorOptions;
+	protected Map<String,String> configurationParameters;
+	
+	public void initialize(String[] generationParameters,
+			String[] generatorOptions,
+			Map<String, String> configurationParameters) {
+		this.generationParameters = generationParameters;
+		this.generatorOptions = generatorOptions;
+		this.configurationParameters = configurationParameters;
+	}
+
+	protected boolean doLog() {
+		if (configurationParameters != null)
+			return configurationParameters.containsKey(StaticConfigurationParameters.GENERATIONOPTIONSLOG_PATH.getLiteral());
+		return false;
+	}
+	
+	protected String getLogFile() {
+		if (doLog())
+			return configurationParameters.get(StaticConfigurationParameters.GENERATIONOPTIONSLOG_PATH.getLiteral());
+		return null;
+	}
+	
+	protected String getTargetPath(){
+		return configurationParameters.get(StaticConfigurationParameters.GENERATIONOPTIONSDESTINATION_PATH.getLiteral());
+	}
+}
---
+++ Application/trunk/com.bluexml.side.Application.generator/src/com/bluexml/side/application/generator/IGenerator.java
@@ -0,0 +1,48 @@
+package com.bluexml.side.application.generator;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+
+public interface IGenerator {
+
+	/**
+	 * This method is called before generation.
+	 * 
+	 * @param generationParameters
+	 *            the list of selected options for the generation (clean,
+	 *            verbose...)
+	 * @param generatorOptions
+	 *            the list of selected options for the generator.
+	 * @param configurationParamers
+	 *            the list of technical parameters.
+	 */
+	public void initialize(String[] generationParameters,
+			String[] generatorOptions,
+			Map<String, String> configurationParameters);
+
+	/**
+	 * This method check if the user have the license to use this generator.
+	 * 
+	 * @return true if the generator can be used.
+	 */
+	public boolean check();
+
+	/**
+	 * This method launch the generation.
+	 * 
+	 * @param model the input model
+	 * @return the list of generated files
+	 * @throws Exception 
+	 */
+	public Collection<String> generate(IFile model) throws Exception;
+	
+	/**
+	 * This method run the post-action after the generation. 
+	 *  
+	 * @return the list of modified files
+	 * @throws Exception 
+	 */
+	public Collection<String> complete() throws Exception;
+}
