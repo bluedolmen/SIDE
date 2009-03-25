4d1032ce3fc0ba4e08823d736b7d1519a152bb22
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-03-25 16:41:39 +0000

    fix fullPath
    git-svn-id: http://svn.bluexml.com/private/S-IDE@5560 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/action/utils/Generate.java
+++ Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/action/utils/Generate.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EPackage;
@@ -35,10 +36,7 @@ public class Generate {
 	 * @throws IllegalAccessException
 	 * @throws IOException
 	 */
-	public static void launch(Configuration configuration,
-			List<String> staticParameters, List<Model> models)
-			throws ClassNotFoundException, InstantiationException,
-			IllegalAccessException, IOException {
+	public static void launch(Configuration configuration, List<String> staticParameters, List<Model> models) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
 		// First we get the meta-model associated to a model
 		HashMap<String, List<IFile>> modelsInfo = (HashMap<String, List<IFile>>) getAssociatedMetaModel(models);
 
@@ -74,9 +72,9 @@ public class Generate {
 				// generator
 				if (modelsInfo.containsKey(elem.getId_metamodel())) {
 					AbstractGenerator generator = (AbstractGenerator) genObj;
-					generator.initialize(generationParameters,
-							generatorOptions, configurationParameters);
-					//TODO : CALL MERGE METHOD!!! For now, we only give the first one
+					generator.initialize(generationParameters, generatorOptions, configurationParameters);
+					// TODO : CALL MERGE METHOD!!! For now, we only give the
+					// first one
 					if (modelsInfo.size() > 0) {
 						try {
 							generator.generate(modelsInfo.get(elem.getId_metamodel()).get(0));
@@ -97,29 +95,19 @@ public class Generate {
 	 * @return
 	 * @throws IOException
 	 */
-	private static Map<String, List<IFile>> getAssociatedMetaModel(
-			List<Model> models) {
+	private static Map<String, List<IFile>> getAssociatedMetaModel(List<Model> models) {
 		Map<String, List<IFile>> result = new HashMap<String, List<IFile>>();
 		for (Model model : models) {
 			Resource modelResource = null;
 			try {
 				modelResource = EResourceUtils.createResource(model.getFile());
 				ResourceSet rs = modelResource.getResourceSet();
-				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(model.getFile()));
-				String fullPath = file.getFullPath().toOSString();
-				if (fullPath.charAt(fullPath.length()-1) != File.separatorChar) {
-					if (model.getFile().charAt(model.getFile().length()-1) != File.separatorChar) {
-						fullPath += File.separatorChar;
-					}
-				} else {
-					if (model.getFile().charAt(model.getFile().length()-1) != File.separatorChar){
-						model.setFile(model.getFile().substring(1));
-					}
-				}
-				fullPath += model.getFile();
 				
-				Resource loadedModel = EResourceUtils.openModel(
-						fullPath, null, rs);
+				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(model.getFile()));
+				
+				String fullPath = file.getRawLocation().toOSString();
+				
+				Resource loadedModel = EResourceUtils.openModel(fullPath, null, rs);
 
 				EPackage metaModel = getMetaModelEpackage(loadedModel);
 
@@ -127,12 +115,11 @@ public class Generate {
 					if (!result.containsKey(metaModel.getNsURI())) {
 						result.put(metaModel.getNsURI(), new ArrayList<IFile>());
 					}
-					
-					
+
 					if (file.exists()) {
 						result.get(metaModel.getNsURI()).add(file);
 					} else {
-						//TODO 
+						// TODO
 						throw new IOException("No model found at " + file.getFullPath());
 					}
 				}
@@ -155,8 +142,7 @@ public class Generate {
 		EPackage result = null;
 		if (r != null) {
 			if (r.getContents() != null && r.getContents().size() > 0) {
-				result = (EPackage) r.getContents().get(0).eClass()
-						.getEPackage();
+				result = (EPackage) r.getContents().get(0).eClass().getEPackage();
 			}
 		}
 		return result;
