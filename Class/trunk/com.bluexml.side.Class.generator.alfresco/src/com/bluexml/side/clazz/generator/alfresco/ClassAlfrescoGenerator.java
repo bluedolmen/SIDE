6cdd8b3028890062bb04da9df9c9a7f9209b3ab4
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-03-26 15:26:05 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@5590 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Class/trunk/com.bluexml.side.Class.generator.alfresco/src/com/bluexml/side/clazz/generator/alfresco/ClassAlfrescoGenerator.java
+++ Class/trunk/com.bluexml.side.Class.generator.alfresco/src/com/bluexml/side/clazz/generator/alfresco/ClassAlfrescoGenerator.java
@@ -25,46 +25,72 @@ public class ClassAlfrescoGenerator extends AbstractAcceleoGenerator {
 	 */
 
 	public static String TEMP_FOLDER = "tmp";
+	public static String CONFIGURATION_PARAMETER_CATALINA_HOME = "CATALINA_HOME";
+	public static String GENERATOR_OPTIONS_DATAMODEL = "alfresco.dataModel";
+	public static String GENERATOR_OPTIONS_SHARE_EXTENSION = "alfresco.share.extension";
 
 	public static String getTEMP_FOLDER() {
 		return TEMP_FOLDER;
 	}
 
 	public static String MMUri = "http://www.kerblue.org/class/1.0";
-	public List<String> classGeneratedFile = new ArrayList<String>();
-	public List<String> classTemplates = new ArrayList<String>();
+	public List<String> classGeneratedFile = null;
+	public List<String> classTemplates = null;
 
 	public static String wClientTmpFile = "shared/classes/alfresco/extension/web-client-config-custom." + TEMP_FOLDER;
 
 	public ClassAlfrescoGenerator() {
-		List<String> result = new ArrayList<String>();
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model.mt");		
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model_properties.mt");
-		
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/webClient/alfrescoGenerator_web_client_config.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/webClient/alfrescoGenerator_web_client_properties.mt");
-		
-		// only to extends alfresco share Forms
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/CustomConfiguration/web-framework-config-custom.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/file-upload-js-get-lib.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/file-upload-response-get-patch.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/flash-upload-js-get-patch.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/html-upload-js-get-patch.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-js-post.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-post-desc.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-html-post.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-json-post.mt");
-		result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-status-get.mt");
-		
-		classTemplates = result;
 
-		List<String> classGeneratedFile_ = new ArrayList<String>();
-		classGeneratedFile_.add("shared/classes/alfresco/extension/BxDS/model.xml");
-		classGeneratedFile_.add("shared/classes/alfresco/extension/BxDS/model.properties");
-		classGeneratedFile_.add("shared/classes/alfresco/extension/web-client-config-custom.xml");
-		classGeneratedFile_.add("shared/classes/alfresco/messages/webclient_fr_FR.properties");
-		classGeneratedFile = classGeneratedFile_;
+	}
 
+	public List<String> getClassGeneratedFile() {
+		if (classGeneratedFile == null) {
+			List<String> classGeneratedFile_ = new ArrayList<String>();
+			classGeneratedFile_.add("shared/classes/alfresco/extension/BxDS/model.xml");
+			classGeneratedFile_.add("shared/classes/alfresco/extension/BxDS/model.properties");
+			classGeneratedFile_.add("shared/classes/alfresco/extension/web-client-config-custom.xml");
+			classGeneratedFile_.add("shared/classes/alfresco/messages/webclient_fr_FR.properties");
+
+			classGeneratedFile = classGeneratedFile_;
+		}
+		return classGeneratedFile;
+	}
+
+	public void setClassGeneratedFile(List<String> classGeneratedFile) {
+		this.classGeneratedFile = classGeneratedFile;
+	}
+
+	public List<String> getClassTemplates() {
+		if (classTemplates == null) {
+			List<String> result = new ArrayList<String>();
+			if (getGeneratorOptionValue(GENERATOR_OPTIONS_DATAMODEL)) {				
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model_properties.mt");
+				
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/webClient/alfrescoGenerator_web_client_config.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/webClient/alfrescoGenerator_web_client_properties.mt");
+			}			
+
+			if (getGeneratorOptionValue(GENERATOR_OPTIONS_SHARE_EXTENSION)) {
+				// only to extends alfresco share Forms
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/CustomConfiguration/web-framework-config-custom.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/file-upload-js-get-lib.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/file-upload-response-get-patch.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/flash-upload-js-get-patch.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/SiteWebscripts/AlfrescoFileUploadPatch/html-upload-js-get-patch.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-js-post.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-post-desc.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-html-post.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-json-post.mt");
+				result.add("/com.bluexml.side.Class.generator.alfresco/templates/shareExtentions/Webscripts/bluexml-upload-response-status-get.mt");
+			}
+			classTemplates = result;
+		}
+		return classTemplates;
+	}
+
+	public void setClassTemplates(List<String> classTemplates) {
+		this.classTemplates = classTemplates;
 	}
 
 	@Override
@@ -74,7 +100,7 @@ public class ClassAlfrescoGenerator extends AbstractAcceleoGenerator {
 
 	@Override
 	protected List<String> getTemplates() {
-		return classTemplates;
+		return getClassTemplates();
 	}
 
 	public boolean check() {
@@ -128,12 +154,12 @@ public class ClassAlfrescoGenerator extends AbstractAcceleoGenerator {
 		}
 		return null;
 	}
-	
+
 	private List<IFile> searchForConflict() {
 		List<IFile> conflict = new ArrayList<IFile>();
 		IWorkspaceRoot myWorkspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 
-		for (String f : classGeneratedFile) {
+		for (String f : getClassGeneratedFile()) {
 			int index = f.lastIndexOf("/");
 			String fPath = f.substring(0, index);
 			String fName = f.substring(index);
