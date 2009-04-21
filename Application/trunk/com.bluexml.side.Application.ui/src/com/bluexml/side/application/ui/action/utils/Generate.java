0b7387c879cc37ce36fcee1108e3388e57f9dfd7
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-04-21 08:32:52 +0000

    Change error message.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@6023 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/action/utils/Generate.java
+++ Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/action/utils/Generate.java
@@ -14,6 +14,8 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -49,9 +51,25 @@ public class Generate extends Thread {
 	 * @throws IllegalAccessException
 	 * @throws IOException
 	 */
-	public static void run(Configuration configuration, List<String> staticParameters, List<Model> models, ProgressBar p_progressBar, Label p_label, StyledText p_styletext) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+	public static void run(Configuration configuration,
+			List<String> staticParameters, List<Model> models,
+			ProgressBar p_progressBar, Label p_label, StyledText p_styletext)
+			throws ClassNotFoundException, InstantiationException,
+			IllegalAccessException, IOException {
+		// Check to know if Log Path and Generation Path are set :
+		
+		
+		progressBar = p_progressBar;
+		label = p_label;
+		styletext = p_styletext;
 		// First we get the meta-model associated to a model
-		HashMap<String, List<IFile>> modelsInfo = (HashMap<String, List<IFile>>) getAssociatedMetaModel(models);
+		HashMap<String, List<IFile>> modelsInfo = null;
+		try {
+			modelsInfo = (HashMap<String, List<IFile>>) getAssociatedMetaModel(models);
+		} catch (IOException e) {
+			addText("Error with model : " + e.getMessage(), true);
+			e.printStackTrace();
+		}
 
 		// Secondly we seek the generator parameters, and separate static fields
 		// of dynamic fields
@@ -64,19 +82,41 @@ public class Generate extends Thread {
 				generationParameters.put(param.getKey(), param.getValue());
 			}
 		}
-		progressBar = p_progressBar;
-		label = p_label;
-		styletext = p_styletext;
-		generate(configuration, modelsInfo, configurationParameters, generationParameters);
+		
+		generate(configuration, modelsInfo, configurationParameters,
+				generationParameters);
+	}
+	
+	static private void  addText(String text) {
+		addText(text, false);
 	}
 
-	private static void generate(final Configuration configuration, final HashMap<String, List<IFile>> modelsInfo, final Map<String, String> configurationParameters, final Map<String, String> generationParameters) throws ClassNotFoundException, InstantiationException,
+	static private void addText(String text, boolean error) {
+		if (!error) {
+			styletext.append(text);
+		} else {
+			StyleRange style2 = new StyleRange();
+			style2.start = styletext.getText().length();
+			style2.length = text.length();
+			style2.foreground = Display.getDefault().getSystemColor(
+					SWT.COLOR_RED);
+			styletext.append(text);
+			styletext.setStyleRange(style2);
+		}
+	}
+	
+	private static void generate(final Configuration configuration,
+			final HashMap<String, List<IFile>> modelsInfo,
+			final Map<String, String> configurationParameters,
+			final Map<String, String> generationParameters)
+			throws ClassNotFoundException, InstantiationException,
 			IllegalAccessException {
 
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
 				// For all generator version we will call generation method
-				int nbTask = configuration.getElements().size() * NB_GENERATION_STEP;
+				int nbTask = configuration.getElements().size()
+						* NB_GENERATION_STEP;
 				progressBar.setMaximum(nbTask);
 				boolean error = false;
 
@@ -114,31 +154,47 @@ public class Generate extends Thread {
 						AbstractGenerator generator = (AbstractGenerator) genObj;
 						// We generate only if there is meta-model available for
 						// the generator
-						if (generator.shouldGenerate(modelsInfo, elem.getId_metamodel())) {
-							String name = elem.getId_generator().substring(elem.getId_generator().lastIndexOf(".") + 1);
+						if (generator.shouldGenerate(modelsInfo, elem
+								.getId_metamodel())) {
+							String name = elem.getId_generator()
+									.substring(
+											elem.getId_generator().lastIndexOf(
+													".") + 1);
 							label.setText("Initialize " + name);
-							addText("\nGeneration for " + name);
-							generator.initialize(generationParameters, generatorOptions, configurationParameters);
+							addText(System.getProperty("line.separator")
+									+ "Generation for " + name);
+							generator.initialize(generationParameters,
+									generatorOptions, configurationParameters);
 							addOneStep(progressBar);
 
 							label.setText("Merging models for " + name);
-							// TODO : CALL MERGE METHOD!!! For now, we only give
+							// TODO : CALL MERGE METHOD!!!
 							addOneStep(progressBar);
 
 							// The first one
 							if (modelsInfo.size() > 0) {
 								try {
 									label.setText("Generate for " + name);
-									generator.generate(modelsInfo, elem.getId_metamodel());
-									
-									label.setText("\nCompleting generation for " + name);
+									generator.generate(modelsInfo, elem
+											.getId_metamodel());
+
+									label.setText(System
+											.getProperty("line.separator")
+											+ "Completing generation for "
+											+ name);
 									Collection<IFile> generatedFiles = new ArrayList<IFile>();
 									generatedFiles = generator.complete();
 
-									addText("\nFiles generated by" + name);
+									addText(System
+											.getProperty("line.separator")
+											+ "Files generated by" + name);
 									if (generatedFiles != null) {
 										for (IFile filePath : generatedFiles) {
-											addText("\n" + filePath.getRawLocation().makeAbsolute().toOSString());
+											addText(System
+													.getProperty("line.separator")
+													+ filePath.getRawLocation()
+															.makeAbsolute()
+															.toOSString());
 										}
 									}
 									addOneStep(progressBar);
@@ -150,7 +206,12 @@ public class Generate extends Thread {
 
 								} catch (Exception e) {
 									error = true;
-									addText("\nERROR : " + (e.getMessage() != null ? e.getMessage() : "") + " see log file.");
+									addText(System
+											.getProperty("line.separator")
+											+ "ERROR : "
+											+ (e.getMessage() != null ? e
+													.getMessage() : "")
+											+ " see log file.", true);
 									e.printStackTrace();
 								}
 								addOneStep(progressBar);
@@ -166,9 +227,7 @@ public class Generate extends Thread {
 				}
 			}
 
-			private void addText(String text) {
-				styletext.setText(styletext.getText() + text);
-			}
+			
 
 			private void addOneStep(ProgressBar progressBar) {
 				progressBar.setSelection(progressBar.getSelection() + 1);
@@ -183,38 +242,49 @@ public class Generate extends Thread {
 	 * @param models
 	 * @return
 	 * @throws IOException
+	 * @throws IOException
 	 */
-	private static Map<String, List<IFile>> getAssociatedMetaModel(List<Model> models) {
+	private static Map<String, List<IFile>> getAssociatedMetaModel(
+			List<Model> models) throws IOException {
 		Map<String, List<IFile>> result = new HashMap<String, List<IFile>>();
 		for (Model model : models) {
 			Resource modelResource = null;
 			try {
 				modelResource = EResourceUtils.createResource(model.getFile());
-				ResourceSet rs = modelResource.getResourceSet();
-
-				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(model.getFile()));
-
-				String fullPath = file.getRawLocation().toOSString();
-
-				Resource loadedModel = EResourceUtils.openModel(fullPath, null, rs);
-
-				EPackage metaModel = getMetaModelEpackage(loadedModel);
-
-				if (metaModel != null) {
-					if (!result.containsKey(metaModel.getNsURI())) {
-						result.put(metaModel.getNsURI(), new ArrayList<IFile>());
-					}
-
-					if (file.exists()) {
-						result.get(metaModel.getNsURI()).add(file);
-					} else {
-						// TODO
-						throw new IOException("No model found at " + file.getFullPath());
-					}
-				}
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw new IOException("Error for file/model " + model.getName());
+			}
+			ResourceSet rs = modelResource.getResourceSet();
+
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(
+					new Path(model.getFile()));
+			if (!file.exists()) {
+				throw  new IOException("File " + file.getName()
+						+ " doesn't exist.");
+			}
+			String fullPath = file.getRawLocation().toOSString();
+			Resource loadedModel;
+			try {
+				loadedModel = EResourceUtils.openModel(fullPath, null, rs);
+			} catch (IOException e) {
+				IOException ioe = new IOException("Error with file " + file.getName() + " (check that it's a correct model file)" );
+				ioe.setStackTrace(e.getStackTrace());
+				throw ioe;
+			}
+			
+			EPackage metaModel = getMetaModelEpackage(loadedModel);
+
+			if (metaModel != null) {
+				if (!result.containsKey(metaModel.getNsURI())) {
+					result.put(metaModel.getNsURI(), new ArrayList<IFile>());
+				}
+
+				if (file.exists()) {
+					result.get(metaModel.getNsURI()).add(file);
+				} else {
+					throw new IOException("No model found at "
+							+ file.getFullPath());
+				}
 			}
 
 		}
@@ -231,7 +301,8 @@ public class Generate extends Thread {
 		EPackage result = null;
 		if (r != null) {
 			if (r.getContents() != null && r.getContents().size() > 0) {
-				result = (EPackage) r.getContents().get(0).eClass().getEPackage();
+				result = (EPackage) r.getContents().get(0).eClass()
+						.getEPackage();
 			}
 		}
 		return result;
