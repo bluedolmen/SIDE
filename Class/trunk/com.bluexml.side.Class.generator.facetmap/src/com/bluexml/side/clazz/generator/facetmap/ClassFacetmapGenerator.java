aadcbf663aa322f35407a6859b46fcf242a847f2
pbertrand <pbertrand@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-05-13 08:42:28 +0000

    ébauche de générateur de facetmap
    git-svn-id: http://svn.bluexml.com/private/S-IDE@6323 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Class/trunk/com.bluexml.side.Class.generator.facetmap/src/com/bluexml/side/clazz/generator/facetmap/ClassFacetmapGenerator.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * 	Copyright (C) BlueXML 2005-2009
+ *
+ * This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Boston, MA 02111.
+ ******************************************************************************/
+package com.bluexml.side.clazz.generator.facetmap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+
+import com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator;
+
+/**
+ * @author <a href="mailto:pbertrand@bluexml.com"> Pierre BERTRAND </a>
+ *
+ */
+public class ClassFacetmapGenerator extends AbstractAcceleoGenerator {
+
+	public static String GENERATOR_CODE = "CODE_GED_G_C_FACETMAP_2";
+	public static String MMUri = "http://www.kerblue.org/class/1.0";
+	
+	/* (non-Javadoc)
+	 * @see com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator#getMetamodelURI()
+	 */
+	@Override
+	protected String getMetamodelURI() {
+		return MMUri;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator#getTemplates()
+	 */
+	@Override
+	protected List<String> getTemplates() {
+			List<String> result = new ArrayList<String>();
+			result.add("/com.bluexml.side.Class.generator.alfresco/templates/facetmap-generation.mt.mt");
+			return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.bluexml.side.application.security.Checkable#check()
+	 */
+	public boolean check() {
+		return true;
+		//return SecurityHelper.check(GENERATOR_CODE,SidePreferences.getKey()); »
+	}
+
+	public Collection<IFile> complete() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
