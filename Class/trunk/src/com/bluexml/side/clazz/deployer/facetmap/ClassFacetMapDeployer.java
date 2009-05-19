1817203cc1d5c363eacc2cd9ebe6d13cabc65c8c
pbertrand <pbertrand@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-05-19 13:06:08 +0000

    Facetmap deployer
    git-svn-id: http://svn.bluexml.com/private/S-IDE@6430 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Class/trunk/src/com/bluexml/side/clazz/deployer/facetmap/ClassFacetMapDeployer.java
@@ -0,0 +1,37 @@
+package com.bluexml.side.clazz.deployer.facetmap;
+
+import java.io.File;
+
+import com.bluexml.side.application.deployer.Deployer;
+
+public class ClassFacetMapDeployer extends Deployer {
+
+	@Override
+	protected void clean(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void deployProcess(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void postProcess(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void preProcess(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean check() {
+		return true;
+	}
+
+}
