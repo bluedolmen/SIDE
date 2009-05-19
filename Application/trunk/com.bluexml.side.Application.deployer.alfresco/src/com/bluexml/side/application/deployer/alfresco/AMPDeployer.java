7ef70b1bd8013ce046f1d3771432b2e9d79014c2
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-05-19 10:10:06 +0000

    fix
    git-svn-id: http://svn.bluexml.com/private/S-IDE@6428 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Application/trunk/com.bluexml.side.Application.deployer.alfresco/src/com/bluexml/side/application/deployer/alfresco/AMPDeployer.java
+++ Application/trunk/com.bluexml.side.Application.deployer.alfresco/src/com/bluexml/side/application/deployer/alfresco/AMPDeployer.java
@@ -1,6 +1,7 @@
 package com.bluexml.side.application.deployer.alfresco;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,17 +19,50 @@ import com.bluexml.side.util.libs.FileHelper;
 public class AMPDeployer extends Deployer {
 	public static String CONFIGURATION_PARAMETER_CATALINA_HOME = "CATALINA_HOME";
 	static final String webapps = "webapps";
-	static String webappName = "alfresco";
+	static final String webappName = "alfresco";
 	static final String alfrescoWar = webappName + ".war";
 	static final String alfrescoWarOrg = webappName + ".war.org";
+	File orgWar = null;
+	File alfWar = null;
+	File alfWebapp = null;
 	// TODO mettre le vrai code
 	public static String DEPLOYER_CODE = "CODE_GED_G_W_ALFRESCO_30L";
+
+	public File getOrgWar() {
+		if (orgWar == null) {
+			orgWar = new File(getTomcatHome() + File.separator + webapps + File.separator + alfrescoWarOrg);
+		}
+		return orgWar;
+	}
+
+	public File getAlfWar() {
+		if (alfWar == null) {
+			alfWar = new File(getTomcatHome() + File.separator + webapps + File.separator + alfrescoWar);
+		}
+		return alfWar;
+	}
+
+	public File getAlfWebapp() {
+		if (alfWebapp == null) {
+			alfWebapp = new File(getTomcatHome() + File.separator + webapps + File.separator + webappName);
+		}
+		return alfWebapp;
+	}
 
 	@Override
 	protected void clean(File fileToDeploy) {
 		// remove existing deployed alfresco webapp.
-		File alfWebapp = new File(getTomcatHome() + File.separator + webapps + File.separator + webappName);
-		FileHelper.deleteFile(alfWebapp);
+		FileHelper.deleteFile(getAlfWebapp());
+		// clean war file
+		if (getOrgWar().exists()) {
+			// restore from backup
+			try {
+				FileHelper.copyFiles(getOrgWar(), getAlfWar(), true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -70,14 +104,12 @@ public class AMPDeployer extends Deployer {
 		return getGenerationParameters().get(CONFIGURATION_PARAMETER_CATALINA_HOME);
 	}
 
-	public static File getWarToPath(File tomcatHome) throws Exception {
-		File orgWar = new File(tomcatHome + File.separator + webapps + File.separator + alfrescoWarOrg);
-		File alfWar = new File(tomcatHome + File.separator + webapps + File.separator + alfrescoWar);
-		if (!orgWar.exists()) {
+	public File getWarToPath(File tomcatHome) throws Exception {
+		if (!getOrgWar().exists()) {
 			// buid backup
-			FileHelper.copyFiles(alfWar, orgWar, true);
+			FileHelper.copyFiles(getAlfWar(), getOrgWar(), true);
 		}
-		return alfWar;
+		return getAlfWar();
 	}
 
 	/**
