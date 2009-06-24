5142bc3d3ac3581d57c258cbdbc17c9272e98297
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-06-24 08:41:12 +0000

    Add List to child.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7004 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/View/trunk/com.bluexml.side.View.edit/src/com/bluexml/side/view/provider/FacetMapItemProvider.java
+++ MetaModel/View/trunk/com.bluexml.side.View.edit/src/com/bluexml/side/view/provider/FacetMapItemProvider.java
@@ -6,7 +6,6 @@
  */
 package com.bluexml.side.view.provider;
 
-
 import com.bluexml.side.view.FacetMap;
 import com.bluexml.side.view.ViewFactory;
 import com.bluexml.side.view.ViewPackage;
@@ -30,23 +29,19 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
 /**
- * This is the item provider adapter for a {@link com.bluexml.side.view.FacetMap} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a
+ * {@link com.bluexml.side.view.FacetMap} object. <!-- begin-user-doc --> <!--
+ * end-user-doc -->
+ * 
  * @generated
  */
-public class FacetMapItemProvider
-	extends AbstractViewItemProvider
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource {
+public class FacetMapItemProvider extends AbstractViewItemProvider implements
+		IEditingDomainItemProvider, IStructuredItemContentProvider,
+		ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public FacetMapItemProvider(AdapterFactory adapterFactory) {
@@ -54,9 +49,9 @@ public class FacetMapItemProvider
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -71,59 +66,56 @@ public class FacetMapItemProvider
 	}
 
 	/**
-	 * This adds a property descriptor for the Display Empty Facet feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Display Empty Facet feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addDisplayEmptyFacetPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_FacetMap_displayEmptyFacet_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_FacetMap_displayEmptyFacet_feature", "_UI_FacetMap_type"),
-				 ViewPackage.Literals.FACET_MAP__DISPLAY_EMPTY_FACET,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+				((ComposeableAdapterFactory) adapterFactory)
+						.getRootAdapterFactory(), getResourceLocator(),
+				getString("_UI_FacetMap_displayEmptyFacet_feature"), getString(
+						"_UI_PropertyDescriptor_description",
+						"_UI_FacetMap_displayEmptyFacet_feature",
+						"_UI_FacetMap_type"),
+				ViewPackage.Literals.FACET_MAP__DISPLAY_EMPTY_FACET, true,
+				false, false, ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE, null,
+				null));
 	}
 
 	/**
-	 * This adds a property descriptor for the Facet Display Type feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Facet Display Type feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addFacetDisplayTypePropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_FacetMap_facetDisplayType_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_FacetMap_facetDisplayType_feature", "_UI_FacetMap_type"),
-				 ViewPackage.Literals.FACET_MAP__FACET_DISPLAY_TYPE,
-				 true,
-				 false,
-				 false,
-				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
-				 null,
-				 null));
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+				((ComposeableAdapterFactory) adapterFactory)
+						.getRootAdapterFactory(), getResourceLocator(),
+				getString("_UI_FacetMap_facetDisplayType_feature"), getString(
+						"_UI_PropertyDescriptor_description",
+						"_UI_FacetMap_facetDisplayType_feature",
+						"_UI_FacetMap_type"),
+				ViewPackage.Literals.FACET_MAP__FACET_DISPLAY_TYPE, true,
+				false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null,
+				null));
 	}
 
 	/**
-	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This specifies how to implement {@link #getChildren} and is used to
+	 * deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand},
+	 * {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in
+	 * {@link #createCommand}. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(
+			Object object) {
 		if (childrenFeatures == null) {
 			super.getChildrenFeatures(object);
 			childrenFeatures.add(ViewPackage.Literals.PAGINABLE__PAGING);
@@ -132,48 +124,49 @@ public class FacetMapItemProvider
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	protected EStructuralFeature getChildFeature(Object object, Object child) {
-		// Check the type of the specified child object and return the proper feature to use for
+		// Check the type of the specified child object and return the proper
+		// feature to use for
 		// adding (see {@link AddCommand}) it as a child.
 
 		return super.getChildFeature(object, child);
 	}
 
 	/**
-	 * This returns FacetMap.gif.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns FacetMap.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/FacetMap"));
+		return overlayImage(object, getResourceLocator().getImage(
+				"full/obj16/FacetMap"));
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the label text for the adapted class. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
 	 * @_generated
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((FacetMap)object).getName();
-		return label == null || label.length() == 0 ?
-			getString("_UI_FacetMap_type") :
-			label;
+		String label = ((FacetMap) object).getName();
+		return label == null || label.length() == 0 ? getString("_UI_FacetMap_type")
+				: label;
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to
+	 * update any cached children and by creating a viewer notification, which
+	 * it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -181,53 +174,72 @@ public class FacetMapItemProvider
 		updateChildren(notification);
 
 		switch (notification.getFeatureID(FacetMap.class)) {
-			case ViewPackage.FACET_MAP__DISPLAY_EMPTY_FACET:
-			case ViewPackage.FACET_MAP__FACET_DISPLAY_TYPE:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-				return;
-			case ViewPackage.FACET_MAP__PAGING:
-				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-				return;
+		case ViewPackage.FACET_MAP__DISPLAY_EMPTY_FACET:
+		case ViewPackage.FACET_MAP__FACET_DISPLAY_TYPE:
+			fireNotifyChanged(new ViewerNotification(notification, notification
+					.getNotifier(), false, true));
+			return;
+		case ViewPackage.FACET_MAP__PAGING:
+			fireNotifyChanged(new ViewerNotification(notification, notification
+					.getNotifier(), true, false));
+			return;
 		}
 		super.notifyChanged(notification);
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s
+	 * describing the children that can be created under this object. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @_generated
 	 */
 	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+	protected void collectNewChildDescriptors(
+			Collection<Object> newChildDescriptors, Object object) {
 		super.collectNewChildDescriptors(newChildDescriptors, object);
-		
-		newChildDescriptors.add
-			(createChildParameter
-				(ViewPackage.Literals.PAGINABLE__PAGING,
-				 ViewFactory.eINSTANCE.createPaging()));
+
+		newChildDescriptors.add(createChildParameter(
+				ViewPackage.Literals.PAGINABLE__PAGING, ViewFactory.eINSTANCE
+						.createPaging()));
+
+		newChildDescriptors.add(createChildParameter(
+				ViewPackage.Literals.FIELD_CONTAINER__CHILDREN,
+				ViewFactory.eINSTANCE.createDataList()));
+
+		newChildDescriptors.add(createChildParameter(
+				ViewPackage.Literals.FIELD_CONTAINER__CHILDREN,
+				ViewFactory.eINSTANCE.createDataTable()));
+
+		newChildDescriptors.add(createChildParameter(
+				ViewPackage.Literals.FIELD_CONTAINER__CHILDREN,
+				ViewFactory.eINSTANCE.createFacetMap()));
+
+		newChildDescriptors.add(createChildParameter(
+				ViewPackage.Literals.FIELD_CONTAINER__CHILDREN,
+				ViewFactory.eINSTANCE.createTree()));
 	}
 
 	/**
-	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the label text for
+	 * {@link org.eclipse.emf.edit.command.CreateChildCommand}. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
-	public String getCreateChildText(Object owner, Object feature, Object child, Collection<?> selection) {
+	public String getCreateChildText(Object owner, Object feature,
+			Object child, Collection<?> selection) {
 		Object childFeature = feature;
 		Object childObject = child;
 
-		boolean qualify =
-			childFeature == ViewPackage.Literals.FIELD_CONTAINER__CHILDREN ||
-			childFeature == ViewPackage.Literals.FIELD_CONTAINER__DISABLED;
+		boolean qualify = childFeature == ViewPackage.Literals.FIELD_CONTAINER__CHILDREN
+				|| childFeature == ViewPackage.Literals.FIELD_CONTAINER__DISABLED;
 
 		if (qualify) {
-			return getString
-				("_UI_CreateChild_text2",
-				 new Object[] { getTypeText(childObject), getFeatureText(childFeature), getTypeText(owner) });
+			return getString("_UI_CreateChild_text2", new Object[] {
+					getTypeText(childObject), getFeatureText(childFeature),
+					getTypeText(owner) });
 		}
 		return super.getCreateChildText(owner, feature, child, selection);
 	}
