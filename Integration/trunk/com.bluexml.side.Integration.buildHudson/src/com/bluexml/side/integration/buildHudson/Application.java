68bf7fd06a49cec02bc63f77af6153602bcc5656
amenager <amenager@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-02 07:33:57 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@7232 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/src/com/bluexml/side/integration/buildHudson/Application.java
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/src/com/bluexml/side/integration/buildHudson/Application.java
@@ -561,7 +561,7 @@ public class Application {
 
 		out += "\t<target name=\"genJavadoc\" depends=\"\" description=\"description\">\n";
 		out += "\t\t<javadoc destdir=\"${buildDir}" + File.separator
-				+ "doc\">\n";
+				+ "doc" + File.separator +"Javadoc\">\n";
 
 		for (int i = 0; i < projects.length; i++) {
 			// si le mot 'feature' n'est pas présent dans le nom du projet
