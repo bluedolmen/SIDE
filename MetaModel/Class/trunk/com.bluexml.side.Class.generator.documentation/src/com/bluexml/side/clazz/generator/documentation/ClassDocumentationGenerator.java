5e6141313070f395db77fd3eadd4715a96983b66
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-07 14:49:48 +0000

    Initial Import
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7417 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Class/trunk/com.bluexml.side.Class.generator.documentation/src/com/bluexml/side/clazz/generator/documentation/ClassDocumentationGenerator.java
@@ -0,0 +1,31 @@
+package com.bluexml.side.clazz.generator.documentation;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+
+import com.bluexml.side.util.generator.documentation.DocumentationGenerator;
+
+public class ClassDocumentationGenerator extends DocumentationGenerator{
+
+	public Collection<IFile> complete() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<IFile> generate(Map<String, List<IFile>> models,
+			String id_metamodel) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean shouldGenerate(HashMap<String, List<IFile>> modelsInfo,
+			String id_metamodel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
