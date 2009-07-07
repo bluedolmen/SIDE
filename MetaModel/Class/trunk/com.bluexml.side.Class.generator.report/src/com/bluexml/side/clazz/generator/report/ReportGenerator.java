82142e8d5742d2c7c2f3cb5bfaa2f3a28d2d5f6f
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-07 11:36:52 +0000

    Add service log.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7355 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Class/trunk/com.bluexml.side.Class.generator.report/src/com/bluexml/side/clazz/generator/report/ReportGenerator.java
+++ MetaModel/Class/trunk/com.bluexml.side.Class.generator.report/src/com/bluexml/side/clazz/generator/report/ReportGenerator.java
@@ -12,17 +12,18 @@ import com.bluexml.side.util.libs.FileHelper;
 import com.bluexml.side.util.libs.IFileHelper;
 
 public class ReportGenerator extends AbstractAcceleoPackageGenerator{
-	
+
 	public static String GENERATOR_OPTIONS_BIRT = "report.birt";
 	public static String GENERATOR_CONFIGURATION_PARAMETER_AUTHOR = "report.author";
 	public static String MMUri = "http://www.kerblue.org/class/1.0";
+	public static String BIRT_WEBAPP_KEY = "birt.webapp";
 
 
 	public ReportGenerator(){
 		techVersion = "Birt_2.3.x";
 		this.setTEMP_FOLDER(getTechVersion());
 	}
-	
+
 	@Override
 	protected String getMetamodelURI() {
 		return MMUri;
@@ -31,25 +32,25 @@ public class ReportGenerator extends AbstractAcceleoPackageGenerator{
 	@Override
 	protected List<String> getTemplates() {
 		List<String> birtTemplates = new ArrayList<String>();
-		
+
 		if (getGeneratorOptionValue(GENERATOR_OPTIONS_BIRT)) {
 			birtTemplates.add("/com.bluexml.side.Class.generator.report/src/com/bluexml/side/clazz/generator/report/birt/templates/alfrescoGeneretor_birt_all_in_one.mt");
 		}
-			
+
 		return birtTemplates;
 	}
 
 	public boolean check() {
 		return true;
 	}
-	
+
 	/**
 	 * This method return the author of the generation
-	 * 
+	 *
 	 * @return the author of the generation
 	 */
 	public static String getAuthor(){
-		
+
 		String auteur = "John Doe";
 		if(!"".equals(getGenerationParameter(GENERATOR_CONFIGURATION_PARAMETER_AUTHOR))){
 			auteur = getGenerationParameter(GENERATOR_CONFIGURATION_PARAMETER_AUTHOR);
@@ -58,18 +59,22 @@ public class ReportGenerator extends AbstractAcceleoPackageGenerator{
 	}
 
 	public Collection<IFile> complete() throws Exception {
-		
+
 		String target = IFileHelper.getSystemFolderPath(getTargetPath()+File.separator+getTechVersion())+File.separator;
 		new File(target).mkdirs();
 		//String source = IFileHelper.getSystemFolderPath(getTemporaryFolder()+File.separator+groupedModels.keySet().toArray()[0].toString()) + File.separator;
 		String source = IFileHelper.getSystemFolderPath(getTemporaryFolder()) + File.separator;
 		FileHelper.copyFiles(new File(source), new File(target), true);
-		
+
 		for (IFile f : generatedFiles) {
 			addFileGeneratedLog("Files Generated", f.getLocation().toOSString() + "", IFileHelper.getFile(f).toURI());
-			//addServiceLog("Birt Report",f.getLocation().toOSString() + "", IFileHelper.getFile(f).toURI());
+			if (generationParameters.containsKey(BIRT_WEBAPP_KEY)) {
+				String birtUrl = generationParameters.get(BIRT_WEBAPP_KEY);
+				String uri = birtUrl + "/frameset?__report=" + IFileHelper.getFile(f).getName();
+				addServiceLog("Birt Report",uri, uri);
+			}
 		}
-		
+
 		return generatedFiles;
 	}
 
