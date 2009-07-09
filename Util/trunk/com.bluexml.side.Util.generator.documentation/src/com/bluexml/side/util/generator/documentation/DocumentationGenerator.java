6b91bb2799f709ccf84771abdf89413c2b3f4467
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-09 08:32:05 +0000

    Add doc generation.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7468 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Util/trunk/com.bluexml.side.Util.generator.documentation/src/com/bluexml/side/util/generator/documentation/DocumentationGenerator.java
+++ Util/trunk/com.bluexml.side.Util.generator.documentation/src/com/bluexml/side/util/generator/documentation/DocumentationGenerator.java
@@ -1,7 +1,36 @@
 package com.bluexml.side.util.generator.documentation;
 
-import com.bluexml.side.util.generator.AbstractGenerator;
+import java.io.File;
+import java.util.Collection;
 
-public abstract class DocumentationGenerator extends AbstractGenerator {
+import org.eclipse.core.resources.IFile;
 
+import com.bluexml.side.util.generator.acceleo.AbstractAcceleoGenerator;
+import com.bluexml.side.util.libs.FileHelper;
+import com.bluexml.side.util.libs.IFileHelper;
+
+public abstract class DocumentationGenerator extends AbstractAcceleoGenerator {
+
+	public static String MMUri;
+
+	public Collection<IFile> complete() throws Exception {
+		String target = IFileHelper.getSystemFolderPath(getTargetPath()+File.separator+getTechVersion())+File.separator;
+		new File(target).mkdirs();
+		String source = IFileHelper.getSystemFolderPath(getTemporaryFolder()) + File.separator;
+		FileHelper.copyFiles(new File(source), new File(target), true);
+
+		for (IFile f : generatedFiles) {
+			addFileGeneratedLog("Files Generated", f.getLocation().toOSString() + "", IFileHelper.getFile(f).toURI());
+		}
+
+		return generatedFiles;
+	}
+
+	protected String getMetamodelURI() {
+		return MMUri;
+	}
+
+	public boolean check() {
+		return true;
+	}
 }
