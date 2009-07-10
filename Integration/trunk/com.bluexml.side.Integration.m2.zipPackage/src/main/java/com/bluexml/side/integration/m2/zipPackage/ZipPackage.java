dc922d4a57337060dc66936863089ce9c3edbb11
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-10 07:30:29 +0000

    Maven2 plug-in to manage zip package (this packager do nothing packaging is delegated to builder)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7502 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Integration/trunk/com.bluexml.side.Integration.m2.zipPackage/src/main/java/com/bluexml/side/integration/m2/zipPackage/ZipPackage.java
@@ -0,0 +1,97 @@
+package com.bluexml.side.integration.m2.zipPackage;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.archiver.MavenArchiveConfiguration;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.zip.ZipArchiver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Goal which touches a timestamp file.
+ * 
+ * 
+ * @goal zip
+ * @phase package
+ */
+public class ZipPackage extends AbstractMojo {
+	/**
+	* The maven project.
+	* @parameter expression="${project}"
+	* @required
+	* @readonly
+	*/
+	private MavenProject project;
+
+	/**
+	* The directory containing generated classes.
+	* @parameter expression="${project.build.outputDirectory}"
+	* @required
+	* @readonly
+	*/
+	private File classesDirectory;
+
+	/**
+	 * Location of the file.
+	 * 
+	 * @parameter expression="${project.build.directory}"
+	 * @required
+	 */
+	private File outputDirectory;
+
+	
+	
+	protected MavenArchiveConfiguration archive = new MavenArchiveConfiguration();
+	private ZipArchiver zarchiver;
+	
+	public void execute() throws MojoExecutionException {
+		File f = outputDirectory;
+
+		if (!f.exists()) {
+			f.mkdirs();
+		}
+
+		File touch = new File(f, "touch.txt");
+
+		FileWriter w = null;
+		String archiveName = project.getBuild().getFinalName() + ".zip";
+	    File custFile = new File(outputDirectory, archiveName);
+
+		project.getArtifact().setFile(custFile);
+
+		try {
+			w = new FileWriter(touch);
+
+			w.write("touch.txt");
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error creating file " + touch, e);
+		} finally {
+			if (w != null) {
+				try {
+					w.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+	}
+}
