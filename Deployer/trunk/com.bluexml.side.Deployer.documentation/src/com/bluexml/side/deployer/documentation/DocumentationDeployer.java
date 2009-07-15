4e5f4f995e27fd98f79758908d5648a72a499649
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-15 13:13:28 +0000

    Integrate doc to log. Improve doc.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7550 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.documentation/src/com/bluexml/side/deployer/documentation/DocumentationDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.documentation/src/com/bluexml/side/deployer/documentation/DocumentationDeployer.java
@@ -1,13 +1,16 @@
 package com.bluexml.side.deployer.documentation;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
-import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 
 import com.bluexml.side.application.StaticConfigurationParameters;
 import com.bluexml.side.util.deployer.Deployer;
-import com.bluexml.side.util.libs.FileHelper;
+import com.bluexml.side.util.documentation.LogSave;
 import com.bluexml.side.util.libs.IFileHelper;
 
 public class DocumentationDeployer extends Deployer {
@@ -19,11 +22,16 @@ public class DocumentationDeployer extends Deployer {
 
 	@Override
 	protected void deployProcess(File fileToDeploy) throws Exception {
-		File src = fileToDeploy;
-		// TODO : Get configuration name
-		IResource destIFile = IFileHelper.createFolder(getConfigurationParameters().get(StaticConfigurationParameters.GENERATIONOPTIONSLOG_PATH.getLiteral()) + File.separator + getConfigurationParameters().get("configurationName") + File.separator + "doc");
-		File dest = IFileHelper.convertIRessourceToFile(destIFile);
-		FileHelper.copyFiles(src, dest, true);
+		IContainer src = IFileHelper.getIFolder(fileToDeploy);
+		if (src != null) {
+			IFolder dest = IFileHelper.createFolder(getConfigurationParameters().get(StaticConfigurationParameters.GENERATIONOPTIONSLOG_PATH.getLiteral()) + File.separator + getConfigurationParameters().get("configurationName") + File.separator + LogSave.LOG_DOC_FOLDER);
+			IFileHelper.refreshFolder((IFolder)src);
+			List<IFile> toDeploy = IFileHelper.getAllFiles(src);
+			for (IFile xmlFile : toDeploy)
+				if (xmlFile.getName().endsWith("-doc.xml")) {
+					IFileHelper.moveFile(xmlFile, dest, true);
+				}
+		}
 	}
 
 	@Override
