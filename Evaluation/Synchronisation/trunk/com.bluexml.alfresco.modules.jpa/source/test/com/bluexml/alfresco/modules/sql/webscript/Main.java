41d4ee618d4d11649a4eaec7664b9f6c35569a91
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-16 14:19:33 +0000

    Initial import for JPA testing
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7581 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Evaluation/Synchronisation/trunk/com.bluexml.alfresco.modules.jpa/source/test/com/bluexml/alfresco/modules/sql/webscript/Main.java
@@ -0,0 +1,13 @@
+package test.com.bluexml.alfresco.modules.sql.webscript;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestSearchWebscript.testSimpleType("Contact");
+
+	}
+
+}
