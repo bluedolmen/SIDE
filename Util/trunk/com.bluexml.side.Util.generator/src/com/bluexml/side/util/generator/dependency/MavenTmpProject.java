05616954296a0e752294d4b1e0f226624cd9b452
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-16 15:06:54 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@7583 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Util/trunk/com.bluexml.side.Util.generator/src/com/bluexml/side/util/generator/dependency/MavenTmpProject.java
+++ Util/trunk/com.bluexml.side.Util.generator/src/com/bluexml/side/util/generator/dependency/MavenTmpProject.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.embedder.Configuration;
-import org.apache.maven.embedder.DefaultConfiguration;
-import org.apache.maven.embedder.MavenEmbedder;
-import org.apache.maven.embedder.MavenEmbedderException;
 import org.apache.maven.execution.MavenExecutionResult;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -25,10 +21,7 @@ public class MavenTmpProject {
 	private File pomFile;
 	private File projectFolder;
 	private File workFolder;
-	private MavenEmbedder embedder;
-	private Configuration configuration;
-	public static final Namespace NAMESPACE_MAVENPOM = Namespace.getNamespace("pom", "http://maven.apache.org/POM/4.0.0");
-
+	private MavenUtil mavenUtil;
 	private static final String TARGET_ARTIFACT = "tmpProject";
 
 	private DependencesManager dm;
@@ -38,18 +31,15 @@ public class MavenTmpProject {
 
 	public MavenTmpProject() throws Exception {
 		this.workFolder = new File(".");
-		initMaven();
 	}
 
 	public MavenTmpProject(File workingFolder) throws Exception {
 		this.workFolder = workingFolder;
-		initMaven();
 	}
 
 	public MavenTmpProject(File workingFolder, DependencesManager dm) throws Exception {
 		this.workFolder = workingFolder;
 		this.dm = dm;
-		initMaven();
 	}
 
 	private void createAndGetDependencies() throws Exception {
@@ -59,12 +49,6 @@ public class MavenTmpProject {
 		getDependences();
 	}
 
-	private void initMaven() throws MavenEmbedderException {
-		configuration = new DefaultConfiguration();
-		configuration.setClassLoader(Thread.currentThread().getContextClassLoader());
-		embedder = new MavenEmbedder(configuration);
-		tpmProject = new File(workFolder.getAbsolutePath() + File.separator + TARGET_ARTIFACT);
-	}
 
 	/**
 	 * update pom file to add wanted dependencies
@@ -80,7 +64,6 @@ public class MavenTmpProject {
 			Element depends = buildPomDependency(n, mc);
 			dependencies.addContent(depends);
 		}
-
 		FileOutputStream os = new FileOutputStream(pomFile);
 		outputter.output(pom, os);
 		os.close();
@@ -95,7 +78,6 @@ public class MavenTmpProject {
 	 */
 	private Element buildPomDependency(Namespace n, ModuleConstraint mc) {
 		Element depends = new Element("dependency", n.getURI());
-
 		Element groupId = new Element("groupId", n.getURI()).setText(mc.getGroupId());
 		Element artifactId = new Element("artifactId", n.getURI()).setText(mc.getArtifactId());
 		Element version = new Element("version", n.getURI()).setText(mc.getVersionRange());
@@ -118,7 +100,7 @@ public class MavenTmpProject {
 	private void getDependences() throws Exception {
 		// resolve version ranges for dependencies (replace by used version
 		// number)
-		MavenExecutionResult result = MavenUtil.doMavenGoal(tpmProject, "versions:resolve-ranges");
+		MavenExecutionResult result = getMavenUtil().doMavenGoal(tpmProject, "versions:resolve-ranges");
 		if (result.getExceptions().size() > 0) {
 			List<?> exceps = result.getExceptions();
 			System.err.println("Exception occure during maven process :" + exceps);
@@ -126,26 +108,12 @@ public class MavenTmpProject {
 		}
 
 		// call compile goal to get dependencies in local repository
-		MavenExecutionResult result2 = MavenUtil.doMavenGoal(tpmProject, "compile");
+		MavenExecutionResult result2 = getMavenUtil().doMavenGoal(tpmProject, "compile");
 		if (result2.getExceptions().size() > 0) {
 			List<?> exceps = result2.getExceptions();
 			System.err.println("Exception occure during maven process :" + exceps);
 			throw new Exception("Exception occure during maven process :" + exceps);
 		}
-
-		/*
-		 * DefaultMavenExecutionRequest archetypeCreateRequest = new
-		 * DefaultMavenExecutionRequest();
-		 * archetypeCreateRequest.setBaseDirectory(tpmProject);
-		 * archetypeCreateRequest.setGoals(Arrays.asList(new String[] {
-		 * "compile" })); archetypeCreateRequest.setProperty("interactiveMode",
-		 * "false"); archetypeCreateRequest.setProperty("basedir",
-		 * workFolder.getAbsolutePath() + File.separator + TARGET_ARTIFACT);
-		 * 
-		 * 
-		 * archetypeCreateRequest.setUpdateSnapshots(true); MavenExecutionResult
-		 * result = embedder.execute(archetypeCreateRequest); return result;
-		 */
 	}
 
 	/**
@@ -165,31 +133,11 @@ public class MavenTmpProject {
 
 	}
 
-	public static void printDoc(File xml) throws Exception {
-		SAXBuilder sxb = new SAXBuilder();
-		Document doc = sxb.build(xml);
-		printDoc(doc);
-	}
-
-	public static void printDoc(Document doc) throws Exception {
-		try {
-			XMLOutputter output = new XMLOutputter(Format.getPrettyFormat());
-			String element_string = output.outputString(doc);
-			System.out.println(element_string);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static Element createElement(String eleName, Document doc) {
-		return new Element(eleName, doc.getBaseURI());
-	}
-
 	public List<File> getAllDependenciesResources() throws Exception {
 		List<File> dependencies = new ArrayList<File>();
 		createAndGetDependencies();
 		Document pom = sxb.build(pomFile);
-		ArtifactRepository artRep = embedder.getLocalRepository();
+		ArtifactRepository artRep = getMavenUtil().getEmbedder().getLocalRepository();
 		System.out.println(artRep.getBasedir());
 		System.out.println(artRep.getUrl());
 		File f = new File(artRep.getBasedir());
@@ -198,33 +146,29 @@ public class MavenTmpProject {
 			for (ModuleConstraint moduleConstraint : l) {
 				String path = moduleConstraint.getGroupId().replaceAll("\\.", File.separator) + File.separator + moduleConstraint.getArtifactId();
 				// String version = moduleConstraint.getVersionMin().toString();
-				String version = getVersionOf(pom, moduleConstraint.getGroupId(), moduleConstraint.getArtifactId());
+				String version = MavenUtil.getVersionOf(pom, moduleConstraint.getGroupId(), moduleConstraint.getArtifactId());
 				path += File.separator + version;
 				String moduleName = moduleConstraint.getArtifactId() + "-" + version + "." + moduleConstraint.getModuleType();
 				File localmodule = new File(f.getAbsolutePath() + File.separator + path + File.separator + moduleName);
 				if (localmodule.exists()) {
 					dependencies.add(localmodule);
 				} else {
-					System.err.println("local package not found ! " + localmodule);
+					throw new Exception("local package not found ! " + localmodule);
 				}
 			}
 		} else {
-			System.err.println("local repository not found !");
+			throw new Exception("local repository not found !");
 		}
 		return dependencies;
 	}
+	
 
-	public static String getVersionOf(Document pom, String groupId, String artifactId) throws Exception {
-		Element project = pom.getRootElement();
-		Namespace n = project.getNamespace();
-		// get the dependence version
-		List<Element> l = pom.getRootElement().getChild("dependencies", n).getChildren("dependency", n);
-		for (Element element : l) {
-			if (element.getChild("groupId", n).getText().equals(groupId) && element.getChild("artifactId", n).getText().equals(artifactId)) {
-				return element.getChild("version", n).getText();
-			}
+	
+	private MavenUtil getMavenUtil() {
+		if (mavenUtil ==null) {
+			mavenUtil = new MavenUtil();
 		}
-		throw new Exception("version number not found please report as bug");
+		return mavenUtil;
 	}
 
 }
--- Util/trunk/com.bluexml.side.Util.generator/src/com/bluexml/side/util/generator/dependency/MavenUtil.java
+++ Util/trunk/com.bluexml.side.Util.generator/src/com/bluexml/side/util/generator/dependency/MavenUtil.java
@@ -7,40 +7,66 @@ import java.util.List;
 import org.apache.maven.embedder.Configuration;
 import org.apache.maven.embedder.DefaultConfiguration;
 import org.apache.maven.embedder.MavenEmbedder;
-import org.apache.maven.embedder.MavenEmbedderException;
 import org.apache.maven.execution.DefaultMavenExecutionRequest;
 import org.apache.maven.execution.MavenExecutionResult;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
 
 public class MavenUtil {
-
-	public static MavenEmbedder getMavenEmbedder() throws MavenEmbedderException {
-		Configuration configuration;
-		MavenEmbedder embedder;
-		configuration = new DefaultConfiguration();
-		configuration.setClassLoader(Thread.currentThread().getContextClassLoader());
-		embedder = new MavenEmbedder(configuration);
-		return embedder;
-	}
-
-	public static MavenExecutionResult doMavenGoal(File baseDir, List<String> goals) throws Exception {
+	private MavenEmbedder embedder;
+	
+	
+	
+	public MavenExecutionResult doMavenGoal(File baseDir, List<String> goals) throws Exception {
 		DefaultMavenExecutionRequest archetypeCreateRequest = new DefaultMavenExecutionRequest();
 		archetypeCreateRequest.setBaseDirectory(baseDir);
 		archetypeCreateRequest.setGoals(goals);
 		archetypeCreateRequest.setProperty("interactiveMode", "false");
 		archetypeCreateRequest.setProperty("basedir", baseDir.getAbsolutePath().toString());
 
-		MavenEmbedder embedder = getMavenEmbedder();
+		MavenEmbedder embedder = getEmbedder();
 		archetypeCreateRequest.setUpdateSnapshots(true);
 		MavenExecutionResult result = embedder.execute(archetypeCreateRequest);
 		return result;
 	}
 
-	public static MavenExecutionResult doMavenGoal(File baseDir, String goal) throws Exception {
+	public MavenExecutionResult doMavenGoal(File baseDir, String goal) throws Exception {
 		return doMavenGoal(baseDir, new String[] { goal });
 	}
 
-	public static MavenExecutionResult doMavenGoal(File baseDir, String[] goals) throws Exception {
+	public MavenExecutionResult doMavenGoal(File baseDir, String[] goals) throws Exception {
 		return doMavenGoal(baseDir, Arrays.asList(goals));
 	}
-
+	
+	public MavenEmbedder getEmbedder() throws Exception {
+		if (embedder ==null) {
+			Configuration configuration;
+			configuration = new DefaultConfiguration();
+			configuration.setClassLoader(Thread.currentThread().getContextClassLoader());
+			embedder = new MavenEmbedder(configuration);			
+		}
+		return embedder;
+	}
+	/**
+	 * get the version number of the dependency in the given pom
+	 * @param pom, the maven pom document
+	 * @param groupId, the groupId of the dependency 
+	 * @param artifactId, the artifactId of the dependency
+	 * @return
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public static String getVersionOf(Document pom, String groupId, String artifactId) throws Exception {
+		Element project = pom.getRootElement();
+		Namespace n = project.getNamespace();
+		// get the dependence version
+		List<Element> l = pom.getRootElement().getChild("dependencies", n).getChildren("dependency", n);
+		for (Element element : l) {
+			if (element.getChild("groupId", n).getText().equals(groupId) && element.getChild("artifactId", n).getText().equals(artifactId)) {
+				return element.getChild("version", n).getText();
+			}
+		}
+		throw new Exception("version number not found please report as bug");
+	}
 }
