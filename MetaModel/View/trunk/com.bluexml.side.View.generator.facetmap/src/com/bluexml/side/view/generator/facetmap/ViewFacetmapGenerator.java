448da469d249c0c81a3ff8a0a3e3974bdf7dd0bf
da <da@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-07-23 13:07:47 +0000

    fix techVersion id
    git-svn-id: http://svn.bluexml.com/private/S-IDE@7675 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/View/trunk/com.bluexml.side.View.generator.facetmap/src/com/bluexml/side/view/generator/facetmap/ViewFacetmapGenerator.java
+++ MetaModel/View/trunk/com.bluexml.side.View.generator.facetmap/src/com/bluexml/side/view/generator/facetmap/ViewFacetmapGenerator.java
@@ -35,88 +35,99 @@ import com.bluexml.side.view.generator.facetmap.utils.FacetmapConstants;
 
 /**
  * @author <a href="mailto:pbertrand@bluexml.com"> Pierre BERTRAND </a>
- *
+ * 
  */
 public class ViewFacetmapGenerator extends AbstractAcceleoPackageGenerator implements FacetmapConstants {
 	public static String GENERATOR_CODE = "CODE_GED_G_C_FACETMAP_2";
 	public static String MMUri = "http://www.kerblue.org/view/1.0";
-	
-	public ViewFacetmapGenerator(){
-		techVersion = "Facetmap 2.x";
+
+	public ViewFacetmapGenerator() {
+		techVersion = "Facetmap_2.x";
 		this.setTEMP_FOLDER(getTechVersion());
 	}
-	
-	/* (non-Javadoc)
-	 * @see com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator#getMetamodelURI()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator
+	 * #getMetamodelURI()
 	 */
 	@Override
 	protected String getMetamodelURI() {
 		return MMUri;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator#getTemplates()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.bluexml.side.application.generator.acceleo.AbstractAcceleoGenerator
+	 * #getTemplates()
 	 */
 	@Override
 	protected List<String> getTemplates() {
-			List<String> templates = new ArrayList<String>();
-				//common
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-propertyfile-generation.mt");
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-cmis-generation.mt");
-				//facets
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-cmis2xfml-generation.mt");
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-xslbasicfacets-generation.mt");
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-xslrightnav-generation.mt");
-				//results
-				templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-content-basicresults-generation.mt");
-			return templates;
+		List<String> templates = new ArrayList<String>();
+		// common
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-propertyfile-generation.mt");
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-cmis-generation.mt");
+		// facets
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-cmis2xfml-generation.mt");
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-xslbasicfacets-generation.mt");
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-facets-xslrightnav-generation.mt");
+		// results
+		templates.add("/com.bluexml.side.View.generator.facetmap/templates/facetmap-content-basicresults-generation.mt");
+		return templates;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.bluexml.side.application.security.Checkable#check()
 	 */
 	public boolean check() {
-		return SecurityHelper.check(GENERATOR_CODE,SidePreferences.getKey());
+		return SecurityHelper.check(GENERATOR_CODE, SidePreferences.getKey());
 	}
-	
-	public String getGeneratorParameter(EObject ob,String paramName) throws Exception {
+
+	public String getGeneratorParameter(EObject ob, String paramName) throws Exception {
 		return configurationParameters.get(paramName);
 	}
 
 	public Collection<IFile> complete() throws Exception {
-		// Don't support 
-		if (groupedModels.entrySet().size()>1)
+		// Don't support
+		if (groupedModels.entrySet().size() > 1)
 			throw new Exception("Error too many root packages for facetmap.");
 		setTEMP_FOLDER("generator_" + getClass().getName());
 		generatedFiles.addAll(buildPackages(groupedModels.keySet().toArray()[0].toString()));
 		for (IFile f : generatedFiles) {
-			addFileGeneratedLog("Files Generated", f.getLocation().toOSString() + "", IFileHelper.getFile(f).toURI());		}
+			addFileGeneratedLog("Files Generated", f.getLocation().toOSString() + "", IFileHelper.getFile(f).toURI());
+		}
 		return generatedFiles;
 	}
 
 	@Override
 	public Collection<IFile> buildPackages(String modelId) throws Exception {
-		String folder = IFileHelper.getSystemFolderPath(getTemporaryFolder()+FILESEP+modelId)+FILESEP;
-		//Destinations
+		String folder = IFileHelper.getSystemFolderPath(getTemporaryFolder() + FILESEP + modelId) + FILESEP;
+		// Destinations
 		String destFacets = folder + "zip" + FILESEP + WEBAPP_FACETS;
 		String destContent = folder + "zip" + FILESEP + WEBAPP_CONTENT;
-		//Copy
+		// Copy
 		FileHelper.copyFiles(new File(folder + "common"), new File(destFacets), true);
 		FileHelper.copyFiles(new File(folder + "common"), new File(destContent), true);
 		FileHelper.copyFiles(new File(folder + "facets"), new File(destFacets), true);
 		FileHelper.copyFiles(new File(folder + "content"), new File(destContent), true);
-		//Zip
-		String zipFolder = IFileHelper.getSystemFolderPath(getTargetPath()+FILESEP+getTechVersion())+FILESEP;
+		// Zip
+		String zipFolder = IFileHelper.getSystemFolderPath(getTargetPath() + FILESEP + getTechVersion()) + FILESEP;
 		new File(zipFolder).mkdirs();
 		File zipFacets = new File(zipFolder + WEBAPP_FACETS + ".zip");
 		File zipContent = new File(zipFolder + WEBAPP_CONTENT + ".zip");
 		ZipManager.zip(new File(destFacets), zipFacets, false);
 		ZipManager.zip(new File(destContent), zipContent, false);
-		//Creating file collection
-		IFolder workingDir = IFileHelper.getIFolder(getTemporaryFolder()+ FILESEP + "../");
+		// Creating file collection
+		IFolder workingDir = IFileHelper.getIFolder(getTemporaryFolder() + FILESEP + "../");
 		Collection<IFile> pkgs = new ArrayList<IFile>();
-		pkgs.add(IFileHelper.getIFile(workingDir.toString().replaceFirst("[^/]*/", "/") + FILESEP + getTechVersion()+ FILESEP + WEBAPP_FACETS + ".zip"));
-		pkgs.add(IFileHelper.getIFile(workingDir.toString().replaceFirst("[^/]*/", "/") + FILESEP + getTechVersion()+ FILESEP + WEBAPP_CONTENT + ".zip"));
+		pkgs.add(IFileHelper.getIFile(workingDir.toString().replaceFirst("[^/]*/", "/") + FILESEP + getTechVersion() + FILESEP + WEBAPP_FACETS + ".zip"));
+		pkgs.add(IFileHelper.getIFile(workingDir.toString().replaceFirst("[^/]*/", "/") + FILESEP + getTechVersion() + FILESEP + WEBAPP_CONTENT + ".zip"));
 		return pkgs;
 	}
 
