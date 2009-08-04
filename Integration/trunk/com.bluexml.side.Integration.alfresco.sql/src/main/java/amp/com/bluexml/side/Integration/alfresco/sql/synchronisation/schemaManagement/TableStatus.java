5c1b91607fa1fa95e89fde97583635e19c53bb06
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-04 08:39:04 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@7948 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/amp/com/bluexml/side/Integration/alfresco/sql/synchronisation/schemaManagement/TableStatus.java
+++
@@ -1,9 +0,0 @@
-package com.bluexml.side.Integration.alfresco.sql.synchronisation.schemaManagement;
-
-public enum TableStatus {
-	EXISTS_SIMILAR,
-	EXISTS_MATCHED,
-	EXISTS_UNMATCHED,
-	NOT_EXISTS,
-	NOT_CHECKABLE
-}
