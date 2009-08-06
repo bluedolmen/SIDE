46b23d214e3f1cdff3c6285aacd6d3377842d3ac
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-06 09:08:18 +0000

    Webscript POST => GET and now working on Alfresco v. 3.2
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8016 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/com/bluexml/side/Integration/alfresco/sql/webscript/SQLSearchServiceWebscript.java
+++ Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/com/bluexml/side/Integration/alfresco/sql/webscript/SQLSearchServiceWebscript.java
@@ -1,51 +1,87 @@
 package com.bluexml.side.Integration.alfresco.sql.webscript;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.web.scripts.AbstractWebScript;
+import org.alfresco.web.scripts.Cache;
+import org.alfresco.web.scripts.DeclarativeWebScript;
+import org.alfresco.web.scripts.Status;
+import org.alfresco.web.scripts.WebScriptException;
 import org.alfresco.web.scripts.WebScriptRequest;
-import org.alfresco.web.scripts.WebScriptResponse;
+import org.apache.log4j.Logger;
 
 import com.bluexml.side.Integration.alfresco.sql.searcher.SQLSearchService;
 
-public class SQLSearchServiceWebscript extends AbstractWebScript {
-		
-	public void execute(WebScriptRequest webscriptrequest, WebScriptResponse webscriptresponse)
-			throws IOException {
-		String result = "";
-				
-		String searchedType = webscriptrequest.getParameter("type");
-		
-		Collection<NodeRef> resultCollection = sqlSearchService.selectNodes(searchedType, "true");
-		result = formatResult_(resultCollection);
-		
-		webscriptresponse.setContentType(WebScriptResponse.XML_FORMAT);
-		webscriptresponse.getWriter().write(result);
+public class SQLSearchServiceWebscript extends DeclarativeWebScript {
+	
+	Logger logger = Logger.getLogger(getClass());
+	
+	private static final String PARAM_ID_OBJECT_TYPE = "type";
+	private static final String PARAM_ID_CONSTRAINT = "where";
+	private static final String TEMPLATE_PARAM_ID_NODES = "nodes";
+	
+//	public void execute(WebScriptRequest webscriptrequest, WebScriptResponse webscriptresponse)
+//			throws IOException {
+//		String result = "";
+//				
+//		String searchedType = webscriptrequest.getParameter(PARAM_ID_OBJECT_TYPE);
+//		String constraint = webscriptrequest.getParameter(PARAM_ID_CONSTRAINT);
+//		
+//		if (constraint == null || "".equals(constraint)) {
+//			constraint = "true";
+//		}
+//		
+//		Collection<NodeRef> resultCollection = sqlSearchService.selectNodes(searchedType, constraint);
+//		result = formatResult_(resultCollection);
+//		
+//		//webscriptresponse.setContentType(WebScriptResponse.XML_FORMAT);
+//		webscriptresponse.getWriter().write(result);
+//
+//	}
 
-	}
-
-	String formatResult_(Collection<NodeRef> collection) {
-		StringBuilder result = new StringBuilder();
+    protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	
+		String searchedType = req.getParameter(PARAM_ID_OBJECT_TYPE);
+		String constraint = req.getParameter(PARAM_ID_CONSTRAINT);
 		
-		result.append("<nodes>");
-		for (NodeRef nodeRef : collection) {
-			result.append("<node>" + nodeRef + "</node>");
+		if (searchedType == null || "".equals(searchedType)) {
+			throw new WebScriptException(HttpServletResponse.SC_BAD_REQUEST, "Search type not provided");
+			// TODO : check whether this type is a valid type (and thus that the table exists)
+//			logger.error("searched type have to be defined");
+//			status.setCode(400, "type is a required parameter");
+//			status.setRedirect(true);
 		}
-		result.append("</nodes>");
 		
-		return result.toString();
-	}
+		if (constraint == null || "".equals(constraint)) {
+			constraint = "true";
+		}
+		
+		Collection<NodeRef> resultCollection = sqlSearchService.selectNodes(searchedType, constraint);
+		model.put(TEMPLATE_PARAM_ID_NODES, resultCollection);
+		
+		return model;
+    }
+	
+//	String formatResult_(Collection<NodeRef> collection) {
+//		StringBuilder result = new StringBuilder();
+//		
+//		result.append("<nodes>");
+//		for (NodeRef nodeRef : collection) {
+//			result.append("<node>" + nodeRef + "</node>");
+//		}
+//		result.append("</nodes>");
+//		
+//		return result.toString();
+//	}
 	
 	// BEAN MANAGEMENT
 	
 	private SQLSearchService sqlSearchService;
-		
-	public SQLSearchService getSQLSearchService() {
-		return sqlSearchService;
-	}
 	
 	public void setSQLSearchService(SQLSearchService sqlSearchService) {
 		this.sqlSearchService = sqlSearchService;
