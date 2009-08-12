ef328152a26b0dc24f1309f6bc57f0e49c1af4c9
avoglozin <avoglozin@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-12 09:15:08 +0000

    Aftermath of adding reference from FormWorkflow to FormClass and changed ModelChoiceFields's widget type from reference enum to choice.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8143 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Form/trunk/com.bluexml.side.Form.edit/src/com/bluexml/side/form/provider/FormWorkflowItemProvider.java
+++ MetaModel/Form/trunk/com.bluexml.side.Form.edit/src/com/bluexml/side/form/provider/FormWorkflowItemProvider.java
@@ -16,6 +16,7 @@ import java.util.List;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
 
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -58,8 +59,31 @@ public class FormWorkflowItemProvider
 		if (itemPropertyDescriptors == null) {
 			super.getPropertyDescriptors(object);
 
+			addDataFormPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Data Form feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addDataFormPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_FormWorkflow_DataForm_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_FormWorkflow_DataForm_feature", "_UI_FormWorkflow_type"),
+				 FormPackage.Literals.FORM_WORKFLOW__DATA_FORM,
+				 true,
+				 false,
+				 true,
+				 null,
+				 null,
+				 null));
 	}
 
 	/**
