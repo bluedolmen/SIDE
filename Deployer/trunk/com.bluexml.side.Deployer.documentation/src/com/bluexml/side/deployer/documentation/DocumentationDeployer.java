091df8daea75c3864507d046ae79d77d4250215b
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-12 14:40:35 +0000

    Change how is retrieve build.xml.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8174 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.documentation/src/com/bluexml/side/deployer/documentation/DocumentationDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.documentation/src/com/bluexml/side/deployer/documentation/DocumentationDeployer.java
@@ -1,7 +1,10 @@
 package com.bluexml.side.deployer.documentation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
@@ -40,33 +43,74 @@ public class DocumentationDeployer extends Deployer {
 			AntRunner runner = new AntRunner();
 			Map<String,String> properties = new HashMap<String, String>();
 			runner.addUserProperties(properties);
-			File f = getAntBuildFile();
-
-			if (src.getType() == IFile.FOLDER) {
-				List<IFolder> srcFiles = IFileHelper.getAllFolderForFolder((IFolder)src);
-				for (IFolder file : srcFiles) {
-					String name = file.getName();
-					List<IFolder> docFiles = IFileHelper.getAllFolderForFolder(file);
-					for (IFolder file2 : docFiles) {
-						if (file2.getName().equals(DOC_FOLDER_NAME)) {
-							runner.setBuildFileLocation(f.getAbsolutePath());
-							properties.put("destDir", dest.getLocation().toFile().getAbsolutePath()); //$NON-NLS-1$
-							properties.put("sourceDir", file.getLocation().toFile().getAbsolutePath()); //$NON-NLS-1$
-							properties.put("docName", name); //$NON-NLS-1$
-							runner.addUserProperties(properties);
-							runner.run();
+			File f = getAntBuildFile(dest);
+			if (f != null && f.exists()) {
+				if (src.getType() == IFile.FOLDER) {
+					List<IFolder> srcFiles = IFileHelper.getAllFolderForFolder((IFolder)src);
+					for (IFolder file : srcFiles) {
+						String name = file.getName();
+						List<IFolder> docFiles = IFileHelper.getAllFolderForFolder(file);
+						for (IFolder file2 : docFiles) {
+							if (file2.getName().equals(DOC_FOLDER_NAME)) {
+								runner.setBuildFileLocation(f.getAbsolutePath());
+								properties.put("destDir", dest.getLocation().toFile().getAbsolutePath()); //$NON-NLS-1$
+								properties.put("sourceDir", file.getLocation().toFile().getAbsolutePath()); //$NON-NLS-1$
+								properties.put("docName", name); //$NON-NLS-1$
+								runner.addUserProperties(properties);
+								runner.run();
+							}
 						}
 					}
 				}
+			} else {
+				throw new Exception("DocumentationDeployer : build.xml file isn't found.");
 			}
 		}
 	}
 
-	private File getAntBuildFile() throws URISyntaxException, IOException {
+	private File getAntBuildFile(IFolder dest) throws URISyntaxException, IOException {
 		Bundle b = Platform.getBundle(Activator.PLUGIN_ID);
-		URL entry = b.getEntry("src/com/bluexml/side/deployer/documentation/build.xml"); //$NON-NLS-1$
-		File f = new File(FileLocator.toFileURL(entry).toURI());
-		return f;
+		String folderPath = dest.getLocation().toOSString() + File.separator;
+		String folderSource = "src/com/bluexml/side/deployer/documentation/";
+		File ant = moveFile(folderPath, "build.xml", folderSource);
+//		URL entry = b.getEntry("src/com/bluexml/side/deployer/documentation/build.xml"); //$NON-NLS-1$
+//		File f = new File(FileLocator.toFileURL(entry).toURI());
+		return ant;
+	}
+
+	private static File moveFile(String folderDest, String fileName,
+			String folderSource) throws IOException  {
+		/*InputStream in = LogSave.class.getResourceAsStream(folderSource
+				+ fileName);*/
+		InputStream in = DocumentationDeployer.class.getClassLoader().getResourceAsStream(folderSource+"/"+fileName);
+		File dest = new File(folderDest);
+		if (!dest.exists()) {
+			dest.mkdirs();
+		}
+
+		File file = new File(folderDest + fileName);
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(file);
+		} catch (FileNotFoundException e) {
+			System.err.println("FileOutputStream can't be call.");
+			e.printStackTrace();
+			throw e;
+		}
+		int data;
+		try {
+			data = in.read();
+		} catch (IOException e) {
+			System.err.println("Data can't be read");
+			e.printStackTrace();
+			throw e;
+		}
+		while (data != -1) {
+			fos.write(data);
+			data = in.read();
+		}
+		fos.close();
+		return file;
 	}
 
 	@Override
