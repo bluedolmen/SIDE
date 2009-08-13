63d2e7d5c34b605c1e6e245e181b81efd01ad914
ec <ec@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-13 15:53:36 +0000

    Remove some options we don't need.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8260 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/ReportDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/ReportDeployer.java
@@ -4,12 +4,12 @@
  * This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Boston, MA 02111.
@@ -23,15 +23,14 @@ import com.bluexml.side.util.libs.FileHelper;
 import com.bluexml.side.util.libs.IFileHelper;
 
 public class ReportDeployer extends Deployer {
-	
-	public static String CONFIGURATION_REPORT_BIRT_DEPLOYER = "birt.deployer";
+
 	public static String CONFIGURATION_TOMCAT_INSTALLATION = "CATALINA_HOME";
 	static final String webapps = "webapps";
 
 	public String getParam(String ParamName) {
 		return getGenerationParameters().get(ParamName);
 	}
-	
+
 	// Emplacements des fichiers
 	public String getLocationInTomcat() throws Exception{
 		String path = getParam(CONFIGURATION_TOMCAT_INSTALLATION) + File.separator + webapps + File.separator + "birt" + File.separator;
@@ -41,18 +40,18 @@ public class ReportDeployer extends Deployer {
 			addErrorLog("Birt Webapp not deployed", e.getStackTrace(), null);
 			throw e;
 		}
-			
-		return path;		
+
+		return path;
 	}
-	
+
 	public String getLocationInGeneration() {
 		System.out.println("Path location"+IFileHelper.getSystemFolderPath(getTargetPath()+File.separator+getTechVersion())+File.separator + "Content_type_report.rptdesign");
 		return IFileHelper.getSystemFolderPath(getTargetPath()+File.separator+getTechVersion())+File.separator + "Content_type_report.rptdesign";
 	}
-	
+
 	@Override
 	protected void clean(File fileToDeploy) throws Exception {}
-	
+
 	@Override
 	protected void deployProcess(File fileToDeploy) throws Exception {
 		// Getting Files
@@ -71,15 +70,15 @@ public class ReportDeployer extends Deployer {
 		}
 		// Path where the files need to be copied
 		String path = getLocationInTomcat()+ "Content_type_report.rptdesign";
-		
+
 		//Deleting
 		if(new File(getLocationInTomcat() + "Content_type_report.rptdesign.bak").exists())
 			FileHelper.deleteFile(new File(getLocationInTomcat() + "Content_type_report.rptdesign.bak"));
-		
+
 		//Renaming
 		if(new File(getLocationInTomcat() + "Content_type_report.rptdesign").exists())
 			new File(getLocationInTomcat() + "Content_type_report.rptdesign").renameTo(new File(getLocationInTomcat() + "Content_type_report.rptdesign.bak"));
-		
+
 		// Copying
 		FileHelper.copyFiles(birtReport, new File(path), true);
 	}
@@ -92,7 +91,7 @@ public class ReportDeployer extends Deployer {
 
 	/**
 	 * This method check if the user have the license to use this deployer.
-	 * 
+	 *
 	 * @return true if the deployer can be used.
 	 */
 	public boolean check() {
