e517e41b8e47c4bc2e8a8181706f24f0cd46fe43
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-18 10:25:58 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@8417 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/com/bluexml/side/Integration/alfresco/sql/synchronisation/dictionary/BidirectionalDatabaseDictionary.java
+++
@@ -1,9 +0,0 @@
-package com.bluexml.side.Integration.alfresco.sql.synchronisation.dictionary;
-
-public interface BidirectionalDatabaseDictionary extends DatabaseDictionary {
-
-	public abstract String resolveTableAsClassName(String tableName);
-
-	public abstract String resolveTableAsAssociationName(String tableName);
-
-}
