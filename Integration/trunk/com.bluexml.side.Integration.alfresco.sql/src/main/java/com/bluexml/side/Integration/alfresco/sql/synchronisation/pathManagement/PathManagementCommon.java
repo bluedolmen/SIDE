359957216432a31b9a60a0b9cd522ac56610f404
bpajot <bpajot@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-18 10:26:56 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@8418 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/com/bluexml/side/Integration/alfresco/sql/synchronisation/pathManagement/PathManagementCommon.java
+++
@@ -1,6 +0,0 @@
-package com.bluexml.side.Integration.alfresco.sql.synchronisation.pathManagement;
-
-
-public interface PathManagementCommon {
-	public static final String PATH_COLUMN_NAME = "path";
-}
--- Integration/trunk/com.bluexml.side.Integration.alfresco.sql/src/main/java/com/bluexml/side/Integration/alfresco/sql/synchronisation/pathManagement/PathService.java
+++
@@ -1,8 +0,0 @@
-package com.bluexml.side.Integration.alfresco.sql.synchronisation.pathManagement;
-
-import org.alfresco.service.cmr.repository.NodeRef;
-
-public interface PathService {
-
-	public void updatePath(NodeRef nodeRef);
-}
