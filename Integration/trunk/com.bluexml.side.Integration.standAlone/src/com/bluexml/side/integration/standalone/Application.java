0dd5ea31274cfbe2f1b5eb0019a2b51cae96b690
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-19 12:01:49 +0000

    modify to launch application with 2 parameters
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8458 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.standAlone/src/com/bluexml/side/integration/standalone/Application.java
+++ Integration/trunk/com.bluexml.side.Integration.standAlone/src/com/bluexml/side/integration/standalone/Application.java
@@ -1,6 +1,11 @@
 package com.bluexml.side.integration.standalone;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Iterator;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
@@ -16,18 +21,40 @@ public class Application implements IApplication {
 		arguments = (String[]) context.getArguments().get("application.args");
 
 		System.out.println("Start !!!!!!!!!!");
-
-		  // Start Generation
-		System.out.println("Start Generation");
-      
-		//File file = new File("workspaceStandAlone/StandAlone/models/My.application");
-		File file = new File(arguments[0]);
 		
-		System.out.println("file.exists(): " + file.exists());
-		Generate gen = new Generate(file, arguments[1]);
-		System.out.println("created");
+		if(!arguments[0].toString().contains(".application")){
+			File root = new File(arguments[0]);
+			String[] extensions = {"application"};
+			boolean recursive = true;
+			try{
+				Collection files = FileUtils.listFiles(root, extensions, recursive);
+			
+			        for (Iterator iterator = files.iterator(); iterator.hasNext();) {
+			        	File file = (File) iterator.next();
+			        	System.out.println("File = " + file.getAbsolutePath());
+			        	File fileAP = new File(file.getAbsolutePath());
+			        	System.out.println("file.exists(): " + fileAP.exists());
+			        	Generate gen = new Generate(fileAP, arguments[1]);
+			        	System.out.println("created");
+			        	gen.run();
+			        }          
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			// Start Generation
+		}else{
+			System.out.println("Start Generation");
+      
+			//File file = new File("workspaceStandAlone/StandAlone/models/My.application");
+		
+			File file = new File(arguments[0]);
+			System.out.println("file.exists(): " + file.exists());
+			Generate gen = new Generate(file, arguments[1]);
+			System.out.println("created");
 
-		gen.run();
+			gen.run();
+		}
 
 		System.out.println("End Generation");
 
