ca8ad1e572101e2180b6bbb462d36ab0b77d60ad
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-26 07:47:43 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@8562 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace_for_test.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace_for_test.sh
@@ -3,10 +3,11 @@
 # 1st parameter = source workspace path
 # 2nd parameter = target workspace path
 # 3rd parameter = percent representing disk full
-if [ $# -eq 3 ]; then
+if [ $# -eq 4 ]; then
   SOURCE_WORKSPACE=$1
   TARGET_WORKSPACE=$2
   FULL=$3
+  OUTPUT_FILE=$4
 else
   exit -2
 fi
@@ -22,5 +23,10 @@ if [ $DF_HOME -gt $FULL ]; then
 fi
 # prepare workspace: copy build.properties, script, jar into working space
 cp -R $SOURCE_WORKSPACE/* $TARGET_WORKSPACE
+
+# create file for the result
+touch $OUTPUT_FILE
+cho "<html><head><title>S-IDE Automatic Tests</title></head>" > $OUTPUT_FILE
+echo "<body><h1>S-IDE Automatic Generator tests</h1>" >>  $INDEX > $OUTPUT_FILE
 
 exit 0
