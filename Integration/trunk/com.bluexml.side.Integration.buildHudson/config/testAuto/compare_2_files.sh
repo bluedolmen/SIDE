8ea28730e5f9a9139760cfd14e344d802024dc38
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-08-27 13:24:53 +0000

    add scripts for testAuto
    git-svn-id: http://svn.bluexml.com/private/S-IDE@8627 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/testAuto/compare_2_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $1 > res1.xml
+sed 's/<date>.*date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res1.xml > res11.xml
+sed 's/date=\".*\" \/>/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/>/' res11.xml > res111.xml
+
+sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $2 > res2.xml
+sed 's/<date>.*<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res2.xml > res22.xml
+sed 's/date=\".*\" \/>/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/>/' res22.xml > res222.xml
+
+
+./diffxml/diffxml.sh -q res111.xml res222.xml > out.xml 
+
+
+
+if test -s out.xml
+then        
+	echo "not empty"
+	rm out.xml
+	./diffxml/diffxml.sh res111.xml res222.xml > out.xml
+else
+        echo "empty"
+fi
+
+rm res1.xml
+rm res11.xml
+rm res111.xml
+rm res2.xml
+rm res22.xml
+rm res222.xml
