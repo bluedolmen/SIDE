868c1120439b93688db88e7f0c1abfdfe0bd60ab
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-04 12:16:58 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@8878 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/rename_folder.pl
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/rename_folder.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl;
-
+use File::Copy;
 #================================================
 # rename folder with the last part
 # com.bluexml.side.Integration.alfresco.cmisFacetMap into cmisFacetMap
@@ -15,6 +15,8 @@ open (POMF,">>$pom") or die "Cannot open the file $pom";
 foreach my $nom ( @LesFichiers ) {
 	$delete="true";
 	$rep=$nom;
+	
+	#recherche des repertoire contant le fichier $pom
 	opendir(RPT,"$rep") or die "Can not open the folder $rep";
 	while($file_name=readdir(RPT)){
 		if ($file_name eq $pom){
@@ -22,6 +24,8 @@ foreach my $nom ( @LesFichiers ) {
 		}
 	}
 	closedir(RPT);
+	
+	#supprime les repertoire ne contenant pas de fichier $pom
 	if ($delete eq "true") {
 		use File::Path;
 		rmtree([$rep],0,1) or "Can not delete the folder $rep";
@@ -29,8 +33,16 @@ foreach my $nom ( @LesFichiers ) {
 		
 	}
 	else {
-		$rep =~ s/.*\.(.+)$/$1/;
-		rename("$nom", "$rep") || die "Fail $!";
+                @nomrepertoire=split(/\./,$rep);
+                $rep =~ s/.*\.(.+)$/$1/;
+                $taille = $#nomrepertoire-1;
+                $repracine=$nomrepertoire[$taille];
+                if (!( -d "$repracine")) {
+                    mkdir ("$repracine",0755) || die ("Err. Cr. répertoire \n");
+                }
+                mkdir ("$repracine/$rep",0755) || die ("Err. Cr. répertoire \n");
+                move ("$nom","$repracine/$rep") or die("Impossible de copier $repertoire1 $!");
+				#rename("$nom", "$rep") || die "Fail $!";
 	}
 
 
@@ -38,24 +50,24 @@ foreach my $nom ( @LesFichiers ) {
 
 #======================================================
 # Nombre d'arguments : 1
-# Argument(s)        : un répertoire ($repertoire)
+# Argument(s)        : un r√©pertoire ($repertoire)
 # Retourne           : Tableau de fichier (@fichiers)
 #======================================================
 sub ListersFichiers {
   my ( $repertoire ) = @_;
   my @fichiers;
   
-  # Ouverture d'un répertoire
+  # Ouverture d'un r√©pertoire
   opendir (my $FhRep, $repertoire) 
-    or die "impossible d'ouvrir le répertoire $repertoire\n";
+    or die "impossible d'ouvrir le r√©pertoire $repertoire\n";
   
-  # Liste fichiers et répertoire sauf (. et ..)
+  # Liste fichiers et r√©pertoire sauf (. et ..)
   my @Contenu = grep { !/^\.\.?$/ } readdir($FhRep);
   
-  # Fermeture du répertoire
+  # Fermeture du r√©pertoire
   closedir ($FhRep);
   
-  # On récupère tous les fichiers
+  # On r√©cup√®re tous les fichiers
   foreach my $nom ( @Contenu ) {
     # Fichiers
     
@@ -64,7 +76,7 @@ sub ListersFichiers {
     }
     # Repertoires
     #elsif ( -d "$repertoire/$nom") {
-      # recursivité
+      # recursivit√©
     #  push ( @fichiers, ListersFichiers("$repertoire/$nom") );
     #}
   }
