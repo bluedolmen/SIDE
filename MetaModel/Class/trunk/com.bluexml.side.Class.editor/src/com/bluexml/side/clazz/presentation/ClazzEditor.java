b0647f35fc3f142435716cbef7962cd15c70f498
bch <bch@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-10 10:05:07 +0000

    Swicth between FormClass and Class
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9068 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Class/trunk/com.bluexml.side.Class.editor/src/com/bluexml/side/clazz/presentation/ClazzEditor.java
+++ MetaModel/Class/trunk/com.bluexml.side.Class.editor/src/com/bluexml/side/clazz/presentation/ClazzEditor.java
@@ -1323,7 +1323,7 @@ public class ClazzEditor
 	 * This accesses a cached version of the content outliner.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @_generated
 	 */
 	public IContentOutlinePage getContentOutlinePage() {
 		if (contentOutlinePage == null) {
@@ -1349,7 +1349,7 @@ public class ClazzEditor
 					if (!editingDomain.getResourceSet().getResources().isEmpty()) {
 					  // Select the root object in the view.
 					  //
-					  contentOutlineViewer.setSelection(new StructuredSelection(editingDomain.getResourceSet().getResources().get(0)), true);
+					  //contentOutlineViewer.setSelection(new StructuredSelection(editingDomain.getResourceSet().getResources().get(0)), true);
 					}
 				}
 
