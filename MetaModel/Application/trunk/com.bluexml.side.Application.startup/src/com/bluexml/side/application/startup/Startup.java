652869285de5b3bb7b1907dc46e0e7239149eee4
bchevallereau <bchevallereau@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-15 09:29:43 +0000

    Add startup plugin
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9224 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Application/trunk/com.bluexml.side.Application.startup/src/com/bluexml/side/application/startup/Startup.java
@@ -0,0 +1,41 @@
+package com.bluexml.side.application.startup;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IStartup;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+
+public class Startup implements IStartup {
+
+	public void earlyStartup() {
+
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchWindow dwindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+				IWorkbenchPage page = dwindow.getActivePage();
+				List<String> commandLineArgs = Arrays.asList(Platform.getCommandLineArgs());
+				if (page != null && !commandLineArgs.contains("-nostartupbrowser")) {
+					try {
+						URL url;
+						if (commandLineArgs.contains("-side.url")) {
+							int index = commandLineArgs.indexOf("-side.url");
+							index++;
+							url = new URL(commandLineArgs.get(index));
+						} else
+							url = new URL("http://www.bluexml.com/");
+						PlatformUI.getWorkbench().getBrowserSupport().createBrowser("com.bluexml.side.application.startup").openURL(url);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+
+	}
+
+}
