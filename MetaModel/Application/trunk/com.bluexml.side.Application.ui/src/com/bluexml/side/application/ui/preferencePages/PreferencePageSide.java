8901be0782c750fb513e995e4944f4caaee5cd04
ecambray <ecambray@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-17 15:49:24 +0000

    Change preference pages.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9362 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/preferencePages/PreferencePageSide.java
@@ -0,0 +1,34 @@
+package com.bluexml.side.application.ui.preferencePages;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePageSide extends PreferencePage implements
+		IWorkbenchPreferencePage {
+
+	public PreferencePageSide() {
+		noDefaultAndApplyButton();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout());
+
+		return composite;
+	}
+}
