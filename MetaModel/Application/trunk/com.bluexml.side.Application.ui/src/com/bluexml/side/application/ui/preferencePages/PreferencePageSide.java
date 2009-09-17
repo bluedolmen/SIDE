2914c87c3d039cc2787e64bec9b7ff9c515d0efa
ecambray <ecambray@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-17 15:52:38 +0000

    Add S-IDE presentation text.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9365 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/preferencePages/PreferencePageSide.java
+++ MetaModel/Application/trunk/com.bluexml.side.Application.ui/src/com/bluexml/side/application/ui/preferencePages/PreferencePageSide.java
@@ -2,11 +2,15 @@ package com.bluexml.side.application.ui.preferencePages;
 
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import com.bluexml.side.application.ui.Activator;
 
 public class PreferencePageSide extends PreferencePage implements
 		IWorkbenchPreferencePage {
@@ -28,6 +32,10 @@ public class PreferencePageSide extends PreferencePage implements
 	protected Control createContents(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout());
+
+		final Label label = new Label(composite, SWT.NONE);
+		label.setLayoutData(new GridData(488, 368));
+		label.setText(Activator.Messages.getString("PreferencePageSide.1")); //$NON-NLS-1$
 
 		return composite;
 	}
