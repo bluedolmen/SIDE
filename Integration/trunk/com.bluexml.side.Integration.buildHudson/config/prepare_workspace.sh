712ffdd234ec8b4ee82c7126f62f7772404e0177
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-21 09:59:38 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@9430 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace.sh
@@ -3,12 +3,13 @@
 # 1st parameter = source workspace path
 # 2nd parameter = target workspace path
 # 3rd parameter = percent representing disk full
-if [ $# -eq 5 ]; then
+if [ $# -eq 6 ]; then
   SOURCE_WORKSPACE=$1
   TARGET_WORKSPACE=$2
   FULL=$3
   MAVEN_SOURCE=$4
   MAVEN_TARGET=$5
+  BUILD_JAR=$6
 else
   exit -2
 fi
@@ -16,8 +17,10 @@ fi
 # delete workingcopy repository
 rm -rf $TARGET_WORKSPACE/workingcopy
 rm -rf $MAVEN_TARGET
+rm -rf $BUILD_JAR
 
 mkdir $MAVEN_TARGET
+mkdir $BUILD_JAR
 
 #$DF_HOME=`df -k | grep -i /home | awk '{print $5}'|cut -d"%" -f1 |while read LINE; do
 DF_HOME=`df -k | grep -i /home | awk '{print $5}'|cut -d"%" -f1`
@@ -28,6 +31,14 @@ fi
 # prepare workspace: copy build.properties, script, jar into working space
 cp -R $SOURCE_WORKSPACE/* $TARGET_WORKSPACE
 cp -R $MAVEN_SOURCE/* $MAVEN_TARGET
+cp -R $SOURCE_WORKSPACE/../* $BUILD_JAR
+
+cd $BUILD_JAR
+rm -f buildForLinux.properties
+rm -f buildForWindows.properties
+rm -rf config
+jar cvfm Build.jar MANIFEST.MF .
+cp -f Build.jar $BUILD_JAR/..
 
 # give rights for all execute files
 cd $TARGET_WORKSPACE
