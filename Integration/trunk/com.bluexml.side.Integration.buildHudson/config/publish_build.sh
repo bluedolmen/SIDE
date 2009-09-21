7593ed64f64bc54499a4b9da43122e86bea6e127
rbaudu <rbaudu@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-21 15:01:20 +0000

    update to make it generic with remote or local copy support
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9449 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/publish_build.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/publish_build.sh
@@ -1,50 +1,47 @@
 # this script will publish the automatic build
 # parameters:
-# dest=devel or public: if devel, on secured devel update site path; public on public update site
-# rev_build_src_nb=the revision and build number to copy; this will take <src_dir>/<build_src_number>
-# src_dir=the folder where the update site is
-if [ $# -ne 3 ]; then
-  echo "Usage: publish_build.sh [devel|public] <rev_build_src_nb> <source_dir>"
-  echo "       with devel to copy on protected update_site "
-  echo "            public to copy on public update_site www.bluexml.com/update-site/SIDE/public/2.0"
-  echo "            build_src_nb the build # to copy (under stager:<src_dir>/<build_src_number>)"
-  echo "        build is copied under www.bluexml.com:/data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/$dest dir"
+# update-site_dest=the update site dir where is defined the update site on which to transfer the build; this can be a remote host and in this case the update-site must be of the form "host@update-site_dir"
+#   ATTENTION: normally, the update-site_dest must end by SIDE-Alfresco, SIDE-Alfresco-devel, SIDE-Labs or SIDE-Labs-devel: the www.bluexml.com/Update-Site/SIDE update-site SIDE-Alfresco-devel & SIDE-Labs-devel are restricted to BlueXML R&D team: they are automatically updated each time an automatic build of SIDE-Alfresco or SIDE-Labs are built; the www.bluexml.com/Update-Site/SIDE update-site SIDE-Alfresco & SIDE-Labs are public.
+# update-site_src=the folder where the local update site is; for instance, on stager it is of the form: /share/Update-Site/SIDE-Alfresco/<build #> or /share/Update-Site/SIDE-Labs/<build #>, this update-site being automatically created by the Hudson nighlty build.
+if [ $# -ne 2 ]; then
+  echo "Usage: publish_build.sh <update_site_dest> <update_site_src>"
+  echo "       with update_site_dest the directory of the destination update site like www.bluexml.com:/data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/SIDE-Alfresco-devel where to copy "
+  echo "            update_site_src the directory of the source update site like /share/Update-Site/SIDE-Alfresco/<build #> from which to copy "
+  echo " Example: "
+  echo "    transfert on remote update-site:"
+  echo "      publish_build.sh /home/stager/share/Update-Site/SIDE-Alfresco/Ankle/9441-651 root@www.bluexml.com:/data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/SIDE-Alfresco-devel/9441-651"
+  echo "    transfert on local update-site:"
+  echo "      publish_build.sh /home/stager/share/Update-Site/SIDE-Alfresco/Ankle/9441-651 /data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/SIDE-Alfresco-devel/9441-651"
   exit -2
 else
-  dest=$1
-  rev_build_src_nb=$2
-  src_dir=$3
-  # set ssh-agent env var to copy on gimly www.bluexml.com
-  ssh_agent_pid=`ps -C ssh-agent -o pid=`
-  ssh_agent_ppid=`ps -C ssh-agent -o ppid=`
-  if [ $ssh_agent_pid -gt 0 ];then
-     ssh_agent_pid=`echo $ssh_agent_pid | sed -e 's/^ *//' -e 's/ *$//'`
-     ssh_agent_ppid=`echo $ssh_agent_ppid | sed -e 's/^ *//' -e 's/ *$//'`
-     ssh_agent_dir=`ls /tmp|grep ssh-|grep $ssh_agent_ppid`
-     SSH_AUTH_SOCK=/tmp/$ssh_agent_dir/agent.$ssh_agent_ppid; export SSH_AUTH_SOCK;
-      SSH_AGENT_PID=$ssh_agent_pid; export SSH_AGENT_PID;
-  else
-     echo " ERROR: ssh-agent is not started; unable to publish local update site on www.bluexml.com update site"
-     exit -1
-  fi
-  if [ $dest = "devel" ] || [ $dest = "public" ]; then
-    if [ ! -d $src_dir/$rev_build_src_nb ]; then
-      echo " ERROR: $src_dir/$rev_build_src_nb does not exist: no build $rev_build_src_nb "
-      echo " No build publication performed"
-      echo " build number must be one of : "
-      ls $src_dir
-      exit -1
-    else
-      if [ $dest = "public" ]; then
-        scp -i /home/stager/.ssh/id_rsa_gimli -r $src_dir/$rev_build_src_nb/* root@www.bluexml.com:/data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/$dest/2.0
-      else
-        scp -i /home/stager/.ssh/id_rsa_gimli -r $src_dir/$rev_build_src_nb root@www.bluexml.com:/data/www/virtuals/b/l/u/bluexml.com/www/html/static/update-site/SIDE/$dest
-      fi
-    fi
-  else
-    echo " ERROR: first parameter must be devel or public. enter publish_build for usage"
-    echo " No build publication performed"
+  dest_dir=$1
+  src_dir=$2
+  if [ ! -d $src_dir ]; then
+    echo " ERROR: $src_dir does not exist locally "
+    echo "        No build publication performed"
     exit -1
+  else
+    # search for the @ character toknow if remote or local site; if @ presente, remote site: it is necessary to perform an scp
+    # and before to ensure that ssh agent is running and to set ssh environment
+    if_remote=`echo $dest_dir | cut -f2- -d\ | sed -e 's/./& /g' | grep -n "@" | cut -f1 -d:`
+    if [ $if_remote -eq 1 ]; then
+      # set ssh-agent env var to copy on gimly www.bluexml.com
+      ssh_agent_pid=`ps -C ssh-agent -o pid=`
+      ssh_agent_ppid=`ps -C ssh-agent -o ppid=`
+      if [ $ssh_agent_pid -gt 0 ];then
+       ssh_agent_pid=`echo $ssh_agent_pid | sed -e 's/^ *//' -e 's/ *$//'`
+       ssh_agent_ppid=`echo $ssh_agent_ppid | sed -e 's/^ *//' -e 's/ *$//'`
+       ssh_agent_dir=`ls /tmp|grep ssh-|grep $ssh_agent_ppid`
+       SSH_AUTH_SOCK=/tmp/$ssh_agent_dir/agent.$ssh_agent_ppid; export SSH_AUTH_SOCK;
+       SSH_AGENT_PID=$ssh_agent_pid; export SSH_AGENT_PID;
+      else
+       echo " ERROR: ssh-agent is not started; unable to publish local update site on remote update site"
+       exit -1
+      fi
+      scp -i ~/.ssh/id_rsa_gimli -r $src_dir/* $dest_dir
+    else
+      cp -r $src_dir/* $dest_dir
+    fi
   fi
 fi
 
