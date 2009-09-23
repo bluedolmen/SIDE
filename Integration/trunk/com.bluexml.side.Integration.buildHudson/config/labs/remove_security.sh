f9d27c0c6c7b02f6f8c245fc1d663ca9e2626503
rbaudu <rbaudu@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-23 09:48:38 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@9665 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/labs/remove_security.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/labs/remove_security.sh
@@ -1,10 +1,30 @@
+#! /bin/bash
+# After preparation of the directory where to build the SIDE Labs release, 
+# this script updates the source of SIDE to keep only what is useful for the Labs release.
+# In particular, all what is License management is removed.
+# 1st parameter = SIDE source directory
+# 2nd parameter = Build directory
+if [ $# -eq 2 ]; then
+  SOURCE_PATH=$1
+  BUILD_PATH=$2
+else
+  echo "Usage: remove_security.sh SOURCE_PATH BUILD_PATH"
+  echo "       with SOURCE_PATH =  source workspace path of SIDE"
+  echo "            BUILD_PATH =  directory where the build will be performed"
+  exit -1
+fi
+cd $BUILD_PATH
+# remove projects not to integrate in labs release
+rm -rf $SOURCE_PATH/S-IDE/Integration
+rm -rf $SOURCE_PATH/S-IDE/Util/trunk/com.bluexml.side.Util.security
+rm -rf $SOURCE_PATH/S-IDE/Experimental
 
 # delete svn folder
-for f in `find $1 -type d -name ".svn"`; do
+for f in `find $SOURCE_PATH -type d -name ".svn"`; do
 	rm -rf $f
 done
 
-for f in `find $1 -type f -name "*.java"`; do
+for f in `find $SOURCE_PATH -type f -name "*.java"`; do
 	
 	# find the text and replace it
 	perl -p -i -e 's/com.bluexml.side.util.security.preferences.SWTResourceManager/com.swtdesigner.SWTResourceManager/g' $f
@@ -40,21 +60,33 @@ for f in `find $1 -type f -name "*.java"`; do
 	fi
 done
 
-for f in `find $1 -type f -name "feature.xml"`; do
+for f in `find $SOURCE_PATH -type f -name "feature.xml"`; do
 
 	perl -0 -p -e 's/( *)<plugin( *)(\s+)( *)id="com.bluexml.side.Util.security"[^<]*//sg' $f
 done
 
-for f in `find $1 -type f -name "*.xml"`; do
-	
-	
+for f in `find $SOURCE_PATH -type f -name "*.xml"`; do
 	# delete line having the pattern 'com.bluexml.side.Util.security'
 	perl -ni -e 'print unless /com.bluexml.side.Util.security/' $f
 done
 
-for f in `find $1 -type f -name "*.MF"`; do
-	
-	
+for f in `find $SOURCE_PATH -type f -name "*.MF"`; do
 	# delete line having the pattern 'com.bluexml.side.Util.security'
 	perl -ni -e 'print unless /com.bluexml.side.Util.security/' $f
 done
+
+# modify header of the source file with license mention and copyright using the openSourcePublication project
+cd $BUILD_PATH
+mkdir buildLicense
+cp -R $SOURCE_PATH/SIDE/Integration/trunk/com.bluexml.side.Integration.openSourcePublication $BUILD_PATH/buildLicense
+cd $BUILD_PATH/buildLicense
+mkdir bin
+ant main
+if [ -f openSourceLicenseHeader.jar ]; then
+  cd ../labs 
+  java -jar ../buildLicense/openSourceLicenseHeader.jar $SOURCE_PATH
+else
+ echo "Unable to build the jar file to change the License and copyright header in source files!"
+ exit -2
+fi
+
--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/prepare_workspace.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# first test disk space: if full, stop build
-# 1st parameter = source workspace path
-# 2nd parameter = target workspace path
-# 3rd parameter = percent representing disk full
+# Prepare the directory where to build the SIDE release
+# 1st parameter = source workspace path of the buildHuson project
+# 2nd parameter = target workspace path of the buildHuson project where the build will be performed
+# 3rd parameter = percent representing disk full; if not enough disk space (more thant this value), build is stopped
+# 4th parameter = Integration module source directory for Maven
+# 5th parameter = Maven working dir
+# 6th parameter = directory where to build the build.jar of the build process com.bluexml.side.integration.buildHudson
 if [ $# -eq 6 ]; then
   SOURCE_WORKSPACE=$1
   TARGET_WORKSPACE=$2
@@ -11,6 +14,13 @@ if [ $# -eq 6 ]; then
   MAVEN_TARGET=$5
   BUILD_JAR=$6
 else
+  echo "Usage: prepare_workspace.sh SOURCE_WORKSPACE TARGET_WORKSPACE FULL MAVEN_SOURCE MAVEN_TARGET BUILD_JAR"
+  echo "       with SOURCE_WORKSPACE =  source workspace path of the buildHuson project"
+  echo "            TARGET_WORKSPACE =  target workspace path of the buildHuson project where the build will be performed"
+  echo "            FULL =  percent representing disk full; if not enough disk space (more thant this value), build is stopped"
+  echo "            MAVEN_SOURCE =  Integration module source directory for Maven"
+  echo "            MAVEN_TARGET =  Maven working dir"
+  echo "            BUILD_JAR =  directory where to build the build.jar of the build process com.bluexml.side.integration.buildHudson "
   exit -2
 fi
 
@@ -36,15 +46,16 @@ cp -R $SOURCE_WORKSPACE/../* $BUILD_JAR
 cd $BUILD_JAR
 mkdir bin
 ant main
-cp -f Build.jar $BUILD_JAR/..
+if [ -f Build.jar ]; then
+  cp -f Build.jar $TARGET_WORKSPACE
+else
+ echo "Unable to build the jar file to compile all the SIDE source!"
+ exit -3
+fi
 
 # give rights for all execute files
 cd $TARGET_WORKSPACE
-chown -R stager test_generator.sh
-chown -R stager docbook.sh
-chown -R stager Gendoc.jar
-
-chmod +x test_generator.sh
-chmod +x docbook.sh
-chmod +x Gendoc.jar
+chmod +x -R *.sh
+chmod +x -R *.jar
+dos2unix *.sh
 exit 0
