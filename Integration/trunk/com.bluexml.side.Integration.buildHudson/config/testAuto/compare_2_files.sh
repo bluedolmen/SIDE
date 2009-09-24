f04d45465b341f190a92eb63be34ea62f5b6c13c
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-24 09:25:30 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@9696 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/testAuto/compare_2_files.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/testAuto/compare_2_files.sh
@@ -2,11 +2,11 @@
 
 sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $1 > res1.xml
 sed 's/<date>.*date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res1.xml > res11.xml
-sed 's/date=\".*\" \/>/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/>/' res11.xml > res111.xml
+sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/><\/de/' res11.xml > res111.xml
 
 sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $2 > res2.xml
 sed 's/<date>.*<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res2.xml > res22.xml
-sed 's/date=\".*\" \/>/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/>/' res22.xml > res222.xml
+sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/><\/de/' res22.xml > res222.xml
 
 
 ./diffxml/diffxml.sh -q res111.xml res222.xml > out.xml 
@@ -22,9 +22,9 @@ else
         echo "empty"
 fi
 
-rm res1.xml
-rm res11.xml
-rm res111.xml
+#rm res1.xml
+#rm res11.xml
+#rm res111.xml
 rm res2.xml
 rm res22.xml
-rm res222.xml
+#rm res222.xml
