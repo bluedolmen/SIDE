5f4257bf74b317dd4e138e9ec1397388fcf578ea
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-24 16:24:42 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1107
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9727 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Class/trunk/com.bluexml.side.Class.service.alfresco/src/com/bluexml/side/clazz/service/alfresco/AttributeServices.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * 	Copyright (C) BlueXML 2005-2008
+ *
+ * This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Boston, MA 02111.
+ ******************************************************************************/
+package com.bluexml.side.clazz.service.alfresco;
+import org.eclipse.emf.ecore.EObject;
+
+import com.bluexml.side.clazz.Attribute;
+import com.bluexml.side.common.DataType;
+
+import fr.obeo.acceleo.gen.template.eval.ENode;
+
+public class AttributeServices {
+
+	public String getPropertyType(EObject node) throws Exception {
+		if (node instanceof Attribute) {
+			Attribute object = (Attribute) node;
+			if (object.getTyp() == DataType.BOOLEAN) {
+				return "d:boolean";
+			} else if (object.getTyp() == DataType.BYTE) {
+				return "d:int";
+			} else if (object.getTyp() == DataType.CHAR) {
+				return "d:text";
+			} else if (object.getTyp() == DataType.DATE) {
+				return "d:date";
+			} else if (object.getTyp() == DataType.DATE_TIME) {
+				return "d:datetime";
+			} else if (object.getTyp() == DataType.DOUBLE) {
+				return "d:double";
+			} else if (object.getTyp() == DataType.FLOAT) {
+				return "d:float";
+			} else if (object.getTyp() == DataType.INT) {
+				return "d:int";
+			} else if (object.getTyp() == DataType.LONG) {
+				return "d:long";
+			} else if (object.getTyp() == DataType.OBJECT) {
+				//return "d:content";
+				return "d:any";
+			} else if (object.getTyp() == DataType.SHORT) {
+				return "d:int";
+			} else if (object.getTyp() == DataType.STRING) {
+				return "d:text";
+			} else if (object.getTyp() == DataType.TIME) {
+				return "d:datetime";
+			}
+		}
+		throw new Exception ("node must be an attribute");
+	}
+	
+}
