d932eb5b9736dbeacfe34b74568d759a32f6b277
ecambray <ecambray@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-25 13:58:37 +0000

    First step for Outline integration in documentation.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@9774 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Form/trunk/com.bluexml.side.Form.edit.ui/src/com/bluexml/side/form/editor/views/service/OutlineViewService.java
+++ MetaModel/Form/trunk/com.bluexml.side.Form.edit.ui/src/com/bluexml/side/form/editor/views/service/OutlineViewService.java
@@ -6,17 +6,18 @@ import com.bluexml.side.form.Field;
 import com.bluexml.side.form.common.utils.FormDiagramUtils;
 
 public class OutlineViewService {
-	
+
 	protected static String nameOfSelectedForm = "";
+	protected static boolean doAll = false;
 
 	public static String getNameOfSelectedForm(EObject element) {
 		return nameOfSelectedForm;
 	}
-	
+
 	public static void setNameOfSelectedForm(String p_nameOfSelectedForm) {
 		nameOfSelectedForm = p_nameOfSelectedForm;
 	}
-	
+
 	/**
 	 * Will search the field to know if this field have been virtualized
 	 * @param element
@@ -28,5 +29,13 @@ public class OutlineViewService {
 			isVirtualized = FormDiagramUtils.IsVirtualized(f);
 		}
 		return isVirtualized;
+	}
+
+	public static boolean isDoAll() {
+		return doAll;
+	}
+
+	public static void setDoAll(boolean doAll) {
+		OutlineViewService.doAll = doAll;
 	}
 }
--- MetaModel/Form/trunk/com.bluexml.side.Form.generator.documentation/src/com/bluexml/side/form/generator/documentation/FormDocumentationGenerator.java
+++ MetaModel/Form/trunk/com.bluexml.side.Form.generator.documentation/src/com/bluexml/side/form/generator/documentation/FormDocumentationGenerator.java
@@ -1,8 +1,13 @@
 package com.bluexml.side.form.generator.documentation;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
+
+import com.bluexml.side.form.editor.views.service.OutlineViewService;
 import com.bluexml.side.util.generator.documentation.DocumentationGenerator;
+import com.bluexml.side.util.generator.documentation.services.DocumentationServices;
 
 public class FormDocumentationGenerator extends DocumentationGenerator {
 
@@ -14,5 +19,16 @@ public class FormDocumentationGenerator extends DocumentationGenerator {
 	protected List<String> getTemplates() {
 		templates.add("/com.bluexml.side.Form.generator.documentation/src/templates/content.mt"); //$NON-NLS-1$
 		return templates;
+	}
+
+	public Collection<IFile> generate(IFile model) throws Exception {
+		OutlineViewService.setDoAll(true);
+		//TODO : lancer la génération de la vue outline (avec le doAll à true)
+		// placer le fichier html ainsi généré dans un répertoire fixé (ex : doc/html)
+		// appeller l'ajout du chemin relatif
+		// faire en sorte que (comme pour les diagrammes) on face le lien dans le fichier odt vers ce fichier
+		//DocumentationServices.addOutlineRelativePath();
+		OutlineViewService.setDoAll(false);
+		return super.generate(model);
 	}
 }
