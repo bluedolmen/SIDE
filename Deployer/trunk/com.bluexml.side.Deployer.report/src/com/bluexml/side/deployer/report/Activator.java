174c1a966cac8058320e717fe36efaa6e8fa89a9
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-29 12:51:00 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1140
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10025 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/Activator.java
+++ Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/Activator.java
@@ -11,7 +11,7 @@ import com.bluexml.side.util.libs.Messages;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.bluexml.side.Deployer.facetMap";
+	public static final String PLUGIN_ID = "com.bluexml.side.Deployer.report";
 	public static final Messages Messages = new Messages(PLUGIN_ID, "com.bluexml.side.deployer.report.messages");
 	// The shared instance
 	private static Activator plugin;
