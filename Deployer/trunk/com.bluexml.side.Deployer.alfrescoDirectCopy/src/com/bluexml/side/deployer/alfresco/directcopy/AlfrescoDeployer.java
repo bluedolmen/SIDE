0564fef80049db0a2e6bbe8dc8cff3dc4e392f56
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-09-30 12:53:39 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1159
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10063 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.alfrescoDirectCopy/src/com/bluexml/side/deployer/alfresco/directcopy/AlfrescoDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.alfrescoDirectCopy/src/com/bluexml/side/deployer/alfresco/directcopy/AlfrescoDeployer.java
@@ -7,7 +7,7 @@ import com.bluexml.side.util.deployer.AbstractMultiDeployer;
 public class AlfrescoDeployer extends AbstractMultiDeployer {
 
 	public AlfrescoDeployer() {
-		this.cleanKey = "com.bluexml.side.deployer.alfresco.clean";
+		this.cleanKey = "com.bluexml.side.deployerDirect.alfresco.clean";
 		this.logChanges = "com.bluexml.side.deployer.alfresco.logChanges";
 
 		AlfrescoAmpDirectDeployer ampDep = new AlfrescoAmpDirectDeployer();
