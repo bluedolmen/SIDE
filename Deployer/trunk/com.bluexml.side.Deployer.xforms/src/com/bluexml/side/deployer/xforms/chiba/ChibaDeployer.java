4103bb2148c361af0b33c33ed1841c7ec5bcf132
avoglozin <avoglozin@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-09 14:49:37 +0000

    Added support for customized webapp name.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10334 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.xforms/src/com/bluexml/side/deployer/xforms/chiba/ChibaDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.xforms/src/com/bluexml/side/deployer/xforms/chiba/ChibaDeployer.java
@@ -2,12 +2,16 @@ package com.bluexml.side.deployer.xforms.chiba;
 
 import java.io.File;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.bluexml.side.util.deployer.war.WarDeployer;
 
 public class ChibaDeployer extends WarDeployer {
 
+	private final String WEBAPP_NAME_KEY = "com.bluexml.side.Deployer.xforms.webappName";
+
 	public ChibaDeployer() {
-		super("xform");
+		super("xforms");
 		cleanKey = "com.bluexml.side.Deployer.xforms.clean";
 	}
 
@@ -18,6 +22,21 @@ public class ChibaDeployer extends WarDeployer {
 	@Override
 	protected void postProcess(File fileToDeploy) throws Exception {
 		// nothing to do
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.bluexml.side.util.deployer.war.WarDeployer#preProcess(java.io.File)
+	 */
+	@Override
+	protected void preProcess(File fileToDeploy) throws Exception {
+		super.preProcess(fileToDeploy);
+
+		String webappName = getConfigurationParameters().get(WEBAPP_NAME_KEY);
+		if (StringUtils.trimToNull(webappName) != null) {
+			setWebappName(webappName);
+		}
 	}
 
 }
