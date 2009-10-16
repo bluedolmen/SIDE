f456ccceefafa852c1382648037f4a55fd05c15f
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-16 08:44:29 +0000

    abstract deployer force class that extends hims to give mandatory attributes value (clean key ...)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10548 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.xforms/src/com/bluexml/side/deployer/xforms/chiba/ChibaDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.xforms/src/com/bluexml/side/deployer/xforms/chiba/ChibaDeployer.java
@@ -2,17 +2,12 @@ package com.bluexml.side.deployer.xforms.chiba;
 
 import java.io.File;
 
-import org.apache.commons.lang.StringUtils;
-
 import com.bluexml.side.util.deployer.war.WarDeployer;
 
 public class ChibaDeployer extends WarDeployer {
 
-	private final String WEBAPP_NAME_KEY = "com.bluexml.side.Deployer.xforms.webappName";
-
 	public ChibaDeployer() {
-		super("xforms");
-		cleanKey = "com.bluexml.side.Deployer.xforms.clean";
+		super("com.bluexml.side.Deployer.xforms.clean", null, "xforms", "com.bluexml.side.Deployer.xforms.webappName");
 	}
 
 	public boolean check() {
@@ -21,22 +16,8 @@ public class ChibaDeployer extends WarDeployer {
 
 	@Override
 	protected void postProcess(File fileToDeploy) throws Exception {
-		// nothing to do
-	}
+		// TODO Auto-generated method stub
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.bluexml.side.util.deployer.war.WarDeployer#preProcess(java.io.File)
-	 */
-	@Override
-	protected void preProcess(File fileToDeploy) throws Exception {
-		super.preProcess(fileToDeploy);
-
-		String webappName = getConfigurationParameters().get(WEBAPP_NAME_KEY);
-		if (StringUtils.trimToNull(webappName) != null) {
-			setWebappName(webappName);
-		}
 	}
 
 }
