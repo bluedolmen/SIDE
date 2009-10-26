e7fa85a23f5c3f97cf480339cbbf8c6e6e1b6142
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-26 15:11:28 +0000

    throw Exception if acceleo generator create log file
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10742 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Util/trunk/com.bluexml.side.Util.generator/src/com/bluexml/side/util/generator/GeneratorException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bluexml.side.util.generator;
+
+/**
+ * @author davidabad
+ *
+ */
+public class GeneratorException extends Exception {
+
+	public GeneratorException(String arg0) {
+		super(arg0);
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6700605659355511096L;
+
+}
