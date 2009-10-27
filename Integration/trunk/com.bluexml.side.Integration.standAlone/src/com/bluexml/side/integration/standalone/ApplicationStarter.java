c35bf4ab21ff5d127fcd6f7ba1613b8978f3afe8
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-27 17:05:08 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1243
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10796 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Integration/trunk/com.bluexml.side.Integration.standAlone/src/com/bluexml/side/integration/standalone/ApplicationStarter.java
@@ -0,0 +1,217 @@
+package com.bluexml.side.integration.standalone;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+import com.bluexml.side.application.Application;
+import com.bluexml.side.application.ApplicationPackage;
+import com.bluexml.side.application.Configuration;
+import com.bluexml.side.application.Model;
+import com.bluexml.side.application.ui.action.ApplicationDialog;
+import com.bluexml.side.application.ui.action.utils.ApplicationUtil;
+import com.bluexml.side.application.ui.action.utils.Generate;
+import com.bluexml.side.util.componentmonitor.headless.FormTextHeadless;
+import com.bluexml.side.util.componentmonitor.headless.LabelHeadLess;
+import com.bluexml.side.util.componentmonitor.headless.StyledTextHeadless;
+import com.bluexml.side.util.componentmonitor.headless.progressBarHeadLess;
+
+public class ApplicationStarter implements IApplication {
+	static final String CONFIGURATION_KEY = "configuration";
+	static final String MODELS_KEY = "models";
+
+	protected String[] arguments;
+
+	public Object start(IApplicationContext context) throws Exception {
+
+		arguments = (String[]) context.getArguments().get("application.args");
+
+		System.out.println("Start !!!!!!!!!!");
+
+		if (!arguments[0].toString().contains(".application")) {
+			File root = new File(arguments[0]);
+			String[] extensions = { "application" };
+			boolean recursive = true;
+			try {
+				Collection files = FileUtils.listFiles(root, extensions, recursive);
+
+				for (Iterator iterator = files.iterator(); iterator.hasNext();) {
+					File file = (File) iterator.next();
+					System.out.println("File = " + file.getAbsolutePath());
+					File fileAP = new File(file.getAbsolutePath());
+					System.out.println("file.exists(): " + fileAP.exists());
+					long time1 = System.currentTimeMillis();
+					Generate gen = new Generate();
+					Map<String, Object> conf = loadConfiguration(fileAP, arguments[1]);
+					System.out.println("created, let's run");
+					gen.run(
+							(Configuration)conf.get(CONFIGURATION_KEY),
+							(List<Model>)conf.get(MODELS_KEY),
+							 new progressBarHeadLess(), new LabelHeadLess(),
+							 new progressBarHeadLess(), new LabelHeadLess(),
+							 new StyledTextHeadless(), new FormTextHeadless()
+					);
+					long time2 = System.currentTimeMillis() - time1;
+					System.out.println("Time " + Long.toString(time2 / 1000));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// Start Generation
+		} else {
+			// System.out.println("Start Generation");
+
+			// File file = new
+			// File("workspaceStandAlone/StandAlone/models/My.application");
+
+			File file = new File(arguments[0]);
+			// System.out.println("file.exists(): " + file.exists());
+			long time1 = System.currentTimeMillis();
+			Generate gen = new Generate();
+			Map<String, Object> conf = loadConfiguration(file, arguments[1]);
+
+			gen.run(
+					(Configuration)conf.get(CONFIGURATION_KEY), 
+					(List<Model>)conf.get(MODELS_KEY),
+					 new progressBarHeadLess(), new LabelHeadLess(),
+					 new progressBarHeadLess(), new LabelHeadLess(),
+					 new StyledTextHeadless(), new FormTextHeadless()
+			);
+			long time2 = System.currentTimeMillis() - time1;
+			System.out.println("Time " + Long.toString(time2 / 1000));
+		}
+
+		System.out.println("End Generation");
+
+		// System.out.println("END !!!!!!!!!!");
+
+		return EXIT_OK;
+	}
+
+	public void stop() {
+		// nothing to do
+
+	}
+
+	protected Map<String,Object> loadConfiguration(File filePath, String name) {
+		Map<String,Object> extractedConfiguration = new HashMap<String, Object>();
+		
+		System.out.println("Start Generate with filePath= " + filePath + " & Name: " + name);
+
+		// Create the IFile
+		IFile file = null;
+		try {
+			// IWorkspace ws = ResourcesPlugin.getWorkspace();
+			// IProject project= ws.getRoot().getProject("StandAlone");
+			// if (!project.exists())
+			// project.create(null);
+			// if (!project.isOpen())
+			// project.open(null);
+			// IPath location = new Path(filePath.getAbsolutePath());
+
+			// file = project.getFile(location.lastSegment());
+
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			System.out.println("workspace: " + workspace);
+			IPath location = Path.fromOSString(filePath.getAbsolutePath());
+			System.out.println("location: " + location);
+			file = workspace.getRoot().getFileForLocation(location);
+			System.out.println("file: " + file);
+
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		System.out.println("getWorkspace: " + ResourcesPlugin.getWorkspace());
+		System.out.println("getRoot: " + ResourcesPlugin.getWorkspace().getRoot().exists() + " -> " + ResourcesPlugin.getWorkspace().getRoot());
+		System.out.println("getPath: " + ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(filePath.getAbsolutePath())));
+
+		System.out.println("\tfile.exists(): " + file.exists());
+
+		URI uri = null;
+		System.out.println("\tURI");
+		try {
+
+			System.out.println("\tgetRawLocation: " + file.getRawLocation());
+			System.out.println("\ttoFile: " + file.getRawLocation().toFile());
+			System.out.println("\tpath: " + file.getRawLocation().toFile().getPath());
+
+			String absolutePath = file.getRawLocation().toFile().getAbsolutePath();
+			System.out.println("\tabsolutePath: " + absolutePath);
+
+			uri = URI.createFileURI(new File(absolutePath).getAbsolutePath());
+
+			System.out.println("URI: " + uri);
+
+		} catch (Exception e) {
+			System.out.println("Exception : " + e.getClass());
+		}
+
+		// System.out.println("\tXMI");
+		XMIResource resource = new XMIResourceImpl(uri);
+		// System.out.println("\tFILE INPUT");
+
+		FileInputStream fi = null;
+		try {
+			fi = new FileInputStream(file.getRawLocation().toFile());
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("\tMAP");
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		map.put(ApplicationPackage.eINSTANCE.getNsURI(), ApplicationPackage.eINSTANCE);
+		map.put(XMLResource.OPTION_SCHEMA_LOCATION_IMPLEMENTATION, Boolean.TRUE);
+
+		System.out.println("\tLOAD");
+		try {
+			resource.load(fi, map);
+		} catch (IOException e) {
+			System.out.println("Exception  " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e1) {
+			System.out.println("Exception  " + e1.getMessage());
+			e1.printStackTrace();
+		}
+
+		try {
+			Application application = (Application) resource.getContents().get(0);
+			System.out.println("\tapplication: " + application);
+			
+			System.out.println("\tstaticParameters: " + ApplicationDialog.staticFieldsName);
+			Configuration configuration = application.getConfiguration(name);
+			System.out.println("\tconfiguration: " + configuration);
+			List<Model> models = ApplicationUtil.getModels(application);
+			System.out.println("\tmodels: " + models);
+			extractedConfiguration.put(CONFIGURATION_KEY, configuration);
+			extractedConfiguration.put(MODELS_KEY, models);
+		} catch (java.lang.NullPointerException e) {
+			System.out.println("NullPointerException  " + e.getMessage());
+			e.printStackTrace();
+		} catch (Exception e1) {
+			System.out.println("Exception  " + e1.getMessage());
+			e1.printStackTrace();
+		}
+		System.out.println("### Generate Done");
+		return extractedConfiguration;
+	}
+}
