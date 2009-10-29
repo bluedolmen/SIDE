87246fd59b8957cdbf278c84136c011a1918b060
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-29 11:20:42 +0000

    import finalized
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10821 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillContent.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillContent.java
@@ -5,14 +5,21 @@ package com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.dictionary.TypeDefinition;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.QName;
 
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.AttributeContentException;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.DuplicateInputPdfException;
@@ -23,6 +30,7 @@ import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.Missi
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.MissingOutputContentException;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.NoContentException;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.NoPdfFileException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.OutputTypeKeyException;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.extract.ExtractDataFromPDF;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.fill.FillDataContent;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.ConstantsLanguage;
@@ -43,12 +51,71 @@ public class FillContent {
 		this.serviceRegistry = serviceRegistry;
 	}
 
-	public void execute(Map<String, String> commands) throws DuplicateInputPdfException, MissingInputPdfKeyException, IOException, NoPdfFileException, DuplicateOutputContentException, MissingOutputContentException, NoContentException, InvalidValueOfParameterException, AttributeContentException, InvalidAssociationException {
+	public void execute(Map<String, String> commands) throws DuplicateInputPdfException, MissingInputPdfKeyException, 
+	                                                         IOException, NoPdfFileException, DuplicateOutputContentException, 
+	                                                         MissingOutputContentException, NoContentException, 
+	                                                         InvalidValueOfParameterException, AttributeContentException, 
+	                                                         InvalidAssociationException, OutputTypeKeyException {
 		PdfReader reader = openAlfrescoPdf(commands);
 		HashMap<String,String> data = ExtractDataFromPDF.extractDataFromPDF(reader);
 		NodeRef content = getContent(commands);
+		if (content == null){
+			content = createContent(commands);
+		}
 		HashMap<String,String> importCommands = LanguageMethods.getPdfToContentCommands(commands);
 		FillDataContent.fillContent(serviceRegistry,content,importCommands,data);
+	}
+
+	private NodeRef createContent(Map<String, String> commands) throws NoContentException, OutputTypeKeyException {
+		String outputTypeValue = commands.get(ConstantsLanguage.OUTPUT_TYPE_CONTENT_KEY);
+		String[] contentTypeInfos = null;
+		if (outputTypeValue != null){
+			contentTypeInfos = outputTypeValue.split(ConstantsLanguage.OUTPUT_TYPE_SEPARATOR);
+		}
+		else{
+			throw new OutputTypeKeyException(OutputTypeKeyException.DOES_NOT_EXISTS);
+		}
+		String uri = getUriModel(contentTypeInfos[0]);
+		QName type = QName.createQName(uri,contentTypeInfos[1]);
+		QName assoc = QName.createQName(uri,"contains");
+		NodeRef parent = getFolderContentParent(commands);
+		TypeDefinition typeDef = serviceRegistry.getDictionaryService().getType(type);
+		Map<QName,Serializable> propertyName = FillDataContent.createNameProperty(typeDef);
+		ChildAssociationRef assocRef = serviceRegistry.getNodeService().createNode(parent, ContentModel.ASSOC_CONTAINS, assoc, type, propertyName);
+		return assocRef.getChildRef();
+	}
+
+	private NodeRef getFolderContentParent(Map<String, String> commands) throws NoContentException {
+		NodeRef parentFolder = null;
+		Set<String> keys = commands.keySet();
+		if (keys.contains(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1])){
+			parentFolder = getParentFolderByPath(commands.get(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1]));
+		}
+		else if (keys.contains(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0])){
+			//we can't find anything
+		}
+		return parentFolder;
+	}
+
+	private NodeRef getParentFolderByPath(String path) throws NoContentException {
+		String[] pathSteps = path.split("/");
+		StringBuffer parentPath = new StringBuffer();
+		for (int i = 1; i < pathSteps.length-1; i++){
+			parentPath.append("/");
+			parentPath.append(pathSteps[i]);
+		}
+		return getContentByPath(parentPath.toString());
+	}
+
+	private String getUriModel(String prefix) {
+		String uri = null;
+		Collection<QName> models = serviceRegistry.getDictionaryService().getAllModels();
+		for (QName model : models) {
+			if (model.getNamespaceURI().contains(prefix)){
+				uri = model.getNamespaceURI();
+			}
+		}
+		return uri;
 	}
 
 	private NodeRef getContent(Map<String, String> commands) throws DuplicateOutputContentException, MissingOutputContentException, NoContentException {
@@ -75,9 +142,9 @@ public class FillContent {
 		if (nodeRefSet.length() == 1) {
 			content = nodeRefSet.getNodeRefs().get(0);
 		}
-		else{
-			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
-		}
+//		else{
+//			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
+//		}
 		return content;
 	}
 
@@ -87,9 +154,9 @@ public class FillContent {
 		if (nodeRefSet.length() == 1) {
 			content = nodeRefSet.getNodeRefs().get(0);
 		}
-		else{
-			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
-		}
+//		else{
+//			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
+//		}
 		return content;
 	}
 
