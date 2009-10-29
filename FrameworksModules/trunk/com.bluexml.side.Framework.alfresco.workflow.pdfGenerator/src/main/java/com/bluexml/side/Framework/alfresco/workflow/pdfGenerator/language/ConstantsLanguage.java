283228ee24a0d30332e186eb258ac1a448e32ca0
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-29 11:21:10 +0000

    import finalized
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10823 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/language/ConstantsLanguage.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/language/ConstantsLanguage.java
@@ -16,13 +16,14 @@ public class ConstantsLanguage {
 	public static final String[] ACTION_VALUES = {"IMPORT_FILLED_PDF","EXPORT_FILLED_PDF"};
 	
 	public static final String[] INPUT_PDF_KEYS = {"parameter.pdf.uuid","parameter.pdf.path"};
-	
 	public static final String[] OUTPUT_CONTENT_KEYS = {"parameter.output.uuid","parameter.output.path"};
+	public static final String OUTPUT_TYPE_CONTENT_KEY = "parameter.output.type";
 	
 	public static final String PDF_PARAMETER_PREFIX = "field";
 	public static final String NAVIGATION_INDICATOR = "->";
 	public static final String ATTRIBUTE_INDICATOR = "\"";	
 	public static final String DATE_VALUE_INDICATOR = "/";
 	public static final String[] BOOLEAN_VALUES = {"Yes","No"};
+	public static final String OUTPUT_TYPE_SEPARATOR = " : ";
 
 }
