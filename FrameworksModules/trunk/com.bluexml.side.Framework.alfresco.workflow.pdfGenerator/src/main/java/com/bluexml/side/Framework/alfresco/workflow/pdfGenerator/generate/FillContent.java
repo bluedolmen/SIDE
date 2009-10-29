16d488182e63343719ee4f9c644a9aef658f6814
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-10-29 16:15:04 +0000

    extract data from content
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10837 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillContent.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillContent.java
@@ -35,6 +35,7 @@ import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.extrac
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.fill.FillDataContent;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.ConstantsLanguage;
 import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.LanguageMethods;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.structure.AlfrescoStructure;
 import com.lowagie.text.pdf.PdfReader;
 
 /**
@@ -42,8 +43,6 @@ import com.lowagie.text.pdf.PdfReader;
  *
  */
 public class FillContent {
-	
-	private static final String LUCENE_SEARCH_QUERY_INDICATOR = "ID:";
 
 	private ServiceRegistry serviceRegistry;
 
@@ -56,13 +55,13 @@ public class FillContent {
 	                                                         MissingOutputContentException, NoContentException, 
 	                                                         InvalidValueOfParameterException, AttributeContentException, 
 	                                                         InvalidAssociationException, OutputTypeKeyException {
-		PdfReader reader = openAlfrescoPdf(commands);
-		HashMap<String,String> data = ExtractDataFromPDF.extractDataFromPDF(reader);
-		NodeRef content = getContent(commands);
+		PdfReader reader = AlfrescoStructure.openAlfrescoPdf(commands);
+		HashMap<String,String> data = ExtractDataFromPDF.extractData(reader);
+		NodeRef content = AlfrescoStructure.getContent(commands,ConstantsLanguage.OUTPUT_CONTENT_KEYS);
 		if (content == null){
 			content = createContent(commands);
 		}
-		HashMap<String,String> importCommands = LanguageMethods.getPdfToContentCommands(commands);
+		HashMap<String,String> importCommands = LanguageMethods.getScriptCommands(commands);
 		FillDataContent.fillContent(serviceRegistry,content,importCommands,data);
 	}
 
@@ -104,7 +103,7 @@ public class FillContent {
 			parentPath.append("/");
 			parentPath.append(pathSteps[i]);
 		}
-		return getContentByPath(parentPath.toString());
+		return AlfrescoStructure.getContentByPath(parentPath.toString());
 	}
 
 	private String getUriModel(String prefix) {
@@ -116,94 +115,6 @@ public class FillContent {
 			}
 		}
 		return uri;
-	}
-
-	private NodeRef getContent(Map<String, String> commands) throws DuplicateOutputContentException, MissingOutputContentException, NoContentException {
-		NodeRef content = null;
-		if (commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0]) && commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1])){
-			throw new DuplicateOutputContentException(DuplicateOutputContentException.DUPLICATE_OUTPUT_CONTENT_KEY);
-		}
-		else if (!commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0]) && !commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1])){
-			throw new MissingOutputContentException(MissingOutputContentException.MISSING_OUTPUT_CONTENT_KEY);
-		}
-		else if (!commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0]) && commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1])){
-			content = getContentByPath(commands.get(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1]));
-		}
-		else if (commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0]) && !commands.containsKey(ConstantsLanguage.OUTPUT_CONTENT_KEYS[1])){
-			content = getContentByUUID(commands.get(ConstantsLanguage.OUTPUT_CONTENT_KEYS[0]));
-		}
-		return content;
-	}
-
-	private NodeRef getContentByUUID(String UUIDContent) throws NoContentException {
-		NodeRef content = null;
-		String query = LUCENE_SEARCH_QUERY_INDICATOR+"\""+StoreRef.STORE_REF_WORKSPACE_SPACESSTORE+"/"+UUIDContent+"\"";
-		ResultSet nodeRefSet = serviceRegistry.getSearchService().query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,SearchService.LANGUAGE_LUCENE,query);
-		if (nodeRefSet.length() == 1) {
-			content = nodeRefSet.getNodeRefs().get(0);
-		}
-//		else{
-//			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
-//		}
-		return content;
-	}
-
-	private NodeRef getContentByPath(String pathContent) throws NoContentException {
-		NodeRef content = null;
-		ResultSet nodeRefSet = serviceRegistry.getSearchService().query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,SearchService.LANGUAGE_XPATH,pathContent);
-		if (nodeRefSet.length() == 1) {
-			content = nodeRefSet.getNodeRefs().get(0);
-		}
-//		else{
-//			throw new NoContentException(NoContentException.CONTENT_DOES_NOT_EXISTS);
-//		}
-		return content;
-	}
-
-	private PdfReader openAlfrescoPdf(Map<String, String> commands) throws DuplicateInputPdfException, MissingInputPdfKeyException, IOException, NoPdfFileException {
-		PdfReader reader = null;
-		if (commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[0]) && commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[1])){
-			throw new DuplicateInputPdfException(DuplicateInputPdfException.DUPLICATE_INPUT_PDF_KEY);
-		}
-		else if (!commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[0]) && !commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[1])){
-			throw new MissingInputPdfKeyException(MissingInputPdfKeyException.MISSING_INPUT_PDF_KEY);
-		}
-		else if (!commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[0]) && commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[1])){
-			reader = getJavaPathFromAlfrescoPathToPdf(commands.get(ConstantsLanguage.INPUT_PDF_KEYS[1]));
-		}
-		else if (commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[0]) && !commands.containsKey(ConstantsLanguage.INPUT_PDF_KEYS[1])){
-			reader = getJavaPathFromAlfrescoUUIDToPdf(commands.get(ConstantsLanguage.INPUT_PDF_KEYS[0]));
-		}
-		return reader;
-	}
-
-	private PdfReader getJavaPathFromAlfrescoUUIDToPdf(String alfrescoUUID) throws IOException, NoPdfFileException {
-		PdfReader reader = null;
-		String query = LUCENE_SEARCH_QUERY_INDICATOR+"\""+StoreRef.STORE_REF_WORKSPACE_SPACESSTORE+"/"+alfrescoUUID+"\"";
-		ResultSet nodeRefSet = serviceRegistry.getSearchService().query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,SearchService.LANGUAGE_LUCENE,query);
-		if (nodeRefSet.length() == 1) {
-			NodeRef alfrescoPdf = nodeRefSet.getNodeRefs().get(0);
-			InputStream inputStreamOfAlfrescoPdf = serviceRegistry.getFileFolderService().getReader(alfrescoPdf).getContentInputStream();
-			reader = new PdfReader(inputStreamOfAlfrescoPdf);
-		} 
-		else {
-			throw new NoPdfFileException(NoPdfFileException.FILE_DOES_NOT_EXISTS);
-		}
-		return reader;
-	}
-
-	private PdfReader getJavaPathFromAlfrescoPathToPdf(String alfrescoPath) throws IOException, NoPdfFileException {
-		PdfReader reader = null;
-		ResultSet nodeRefSet = serviceRegistry.getSearchService().query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,SearchService.LANGUAGE_XPATH,alfrescoPath);
-		if (nodeRefSet.length() == 1) {
-			NodeRef alfrescoPdf = nodeRefSet.getNodeRefs().get(0);
-			InputStream inputStreamOfAlfrescoPdf = serviceRegistry.getFileFolderService().getReader(alfrescoPdf).getContentInputStream();
-			reader = new PdfReader(inputStreamOfAlfrescoPdf);
-		} 
-		else {
-			throw new NoPdfFileException(NoPdfFileException.FILE_DOES_NOT_EXISTS);
-		}
-		return reader;
 	}
 	
 }
--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillPDF.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/FillPDF.java
@@ -1,12 +1,46 @@
 package com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
-public class FillPDF {
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.NodeRef;
 
-	public void execute(Map<String, String> commands) {
-		// TODO Auto-generated method stub
-		
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.AttributeContentException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.DuplicateInputPdfException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.DuplicateOutputContentException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.InvalidAssociationException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.InvalidContentException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.InvalidValueOfParameterException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.MissingInputPdfKeyException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.MissingOutputContentException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.exception.NoPdfFileException;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.extract.ExtractDataFromContent;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.fill.FillDataPDF;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.ConstantsLanguage;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.LanguageMethods;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.structure.AlfrescoStructure;
+import com.lowagie.text.pdf.PdfReader;
+
+public class FillPDF {
+	
+	private ServiceRegistry serviceRegistry;
+	
+	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
+	}
+
+	public void execute(Map<String, String> commands) throws DuplicateInputPdfException, MissingInputPdfKeyException, 
+	                                                         IOException, NoPdfFileException, DuplicateOutputContentException, 
+	                                                         MissingOutputContentException, InvalidValueOfParameterException, 
+	                                                         AttributeContentException, InvalidAssociationException, 
+	                                                         InvalidContentException {
+		PdfReader reader = AlfrescoStructure.openAlfrescoPdf(commands);
+		NodeRef content = AlfrescoStructure.getContent(commands,ConstantsLanguage.INPUT_CONTENT_KEYS);
+		HashMap<String,String> exportCommands = LanguageMethods.getScriptCommands(commands);
+		HashMap<String,Object> data = ExtractDataFromContent.extractData(content,serviceRegistry,exportCommands);
+		FillDataPDF.fillPDF(serviceRegistry,reader,exportCommands,data);
 	}
 
 }
