6b355a5cf34bdc327bc52a1989e118ae1de2681e
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-02 15:47:24 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1251
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10971 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.xformsDirectCopy/src/com/bluexml/side/deployer/xformsdirectcopy/DeployerXforms.java
+++ Deployer/trunk/com.bluexml.side.Deployer.xformsDirectCopy/src/com/bluexml/side/deployer/xformsdirectcopy/DeployerXforms.java
@@ -3,6 +3,8 @@
  */
 package com.bluexml.side.deployer.xformsdirectcopy;
 
+import java.io.File;
+
 import com.bluexml.side.util.deployer.war.DirectWebAppsDeployer;
 
 /**
@@ -11,7 +13,13 @@ import com.bluexml.side.util.deployer.war.DirectWebAppsDeployer;
  */
 public class DeployerXforms extends DirectWebAppsDeployer {
 	public DeployerXforms() {
-		super("com.bluexml.side.Deployer.xformsDirectCopy.clean", "xforms", "com.bluexml.side.Deployer.xforms.webappName");
+		super("com.bluexml.side.Deployer.xformsDirectCopy.clean", "xforms", "com.bluexml.side.Deployer.xforms.webappName", "zip");
+	}
+
+	@Override
+	protected void postProcess(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
