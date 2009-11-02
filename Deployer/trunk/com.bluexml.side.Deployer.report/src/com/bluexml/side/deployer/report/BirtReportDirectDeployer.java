b8d20fc246e13b8b9b2139214ac829b32614fd12
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-02 15:47:15 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1251
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10966 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/BirtReportDirectDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.report/src/com/bluexml/side/deployer/report/BirtReportDirectDeployer.java
@@ -1,11 +1,19 @@
 package com.bluexml.side.deployer.report;
 
+import java.io.File;
+
 import com.bluexml.side.util.deployer.war.DirectWebAppsDeployer;
 
 public class BirtReportDirectDeployer extends DirectWebAppsDeployer {
 
 	public BirtReportDirectDeployer() {
-		super("com.bluexml.side.Deployer.birtReport.clean", "birt", "com.bluexml.side.Deployer.birtReport.webappName");
+		super("com.bluexml.side.Deployer.birtReport.clean", "birt", "com.bluexml.side.Deployer.birtReport.webappName", "zip");
+	}
+
+	@Override
+	protected void postProcess(File fileToDeploy) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
