384ac9a2ed8f332d976abb01e0202f95eead3612
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-02 08:33:06 +0000

    export finalized
    git-svn-id: http://svn.bluexml.com/private/S-IDE@10941 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/fill/FillDataPDF.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/generate/fill/FillDataPDF.java
@@ -3,11 +3,14 @@
  */
 package com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.generate.fill;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Set;
 
-import org.alfresco.service.ServiceRegistry;
-
-import com.lowagie.text.pdf.PdfReader;
+import com.bluexml.side.Framework.alfresco.workflow.pdfGenerator.language.ConstantsLanguage;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.AcroFields;
+import com.lowagie.text.pdf.PdfStamper;
 
 /**
  * @author dchevrier
@@ -15,9 +18,30 @@ import com.lowagie.text.pdf.PdfReader;
  */
 public class FillDataPDF {
 
-	public static void fillPDF(ServiceRegistry serviceRegistry, PdfReader reader, HashMap<String, String> exportCommands, HashMap<String, Object> data) {
-		// TODO Auto-generated method stub
-		
+	public static void fillPDF(PdfStamper stamper, HashMap<String, String> exportCommands, HashMap<String, Object> data) throws IOException, DocumentException {
+		AcroFields form = stamper.getAcroFields();
+		Set<String> exportCommandsKeys = exportCommands.keySet();
+		for (String commandKey : exportCommandsKeys) {
+			String value = convertToStringAlfrescoType(data.get(commandKey));
+			form.setField(commandKey,value);
+		}
+		stamper.close();
+	}
+
+	private static String convertToStringAlfrescoType(Object object) {
+		String value = null;
+		if (object instanceof Boolean){
+			if (object.toString().equals(Boolean.TRUE.toString())){
+				value = ConstantsLanguage.BOOLEAN_VALUES[0];
+			}
+			else{
+				value = ConstantsLanguage.BOOLEAN_VALUES[1];
+			}
+		}
+		else{
+			value = object.toString();
+		}
+		return value;
 	}
 
 }
