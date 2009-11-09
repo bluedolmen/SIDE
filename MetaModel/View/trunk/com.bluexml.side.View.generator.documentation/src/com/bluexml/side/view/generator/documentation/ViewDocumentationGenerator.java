8dc12ebb6f10ad2ade03c28be24b69c6219cb98d
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-09 13:33:55 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1262
    git-svn-id: http://svn.bluexml.com/private/S-IDE@11177 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/View/trunk/com.bluexml.side.View.generator.documentation/src/com/bluexml/side/view/generator/documentation/ViewDocumentationGenerator.java
+++ MetaModel/View/trunk/com.bluexml.side.View.generator.documentation/src/com/bluexml/side/view/generator/documentation/ViewDocumentationGenerator.java
@@ -12,6 +12,7 @@ public class ViewDocumentationGenerator extends DocumentationGenerator {
 
 	@Override
 	protected List<String> getTemplates() {
+		List<String> templates = getDefaultTemplates();
 		templates.add("/com.bluexml.side.View.generator.documentation/templates/content.mt"); //$NON-NLS-1$
 		return templates;
 	}
