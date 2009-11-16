d7997c25931c0f2c421b66818ebe5d7ad49e98b9
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-16 16:37:46 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1254
    git-svn-id: http://svn.bluexml.com/private/S-IDE@11296 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Util/trunk/com.bluexml.side.Util.libs/src/com/bluexml/side/util/libs/ui/UIUtils.java
+++ Util/trunk/com.bluexml.side.Util.libs/src/com/bluexml/side/util/libs/ui/UIUtils.java
@@ -5,49 +5,56 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 
 public class UIUtils {
-	
+
 	public static int showAvert(String title, String message) {
 		int style = 0;
 		style |= SWT.YES | SWT.NO | SWT.CANCEL;
-		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style	);
+		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style);
 		mb.setText(title);
-        mb.setMessage(message);
-        int val = mb.open();
-        return val;
+		mb.setMessage(message);
+		int val = mb.open();
+		return val;
 	}
-	
+
 	public static boolean showConfirmation(String title, String message) {
 		boolean doWork = true;
 		int style = 0;
 		style |= SWT.YES | SWT.NO;
-		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style	);
+		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style);
 		mb.setText(title);
-        mb.setMessage(message);
-        int val = mb.open();
-        switch (val)
-        {
-	        case SWT.OK:
-	        	doWork = true;
-	          break;
-	        case SWT.CANCEL:
-	        	doWork = false;
-	          break;
-	        case SWT.YES:
-	        	doWork = true;
-	          break;
-	        case SWT.NO:
-	        	doWork = false;
-	          break;
-        }
-        return doWork;
+		mb.setMessage(message);
+		int val = mb.open();
+		switch (val) {
+		case SWT.OK:
+			doWork = true;
+			break;
+		case SWT.CANCEL:
+			doWork = false;
+			break;
+		case SWT.YES:
+			doWork = true;
+			break;
+		case SWT.NO:
+			doWork = false;
+			break;
+		}
+		return doWork;
 	}
-	
+
 	public static void showError(String title, String message) {
 		int style = 0;
 		style |= SWT.OK;
-		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style	);
+		MessageBox mb = new MessageBox(Display.getCurrent().getActiveShell(), style);
 		mb.setText(title);
-        mb.setMessage(message);
-        mb.open();
+		mb.setMessage(message);
+		mb.open();
+	}
+
+	public static Display getDisplay() {
+		Display display = Display.getCurrent();
+		// may be null if outside the UI thread
+		if (display == null)
+			display = Display.getDefault();
+		return display;
 	}
 }
