9a78174fc7ae9b54dfc11efb03b5139ad8634eb5
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-18 15:24:48 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@11409 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/testAuto/compare_2_files.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/testAuto/compare_2_files.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $1 > res1.xml
-sed 's/<date>.*date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res1.xml > res11.xml
-sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/><\/de/' res11.xml > res111.xml
+sed 's/date=\"........................UTC\" /date=\"AAAA-MM-DD HH:MM:SS.MMM UTC\" /g' $1 > res1.xml
+sed 's/<date>........................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res1.xml > res11.xml
+sed 's/<date>.......................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res11.xml > res111.xml
+sed 's/<date>......................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res111.xml > res1111.xml
 
-sed 's/date=\".*CEST\" /date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" /' $2 > res2.xml
-sed 's/<date>.*<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM CEST<\/date>/' res2.xml > res22.xml
-sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM CEST\" \/><\/de/' res22.xml > res222.xml
+#sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM UTC\" \/><\/de/' res11.xml > res111.xml
+
+sed 's/date=\"........................UTC\" /date=\"AAAA-MM-DD HH:MM:SS.MMM UTC\" /g' $2 > res2.xml
+sed 's/<date>........................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res2.xml > res22.xml
+sed 's/<date>.......................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res22.xml > res222.xml
+sed 's/<date>......................UTC<\/date>/<date>AAAA-MM-DD HH:MM:SS.MMM UTC<\/date>/g' res222.xml > res2222.xml
+
+#sed 's/date=\".*\" \/><\/de/date=\"AAAA-MM-DD HH:MM:SS.MMM UTC\" \/><\/de/' res22.xml > res222.xml
 
 
-./diffxml/diffxml.sh -q res111.xml res222.xml > out.xml 
+./diffxml/diffxml.sh -q res1111.xml res2222.xml > out.xml
 
 
 
 if test -s out.xml
-then        
-	echo "not empty"
-	rm out.xml
-	./diffxml/diffxml.sh res111.xml res222.xml > out.xml
+then
+        echo "not empty"
+        rm out.xml
+        ./diffxml/diffxml.sh res1111.xml res222.xml > out.xml
 else
         echo "empty"
 fi
 
+
 rm res1.xml
 rm res11.xml
 rm res111.xml
+rm res1111.xml
 rm res2.xml
 rm res22.xml
 rm res222.xml
+rm res2222.xml
