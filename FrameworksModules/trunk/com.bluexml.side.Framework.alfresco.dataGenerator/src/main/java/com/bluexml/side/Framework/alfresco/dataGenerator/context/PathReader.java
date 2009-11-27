1c8872a7e53b56cd2dbcf2b017fd53c3402d9610
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-11-27 10:42:59 +0000

    new bean pathReader
    git-svn-id: http://svn.bluexml.com/private/S-IDE@11754 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/context/PathReader.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.bluexml.side.Framework.alfresco.dataGenerator.context;
+
+
+/**
+ * @author dchevrier
+ *
+ */
+public class PathReader {
+
+	private String pathPattern;
+
+	/**
+	 * @return the path
+	 */
+	public String getPathPattern() {
+		return pathPattern;
+	}
+
+	/**
+	 * @param path the path to set
+	 */
+	public void setPathPattern(String pathPattern) {
+		this.pathPattern = pathPattern;
+	}
+	
+}
