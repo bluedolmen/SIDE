90e04db10337dc278d601383463693fd22d0ffe1
bchevallereau <bchevallereau@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-12-08 11:36:40 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@11989 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Util/trunk/com.bluexml.side.Util.feedback.startup/src/com/bluexml/side/util/feedback/startup/Activator.java
@@ -0,0 +1,32 @@
+package com.bluexml.side.util.feedback.startup;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+}
