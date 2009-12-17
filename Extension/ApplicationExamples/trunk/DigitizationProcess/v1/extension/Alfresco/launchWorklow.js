31c03800d98705be2f35f46bf30c1c44263e1a77
rbaudu <rbaudu@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-12-17 10:07:55 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@12249 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Extension/ApplicationExamples/trunk/DigitizationProcess/v1/extension/Alfresco/launchWorklow.js
+++ Extension/ApplicationExamples/trunk/DigitizationProcess/v1/extension/Alfresco/launchWorklow.js
@@ -1,9 +1,9 @@
-document.specializeType('DigitizationProcess:com_bluexml_side_models_liste_Document');
+document.specializeType('DigitizationProcess:com_bluexml_side_models_list_Document');
 
 var wfAction = actions.create("start-workflow");
 wfAction.parameters.workflowName = "jbpm$wfbxDigitizationProcess:DigitizationProcess";
 wfAction.parameters.endStartTask = true;
-wfAction.parameters["bpm:pooledActors"] = new Array(people.getGroup('GROUP_Numerisation'));
+wfAction.parameters["bpm:pooledActors"] = new Array(people.getGroup('GROUP_Digitization'));
 wfAction.parameters["cm:owner"] = null;
 wfAction.parameters["cm:context"] = null;
 
