1488ae17793a89df3cd9d7bb27d7bb02eb292b00
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2009-12-18 20:02:43 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1258
    git-svn-id: http://svn.bluexml.com/private/S-IDE@12316 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.facetMap/src/com/bluexml/side/deployer/facetmap/FacetMapDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.facetMap/src/com/bluexml/side/deployer/facetmap/FacetMapDeployer.java
@@ -5,77 +5,22 @@ package com.bluexml.side.deployer.facetmap;
 
 import java.io.File;
 
-import com.bluexml.side.util.deployer.AbstractMultiDeployer;
 import com.bluexml.side.util.deployer.war.WarDeployer;
 
 /**
  * @author davidabad
  * 
  */
-public class FacetMapDeployer extends AbstractMultiDeployer {
+public class FacetMapDeployer extends WarDeployer {
 
 	public FacetMapDeployer() {
-		super("com.bluexml.side.Deployer.facetMap.clean", null);
-
-		// build map of ModuleID,(facetmap-facets.zip,facetmap-conent.zip)
-		// for each moduleID build 2 war deployer and process them
-
-		// record first webapp deployer
-		final String webappName1 = "facetmap-facets";
-		WarDeployer wd1 = new WarDeployer(this.cleanKey, this.logChangesKey, webappName1, "") {
-			@Override
-			protected void postProcess(File fileToDeploy) throws Exception {
-			};
-
-			public File getFileToDeploy(String absoluteWKDirePath) {
-				return new File(absoluteWKDirePath + File.separator + techVersion + File.separator + webappName1 + ".zip");
-			}
-
-		};
-		addDeployer(wd1);
-
-		// record second webapp deployer
-		final String webappName2 = "facetmap-content";
-		WarDeployer wd2 = new WarDeployer(this.cleanKey,this.logChangesKey,webappName2,"") {
-			@Override
-			protected void postProcess(File fileToDeploy) throws Exception {
-			};
-
-			public File getFileToDeploy(String absoluteWKDirePath) {
-				return new File(absoluteWKDirePath + File.separator + techVersion + File.separator + webappName2 + ".zip");
-			}
-		};
-		addDeployer(wd2);
+		super("com.bluexml.side.Deployer.facetMap.clean", null, "facetmap", "");
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.bluexml.side.application.deployer.Deployer#postProcess(java.io.File)
-	 */
 	@Override
 	protected void postProcess(File fileToDeploy) throws Exception {
-		// nothing to do
-	}
+		// TODO Auto-generated method stub
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.bluexml.side.application.security.Checkable#check()
-	 */
-	public boolean check() {
-		return true;
-	}
-
-	@Override
-	protected void clean(File fileToDeploy) throws Exception {
-		// nothing to do
-	}
-
-	@Override
-	protected void preProcess(File fileToDeploy) throws Exception {
-		// nothing to do
 	}
 
 }
