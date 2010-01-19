669610392c1aaf3fa60ce5c099da089873c2211c
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-01-19 14:05:02 +0000

    take into account scenario
    git-svn-id: http://svn.bluexml.com/private/S-IDE@12981 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/webscript/Generate.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/webscript/Generate.java
@@ -15,6 +15,9 @@ import org.alfresco.web.scripts.Cache;
 import org.alfresco.web.scripts.DeclarativeWebScript;
 import org.alfresco.web.scripts.Status;
 import org.alfresco.web.scripts.WebScriptRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xml.sax.SAXException;
 
 import com.bluexml.side.Framework.alfresco.dataGenerator.dictionary.AlfrescoModelDictionary;
 import com.bluexml.side.Framework.alfresco.dataGenerator.generator.AlfrescoModelRandomDataGenerator;
@@ -23,9 +26,6 @@ import com.bluexml.side.Framework.alfresco.dataGenerator.load.ImportACP;
 import com.bluexml.side.Framework.alfresco.dataGenerator.serialization.ACPPackaging;
 import com.bluexml.side.Framework.alfresco.dataGenerator.serialization.XMLForACPSerialization;
 import com.bluexml.side.Framework.alfresco.dataGenerator.structure.IStructure;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.xml.sax.SAXException;
 
 /**
  * @author davidchevrier
@@ -39,18 +39,22 @@ public class Generate extends DeclarativeWebScript {
 	private static final String NUMBER_OF_OUTPUT_ARCS_PARAMETER_NAME = "numOfOutputArcs";
 	private static final String PATH_TO_ALFRESCO_REPOSITORY = "alfrescoRepository";
 	private static final String PATH_TO_DOCUMENTS = "pathToDocuments";
+	private static final String SCENARIO = "scenario";
 	
 	private static final String XML_FILE_NAME = "Data.xml";
 	public static final String ACP_FILE_NAME = "Data";
-
 	
+	private Map<String,Object> model = new HashMap<String, Object>();
+
 	@Override
 	protected Map<String, Object> executeImpl(WebScriptRequest req,Status status, Cache cache) {		
 		//get and fill generator parameters
 		String numOfContentsParameterValue = req.getParameter(NUMBER_OF_CONTENTS_PARAMETER_NAME);
 		String numOfOutPutArcsParameterValue = req.getParameter(NUMBER_OF_OUTPUT_ARCS_PARAMETER_NAME);
+		String scenarioParameterValue = req.getParameter(SCENARIO);
 		generator.setNumberOfNodes(Integer.valueOf(numOfContentsParameterValue));
 		generator.setNumberOfOutputArcs(Integer.valueOf(numOfOutPutArcsParameterValue));
+		generator.setScenario(scenarioParameterValue);
 		
 		String modelParameterValue = req.getParameter(MODEL_PARAMETER_NAME);
 		dictionary.setQnameStringModel(modelParameterValue);
@@ -58,16 +62,18 @@ public class Generate extends DeclarativeWebScript {
 		String pathToDocuments = req.getParameter(PATH_TO_DOCUMENTS);
 		nativeGenerator.setPathToDocumentsFolder(pathToDocuments);
 		
-		
 		//get model structure
 		IStructure structure = null;
 		try {
 			structure = dictionary.getStructure(dictionary.getQnameStringModel());
 		} catch (ParserConfigurationException e2) {
+			model.put("error", e2);
 			logger.error("Error :", e2);
 		} catch (SAXException e2) {
+			model.put("error", e2);
 			logger.error("Error :", e2);
 		} catch (IOException e2) {
+			model.put("error", e2);
 			logger.error("Error :", e2);
 		}
 		
@@ -76,6 +82,7 @@ public class Generate extends DeclarativeWebScript {
 		try {
 			generated = generator.generateNodesInstances(structure);
 		} catch (Exception e1) {
+			model.put("error", e1);
 			logger.error("Error :", e1);
 		}
 		try {
@@ -83,16 +90,17 @@ public class Generate extends DeclarativeWebScript {
 				generator.generateArcsInstances(structure);
 			}
 		} catch (Exception e1) {
+			model.put("error", e1);
 			logger.error("Error :", e1);
 		}
 		generator.deleteExceededNodes();
-//		generator.deleteExceededArcs();
 		
 		//serialize xml for acp
 		serializer.setFileName(XML_FILE_NAME);
 		try {
 			serializer.serializeXml();
 		} catch (IOException e) {
+			model.put("error", e);
 			logger.error("Error :", e);
 		}
 		
@@ -102,6 +110,7 @@ public class Generate extends DeclarativeWebScript {
 		try {
 			acp = packager.packageACP();
 		} catch (IOException e) {
+			model.put("error", e);
 			logger.error("Error :", e);
 		}
 		
@@ -120,17 +129,21 @@ public class Generate extends DeclarativeWebScript {
 			try {
 				importer.saveACP(acp,repository);
 			} catch (IOException e) {
+				model.put("error", e);
 				logger.error("Error :", e);
 			}
 			// Brice : First save (in case of problem during import in order to analyze the situation), next import
-			importer.importACP(acp,repository);
-
-			
+			try {
+				importer.importACP(acp,repository);
+			} catch (Exception e) {
+				model.put("error", e);
+				logger.error("Error :", e);
+			}
 		}
 		
 		packager.clean(acp);
 		
-		return new HashMap<String, Object>();
+		return model;
 	}
 	
 	/*
@@ -167,6 +180,5 @@ public class Generate extends DeclarativeWebScript {
 	public void setNativeAlfrescoModelRandomDataGenerator(NativeAlfrescoModelRandomDataGenerator nativeGenerator_){
 		nativeGenerator = nativeGenerator_;
 	}
-
 
 }
