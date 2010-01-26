d20f27c2762d6a55283bd437c7c97b4f51308b3f
bchevallereau <bchevallereau@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-01-26 13:07:41 +0000

    Add extensions
    git-svn-id: http://svn.bluexml.com/private/S-IDE@13204 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.eclipse.builder/src/com/bluexml/side/integration/eclipse/builder/SIDEBuilderConstants.java
+++ Integration/trunk/com.bluexml.side.Integration.eclipse.builder/src/com/bluexml/side/integration/eclipse/builder/SIDEBuilderConstants.java
@@ -4,8 +4,8 @@ public class SIDEBuilderConstants {
 
 	public static String metadataFolder = ".metadata";
 	public static String referentialFileName = metadataFolder+"/referential.xml";
-	public static String[] availableExtensions = {"application","form","dt","requirements","workflow"};
-	public static String[] availableExtensionsDiagrams = {"dtdi","requirementsdi","workflowdi"};
+	public static String[] availableExtensions = {"application","form","dt","requirements","workflow","portal","view"};
+	public static String[] availableExtensionsDiagrams = {"dtdi","requirementsdi","workflowdi","portaldi"};
 	public static String[] protectedPrefix = {".backup"};
 	
 }
