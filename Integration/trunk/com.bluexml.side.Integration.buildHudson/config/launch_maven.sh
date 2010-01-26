399a29029c7d3b7d29abc932985cd4941f767184
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-01-26 14:30:23 +0000

    script shell to launch maven deploy command
    git-svn-id: http://svn.bluexml.com/private/S-IDE@13207 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Prepare maven workspace and launch maven deploy
+# 1st parameter =  workspace path of the buildHuson project
+if [ $# -eq 2 ]; then
+  WORKSPACE=$1
+  REPOSITORY_COPY=$2
+else
+  echo "Usage: launch_maven.sh WORKSPACE"
+  echo "       with WORKSPACE =  workspace path of the buildHuson project"
+  exit -2
+fi
+
+cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
+cp -R $REPOSITORY_COPY/S-IDE/FrameworksModules/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
+cp -R $REPOSITORY_COPY/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.utils $WORKSPACE/../buildAuto/Ankle/maven_workspace
+chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_workspace
+
+
+cp $WORKSPACE/../buildAuto/Ankle/delete_folder.pl $WORKSPACE/../buildAuto/Ankle/maven_workspace
+cd $WORKSPACE/../buildAuto/Ankle/maven_workspace
+perl delete_folder.pl pom.xml
+cp $WORKSPACE/../../superpom/workspace/superpom/pom.xml .
+
+perl -pi -le 'print "<modules>" if $. == 12' pom.xml
+perl -pi -le 'print "</modules>" if $. == 13' pom.xml
+    
+for s in `ls -d */ `; do
+        repertoire=$s
+	name=`expr match "$repertoire" '\([a-zA-Z0-9.]*\)'`
+     	res="<module>$name</module>"
+	perl -pi -le 'print "'$res'" if $. == 13' pom.xml
+done
+
+mvn clean deploy -e -P public > log_maven.log
+
+exit 0
