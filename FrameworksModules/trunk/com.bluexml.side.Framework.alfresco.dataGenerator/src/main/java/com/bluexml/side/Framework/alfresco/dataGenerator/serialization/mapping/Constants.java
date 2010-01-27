9fbdfaf3b509a5a2f531c7b3ddb6ef8549feda5f
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-01-27 20:33:11 +0000

    new way to build xml
    git-svn-id: http://svn.bluexml.com/private/S-IDE@13429 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/serialization/mapping/Constants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.bluexml.side.Framework.alfresco.dataGenerator.serialization.mapping;
+
+/**
+ * @author dchevrier
+ *
+ */
+public class Constants {
+	
+	public static final String SEPARATOR = ":";
+	public static final String NATIVE_SIDE_PREFIX = "bxcm";
+	public static final String NATIVE_SIDE_URI = "http://www.bluexml.com/model/content/1.0";
+	public static final String ASPECTS = "aspects";
+	public static final String REFERENCE = "reference";
+	public static final String PATHREF = "pathref";
+	public static final String NAME = "name";
+	public static final String ASSOCIATIONS = "associations";
+	public static final String PROPERTIES = "properties";
+	public static final String TITLE = "title";
+	public static final String BLANK_REPLACE = "_x0020_";
+
+}
