bb612a78f78d0e99067dc3d36f31d126e71f2456
bchevallereau <bchevallereau@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-01-27 11:32:01 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@13272 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Workflow/trunk/com.bluexml.side.Workflow.modeler/src/com/bluexml/side/Workflow/modeler/diagram/figures/TransitionFigure.java
+++ MetaModel/Workflow/trunk/com.bluexml.side.Workflow.modeler/src/com/bluexml/side/Workflow/modeler/diagram/figures/TransitionFigure.java
@@ -17,7 +17,7 @@ package com.bluexml.side.Workflow.modeler.diagram.figures;
 import org.eclipse.draw2d.Locator;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 import org.topcased.modeler.edit.locators.EdgeObjectOffsetLocator;
 import org.topcased.modeler.figures.EdgeObjectOffsetEditableLabel;
 import org.topcased.modeler.figures.IEdgeObjectFigure;
@@ -42,8 +42,7 @@ public class TransitionFigure extends PolylineConnectionEx {
 		setLineStyle(SWT.LINE_SOLID);
 
 		middleNameEdgeObject = new EdgeObjectOffsetEditableLabel(this);
-		float[] rgb = {0f,0f,0f}; 
-		middleNameEdgeObject.setForegroundColor(Color.cocoa_new(null, rgb));
+		middleNameEdgeObject.setForegroundColor(Display.getCurrent().getSystemColor(SWT.COLOR_BLACK));
 		middleNameLocator = new EdgeObjectOffsetLocator(
 				(IEdgeObjectOffsetFigure) middleNameEdgeObject);
 		add(middleNameEdgeObject, middleNameLocator);
