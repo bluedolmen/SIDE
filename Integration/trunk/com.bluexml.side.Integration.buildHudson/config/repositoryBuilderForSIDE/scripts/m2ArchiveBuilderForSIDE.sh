a9ab9cda006d05d82d134d7f2c3b61c653318479
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-02-03 15:27:04 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@13771 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/scripts/m2ArchiveBuilderForSIDE.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/scripts/m2ArchiveBuilderForSIDE.sh
@@ -1,12 +1,13 @@
 # configuration
 SIDEHOME=/Users/davidabad/Workspace2.0/S-IDE
 BUILDERHOME=/Users/davidabad/Workspace2.0/buildAllMaven
-POM_PATCHER=/Users/davidabad/Workspace2.0/S-IDE/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/target/repositoryBuilderForSIDE-0.0.1-jar-with-dependencies.jar
+POM_PATCHER_PROJECT=/Users/davidabad/Workspace2.0/S-IDE/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE
 POM_IN=/Users/davidabad/Workspace2.0/superpom/pom.xml
 MAVENREPO_ARCHIVE=/Users/davidabad/Workspace2.0/S-IDE/Util/trunk/com.bluexml.side.Util.dependencies/src/com/bluexml/side/util/dependencies/mavenRepository/m2repositoryForSIDE.zip
 
 
 # set constantes
+POM_PATCHER=$POM_PATCHER_PROJECT/target/repositoryBuilderForSIDE-0.0.1-jar-with-dependencies.jar
 WORKDIR=$BUILDERHOME/projects
 POM_OUT=$WORKDIR/pom.xml
 MAVENREPO=$BUILDERHOME/repository
@@ -23,6 +24,10 @@ echo MAVENREPO=$MAVENREPO
 echo MAVENREPO_ARCHIVE=$MAVENREPO_ARCHIVE
 echo ==================
 
+# build patcher jar
+cd $POM_PATCHER_PROJECT
+mvn clean package
+
 
 # clean working directories
 rm -rf $BUILDERHOME/*
@@ -37,4 +42,7 @@ java -jar $POM_PATCHER $SIDEHOME $POM_OUT
 # build archive of all maven requirements
 mvn dependency:go-offline -P offline -Dmaven.repo.local=$MAVENREPO
 cd $MAVENREPO
+if [ -f $MAVENREPO_ARCHIVE ] ; then
+	rm $MAVENREPO_ARCHIVE	
+fi
 zip -r $MAVENREPO_ARCHIVE .
