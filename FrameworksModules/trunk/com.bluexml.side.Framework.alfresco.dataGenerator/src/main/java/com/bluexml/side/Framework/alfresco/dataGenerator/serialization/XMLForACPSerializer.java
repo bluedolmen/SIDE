76c79a4d854c721b0ed29d69abd00db792fb3451
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-02-07 22:14:07 +0000

    to take into account UTF-8 encoding
    git-svn-id: http://svn.bluexml.com/private/S-IDE@14060 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/serialization/XMLForACPSerializer.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.dataGenerator/src/main/java/com/bluexml/side/Framework/alfresco/dataGenerator/serialization/XMLForACPSerializer.java
@@ -3,7 +3,11 @@
  */
 package com.bluexml.side.Framework.alfresco.dataGenerator.serialization;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.OutputStreamWriter;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 
 import com.bluexml.side.Framework.alfresco.dataGenerator.serialization.mapping.XMLForACPMappingBuilder;
 
@@ -33,8 +37,10 @@ public class XMLForACPSerializer {
 	}
 	
 	public void serializeXml() throws Exception {
-		FileWriter out = new FileWriter(fileName);
-	    builder.build().write(out); 
-	    out.close();
+		FileOutputStream fos = new FileOutputStream(fileName);
+		OutputStreamWriter osw = new OutputStreamWriter(fos,"UTF-8");
+	    builder.build().write(osw); 
+	    osw.close();
+	    fos.close();
 	}	
 }
