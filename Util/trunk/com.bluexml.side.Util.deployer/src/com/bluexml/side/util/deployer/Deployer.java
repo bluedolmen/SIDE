e684a3e4c61bfe1e2ea6bdf83d43016110364866
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-02-08 13:02:42 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=1394, add fake deployer
    git-svn-id: http://svn.bluexml.com/private/S-IDE@14103 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Util/trunk/com.bluexml.side.Util.deployer/src/com/bluexml/side/util/deployer/Deployer.java
+++ Util/trunk/com.bluexml.side.Util.deployer/src/com/bluexml/side/util/deployer/Deployer.java
@@ -236,4 +236,6 @@ public abstract class Deployer implements Checkable {
 			}
 		}
 	}
+	
+	
 }
---
+++ Util/trunk/com.bluexml.side.Util.deployer/src/com/bluexml/side/util/deployer/FakeDeployer.java
@@ -0,0 +1,35 @@
+package com.bluexml.side.util.deployer;
+
+import java.io.File;
+
+public class FakeDeployer extends Deployer {
+
+	public FakeDeployer() {
+		super("fakeClean", "fakeLog");
+	}
+
+	@Override
+	protected void clean(File fileToDeploy) throws Exception {
+		// nothing to do
+	}
+
+	@Override
+	protected void deployProcess(File fileToDeploy) throws Exception {
+		// nothing to do
+	}
+
+	@Override
+	protected void postProcess(File fileToDeploy) throws Exception {
+		// nothing to do
+	}
+
+	@Override
+	protected void preProcess(File fileToDeploy) throws Exception {
+		// nothing to do
+	}
+
+	public boolean check() {
+		return true;
+	}
+
+}
