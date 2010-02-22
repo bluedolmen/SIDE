9f29be9eb5dac7ae5c2fce55d6cd2ea822fabdbb
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-02-22 15:30:26 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@14760 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder/build.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder/build.sh
@@ -4,21 +4,25 @@ SIDE_HOME=$WORKSPACE/S-IDE
 #BUILDER_HOME=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder
 BUILDER_HOME=/Users/davidabad/Workspace2.0/S-IDE/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder
 
-REPO_BULDER=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/scripts/m2ArchiveBuilderForSIDE.sh
+#REPO_BUILDER=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/scripts/m2ArchiveBuilderForSIDE.sh
 
 EclipseZIP=/Users/davidabad/Archive/eclipse3.5.1ForSIDE.zip
 EclipseDeltaPack=/Users/davidabad/Archive/eclipse-3.5.1-delta-pack.tar.gz
-WORKDIR=$BUILDER_HOME/work
+
+WORKDIR=$WORKSPACE/work
+
 ECLIPSE_BUILDER=$WORKDIR/eclipse
 ECLIPSE_TOBUILD=$WORKSPACE/sources
+#ECLIPSE_TOBUILD=$ECLIPSE_BUILDER
 echo "=========="
 echo WORKSPACE			=$WORKSPACE 
 echo SIDE_HOME			=$SIDE_HOME
 echo BUILDER_HOME		=$BUILDER_HOME
-echo REPO_BULDER		=$REPO_BULDER
 echo EclipseZIP			=$EclipseZIP
 echo EclipseDeltaPack	=$EclipseDeltaPack
 echo WORKDIR			=$WORKDIR
+echo ECLIPSE_BUILDER	=$ECLIPSE_BUILDER
+echo ECLIPSE_TOBUILD	=$ECLIPSE_TOBUILD
 echo "=========="
 
 
@@ -36,6 +40,8 @@ echo "=========="
 #sh $REPO_BULDER
 
 ## Eclipse preparation
+
+
 echo "== Eclipse =="
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
@@ -44,6 +50,7 @@ unzip $EclipseZIP
 mkdir -p eclipse/deltapack
 cd $ECLIPSE_BUILDER/deltapack
 tar -xvzf $EclipseDeltaPack
+
 echo "== copy plugins from SIDE source =="
 rm -rf $ECLIPSE_TOBUILD
 mkdir -p $ECLIPSE_TOBUILD/plugins
@@ -51,14 +58,13 @@ cp -rf $SIDE_HOME/MetaModel/*/trunk/* $ECLIPSE_TOBUILD/plugins
 cp -rf $SIDE_HOME/Util/trunk/* $ECLIPSE_TOBUILD/plugins
 cp -rf $SIDE_HOME/Deployer/trunk/* $ECLIPSE_TOBUILD/plugins
 cp -rf $SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.eclipse* $ECLIPSE_TOBUILD/plugins
+
 echo "== copy features from SIDE source =="
 mkdir -p $ECLIPSE_TOBUILD/features
 for f in `find $SIDE_HOME -type d -name *feature`; do
      cp -rfv $f $ECLIPSE_TOBUILD/features
 done
-# remove svn
-#find $ECLIPSE_BUILDER/features -name .svn | xargs sudo rm -rf
-#find $ECLIPSE_BUILDER/plugins -name .svn | xargs sudo rm -rf
+
 
 echo "== clean previous build =="
 rm -rf $BUILDER_HOME/dist
