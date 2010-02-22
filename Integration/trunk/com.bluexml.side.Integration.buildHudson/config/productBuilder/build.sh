d0627e2a547a1c0f8b244d3f94ddf9a774c503a6
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-02-22 10:33:55 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@14742 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder/build.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder/build.sh
@@ -1,21 +1,35 @@
-WORKSPACE=/Users/davidabad/Workspace2.0
+WORKSPACE=/Users/davidabad/.hudson/jobs/SIDE_Enterprise_Product_Builder/workspace
+#WORKSPACE=/Users/davidabad/Workspace2.0
 SIDE_HOME=$WORKSPACE/S-IDE
-BUILDER_HOME=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder
+#BUILDER_HOME=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder
+BUILDER_HOME=/Users/davidabad/Workspace2.0/S-IDE/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/productBuilder
+
 REPO_BULDER=$SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/scripts/m2ArchiveBuilderForSIDE.sh
 
 EclipseZIP=/Users/davidabad/Archive/eclipse3.5.1ForSIDE.zip
 EclipseDeltaPack=/Users/davidabad/Archive/eclipse-3.5.1-delta-pack.tar.gz
 WORKDIR=$BUILDER_HOME/work
 ECLIPSE_BUILDER=$WORKDIR/eclipse
+ECLIPSE_TOBUILD=$WORKSPACE/sources
+echo "=========="
+echo WORKSPACE			=$WORKSPACE 
+echo SIDE_HOME			=$SIDE_HOME
+echo BUILDER_HOME		=$BUILDER_HOME
+echo REPO_BULDER		=$REPO_BULDER
+echo EclipseZIP			=$EclipseZIP
+echo EclipseDeltaPack	=$EclipseDeltaPack
+echo WORKDIR			=$WORKDIR
+echo "=========="
 
+
+#cd $WORKSPACE
+#svn co http://www.bluexml.com/svn/private/S-IDE
 #rm -rf $SIDE_HOME
 #mkdir -p $SIDE_HOME
-#cd $SIDE_HOME
 
-#svn co http://www.bluexml.com/svn/private/S-IDE/MetaModel
-#svn co http://www.bluexml.com/svn/private/S-IDE/Deployer
-#svn co http://www.bluexml.com/svn/private/S-IDE/Util
-#svn co http://www.bluexml.com/svn/private/S-IDE/Integration
+#cd $SIDE_HOME
+#echo "== Update S-IDE =="
+#svn update
 
 
 # build mavenRepo and includes depndencies
@@ -31,14 +45,16 @@ mkdir -p eclipse/deltapack
 cd $ECLIPSE_BUILDER/deltapack
 tar -xvzf $EclipseDeltaPack
 echo "== copy plugins from SIDE source =="
-cp -rf $SIDE_HOME/MetaModel/*/trunk/* $ECLIPSE_BUILDER/plugins
-cp -rf $SIDE_HOME/Util/trunk/* $ECLIPSE_BUILDER/plugins
-cp -rf $SIDE_HOME/Deployer/trunk/* $ECLIPSE_BUILDER/plugins
-cp -rf $SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.eclipse* $ECLIPSE_BUILDER/plugins
-echo "== Add Fake feature includings all dependencies =="
-#cp -rfv /Users/davidabad/Workspace2.0/TestRCP $ECLIPSE_BUILDER/plugins
+rm -rf $ECLIPSE_TOBUILD
+mkdir -p $ECLIPSE_TOBUILD
+cp -rf $SIDE_HOME/MetaModel/*/trunk/* $ECLIPSE_TOBUILD/plugins
+cp -rf $SIDE_HOME/Util/trunk/* $ECLIPSE_TOBUILD/plugins
+cp -rf $SIDE_HOME/Deployer/trunk/* $ECLIPSE_TOBUILD/plugins
+cp -rf $SIDE_HOME/Integration/trunk/com.bluexml.side.Integration.eclipse* $ECLIPSE_TOBUILD/plugins
 echo "== copy features from SIDE source =="
-#find $SIDE_HOME -name *feature | xargs -I source cp -rfv source $ECLIPSE_BUILDER/features
+for f in `find $SIDE_HOME -type d -name *feature`; do
+     cp -rfv $f $ECLIPSE_TOBUILD/features
+done
 # remove svn
 #find $ECLIPSE_BUILDER/features -name .svn | xargs sudo rm -rf
 #find $ECLIPSE_BUILDER/plugins -name .svn | xargs sudo rm -rf
@@ -48,4 +64,5 @@ rm -rf $BUILDER_HOME/dist
 mkdir -p $BUILDER_HOME/dist
 echo "== run builder =="
 cd $BUILDER_HOME
+echo $BUILDER_HOME
 ant pde-build2
