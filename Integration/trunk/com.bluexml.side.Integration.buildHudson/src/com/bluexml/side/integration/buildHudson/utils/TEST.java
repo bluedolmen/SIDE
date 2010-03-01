4ec7ebddcbe00a8dfe0e5c5a0cba96dc112f861d
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-03-01 16:53:49 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@15127 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/src/com/bluexml/side/integration/buildHudson/utils/TEST.java
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/src/com/bluexml/side/integration/buildHudson/utils/TEST.java
@@ -10,19 +10,7 @@ public class TEST {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(Utils.getBuildPath());
-		File testBuild = new File("/Users/davidabad/Workspace2.0/test/BuildAutoHudson/build.xml");
-		FileWriter fw;
-		try {
-			testBuild.createNewFile();
-			fw = new FileWriter(testBuild);
-			//fw.write(Application.getCorpsBuild());
-			fw.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		String workspace = "/Users/davidabad/.hudson/jobs/SIDE_Enterprise_Product_Builder/workspace";
 		String workspace_2 = "/Users/davidabad/Workspace2.0";
 		File product =new File(workspace_2+"/S-IDE/Integration/trunk/com.bluexml.side.Integration.eclipse.branding/side.product");
