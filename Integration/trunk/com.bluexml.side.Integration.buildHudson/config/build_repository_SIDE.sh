d99546c0795c46272553c22453205cfac1413bab
mtran <mtran@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-03-02 14:05:07 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@15179 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/build_repository_SIDE.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/build_repository_SIDE.sh
@@ -16,7 +16,7 @@ SIDEHOME=$WORKSPACE/../buildAuto/Ankle/repositoryCopy/S-IDE
 BUILDERHOME=$WORKSPACE/../buildAuto/Ankle/buildAllMaven
 POM_PATCHER=$WORKSPACE/../buildAuto/Ankle/repositoryBuilderForSIDE/target/repositoryBuilderForSIDE-0.0.1-jar-with-dependencies.jar
 POM_IN=$WORKSPACE/../../superpom/workspace/superpom/pom.xml
-MAVENREPO_ARCHIVE=$WORKSPACE/S-IDE/Util/trunk/com.bluexml.side.Util.dependencies/src/com/bluexml/side/util/dependencies/mavenRepository/m2repositoryForSIDE.zip
+MAVENREPO_ARCHIVE=$WORKSPACE/../buildAuto/Ankle/repositoryCopy/S-IDE/Util/trunk/com.bluexml.side.Util.dependencies/src/com/bluexml/side/util/dependencies/mavenRepository/m2repositoryForSIDE.zip
 
 
 # set constantes
