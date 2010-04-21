acf098ca95d82b1f28285a304196a9e484c3a6f6
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-04-21 10:31:17 +0000

    use com.bluexml.side.Framework.alfresco.cmisFacetMap instead
    git-svn-id: http://svn.bluexml.com/private/S-IDE@16503 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.alfresco.cmisFacetMap/src/main/java/com/bluexml/side/framework/alfresco/cmisFacetMap/CMISExtensionWebScript.java
+++
@@ -1,7 +0,0 @@
-package com.bluexml.side.framework.alfresco.cmisFacetMap;
- 
-import org.alfresco.repo.jscript.BaseScopableProcessorExtension;
-
-public class CMISExtensionWebScript extends BaseScopableProcessorExtension {
-
-}
