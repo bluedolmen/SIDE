a39070666b631a44302934002e1a39912fce6956
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-06-11 15:56:46 +0000

    add graph dot file generation (only includes data extracted from Blx extension point)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@17971 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
@@ -1,34 +1,37 @@
 package com.bluexml.side.m2.repositoryBuilder;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
+import org.jdom.JDOMException;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
 
 import com.bluexml.side.util.dependencies.ModuleConstraint;
-import com.bluexml.side.util.dependencies.ModuleVersion;
+import com.bluexml.side.util.dependencies.PluginsUtils;
 
 public class Builder {
 
-	public static List<ModuleConstraint> getModulesConstraint(File rootPlugins) {
+	public static List<ModuleConstraint> getModulesConstraint(File rootPlugins) throws Exception {
 
 		List<ModuleConstraint> moduleIds = new ArrayList<ModuleConstraint>();
 
 		// search in folder to get plugin.xml files
 
-		List<File> lPlug = getPluginXML(rootPlugins);
+		List<File> lPlug = PluginsUtils.getPluginXML(rootPlugins);
 		for (File file : lPlug) {
 			// search <moduleDependency>.*<moduleDependency>
 			try {
-				Document pluginXML = buildJdomDocument(file);
+				Document pluginXML = PluginsUtils.buildJdomDocument(file);
 				System.out.println("Scanning File :" + file);
 				XPath xpa = XPath.newInstance("//moduleDependence");
 				List<?> lmd = xpa.selectNodes(pluginXML.getRootElement());
@@ -75,106 +78,109 @@ public class Builder {
 		return moduleIds;
 	}
 
-	/**
-	 * search for plugin.xml files
-	 * 
-	 * @param folder
-	 *            where to start search
-	 * @return List of plugin.xml files
-	 */
-	private static List<File> getPluginXML(File f) {
-		List<File> rs = new ArrayList<File>();
-
-		File[] lf = f.listFiles();
-		for (File file : lf) {
-			if (file.isFile()) {
-				if (file.getName().equals("plugin.xml")) {
-					rs.add(file);
-					break;
-				}
-			} else if (!file.getName().startsWith(".")) {
-				List<File> ll = getPluginXML(file);
-				rs.addAll(ll);
-			}
-		}
-		return rs;
-	}
-
-	public static Document buildJdomDocument(File xmlFile) throws Exception {
-		Document doc;
-		org.jdom.input.SAXBuilder builder = new SAXBuilder();
-		doc = builder.build(xmlFile);
-		return doc;
-	}
-
 	public static void main(String[] args) {
-		// check parameters
-		boolean ok = true;
-		File rootPlugins = new File(args[0]);
-		if (!rootPlugins.exists()) {
-			ok = false;
-			System.err.println("File do not exist :" + rootPlugins);
-		}
-		File pomFile = new File(args[1]);
-		if (!pomFile.exists()) {
-			ok = false;
-			System.err.println("File do not exist :" + pomFile);
-		}
-		if (!ok) {
-			System.out.println("Usage :");
-			System.out.println("repositoryBuilderForSIDE.jar <pluginsSRCHome> <pom.xml>");
-		}
 
-		List<ModuleConstraint> res = Builder.getModulesConstraint(rootPlugins);
-
-		// add default dependencies to allow dependency maven plugin to be used
-		// later
-//		ModuleConstraint McForMavenDependeciesPlugins = new ModuleConstraint("org.apache.maven.plugins.maven-dependency-plugin", null, "maven-plugin", "2.0", "2.0");
-//
-//		res.add(McForMavenDependeciesPlugins);
-		
-
+		File rootPlugins = getPluginRepo(args);
 		try {
-			Document pom = buildJdomDocument(pomFile);
-			// search existing dependencies node
-			XPath xpa = XPath.newInstance("/project/dependencies");
-			Element deps = (Element) xpa.selectSingleNode(pom.getRootElement());
-			if (deps == null) {
-				deps = new Element("dependencies");
-				pom.getRootElement().addContent(deps);
+			switch (args.length) {
+			case 5:
+				// two option activated
+				String option2 = args[3];
+				String output2 = args[4];
+				applyOptionalJob(rootPlugins, option2, output2);
+			case 3:
+				// only one option activated				
+				String option = args[1];
+				String output = args[2];
+				applyOptionalJob(rootPlugins, option, output);
+				break;
+			default:
+				displayUsage();
 			}
-			// add dependencies
-			for (ModuleConstraint moduleConstraint : res) {
-				Element dep = new Element("dependency");
-
-				Element groupId = new Element("groupId").setText(moduleConstraint.getGroupId());
-				Element artifactId = new Element("artifactId").setText(moduleConstraint.getArtifactId());
-				Element version = new Element("version").setText(moduleConstraint.getVersionRange());
-				Element type = new Element("type").setText(moduleConstraint.getModuleType());
-				Element scope = new Element("scope").setText("compile");
-
-				dep.addContent(groupId);
-				dep.addContent(artifactId);
-				dep.addContent(version);
-				dep.addContent(type);
-				dep.addContent(scope);
-				deps.addContent(dep);
-				System.out.println("added :" + moduleConstraint);
-			}
-			// save changes
-			org.jdom.output.XMLOutputter out = new XMLOutputter();
-			Format format = Format.getPrettyFormat();
-			out.setFormat(format);
-			FileOutputStream outStream = new FileOutputStream(pomFile);
-			out.output(pom, outStream);
-			outStream.close();
 
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		System.out.println("pom Patched");
+	}
+
+	private static File getPluginRepo(String[] args) {
+		File rootPlugins = new File(args[0]);
+		if (!rootPlugins.exists()) {
+			System.err.println("File do not exist :" + rootPlugins);
+		}
+		return rootPlugins;
+	}
+
+	private static void applyOptionalJob(File rootPlugins, String option, String filePath) throws Exception, IOException {
+		if (option.equals("-makeDot")) {
+			writeDotFile(rootPlugins, filePath);
+			System.out.println("graph dot file generated :" + filePath);
+		} else if (option.equals("-patchPom")) {
+			List<ModuleConstraint> res = Builder.getModulesConstraint(rootPlugins);
+			File file = new File(filePath);
+			if (!file.exists()) {
+				System.err.println("File do not exist :" + file);
+			}
+			writePomFile(file, res);
+			System.out.println("pom Patched");
+		}
+	}
+
+	private static void displayUsage() {
+		System.out.println("Usage : repositoryBuilderForSIDE.jar <pluginsSRCHome> [-patchPom <pom.xml>] [-makeDot <output dot file>]");
+		System.out.println(" <pluginsSRCHome> : directory containing Eclipse plugins");
+		System.out.println(" <pom.xml>        : pom file to patch");
+		System.out.println(" -makeDot         : option to generate a graph dot file");
+		System.out.println(" <output dot file>: the output dot file (created or overriten)");
+	}
+
+	private static void writeDotFile(File rootPlugins, String output) throws Exception, IOException {
+		SideOptionsTree sot = new SideOptionsTree();
+		sot.extensionPointRenderer(rootPlugins);
+
+		File outputFile = new File(output);
+		FileWriter fw = new FileWriter(outputFile);
+		DotRenderer dotRenderer = new DotRenderer(fw, sot.getParentChildren(), sot.getNode2NodeType());
+		dotRenderer.render();
+		fw.flush();
+		fw.close();
+	}
+
+	private static void writePomFile(File pomFile, List<ModuleConstraint> res) throws Exception, JDOMException, FileNotFoundException, IOException {
+		Document pom = PluginsUtils.buildJdomDocument(pomFile);
+		// search existing dependencies node
+		XPath xpa = XPath.newInstance("/project/dependencies");
+		Element deps = (Element) xpa.selectSingleNode(pom.getRootElement());
+		if (deps == null) {
+			deps = new Element("dependencies");
+			pom.getRootElement().addContent(deps);
+		}
+		// add dependencies
+		for (ModuleConstraint moduleConstraint : res) {
+			Element dep = new Element("dependency");
+
+			Element groupId = new Element("groupId").setText(moduleConstraint.getGroupId());
+			Element artifactId = new Element("artifactId").setText(moduleConstraint.getArtifactId());
+			Element version = new Element("version").setText(moduleConstraint.getVersionRange());
+			Element type = new Element("type").setText(moduleConstraint.getModuleType());
+			Element scope = new Element("scope").setText("compile");
+
+			dep.addContent(groupId);
+			dep.addContent(artifactId);
+			dep.addContent(version);
+			dep.addContent(type);
+			dep.addContent(scope);
+			deps.addContent(dep);
+			System.out.println("added :" + moduleConstraint);
+		}
+		// save changes
+		org.jdom.output.XMLOutputter out = new XMLOutputter();
+		Format format = Format.getPrettyFormat();
+		out.setFormat(format);
+		FileOutputStream outStream = new FileOutputStream(pomFile);
+		out.output(pom, outStream);
+		outStream.close();
 	}
 
 }
