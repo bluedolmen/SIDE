9fc10cab8ab27b91cb06ab35c8cda3a3be264ffe
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-06-17 10:02:18 +0000

    maven deploy done in two time : first all mojo (in Integration), second all other maven projects
    git-svn-id: http://svn.bluexml.com/private/S-IDE@18398 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
@@ -11,47 +11,30 @@ else
   exit -2
 fi
 
-cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
+if [ -f launch_maven_subProcess.sh ]; then
+chmod +x launch_maven_subProcess.sh
+cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+echo "deploy mojos"
+./launch_maven_subProcess.sh $WORKSPACE maven_mojo_workspace
+
+
+
+
 cp -R $REPOSITORY_COPY/S-IDE/FrameworksModules/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
 cp -R $REPOSITORY_COPY/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/../buildAuto/Ankle/maven_workspace
+mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_workspace
 chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_workspace
+echo "deploy other m2 projects"
+./launch_maven_subProcess.sh $WORKSPACE maven_workspace
 
-
-cp $WORKSPACE/../buildAuto/Ankle/delete_folder.pl $WORKSPACE/../buildAuto/Ankle/maven_workspace
-cd $WORKSPACE/../buildAuto/Ankle/maven_workspace
-perl delete_folder.pl pom.xml
-cp $WORKSPACE/superpom/pom.xml .
-
-echo "update superpom"
-perl -pi -le 'print "<modules>" if $. == 12' pom.xml
-perl -pi -le 'print "</modules>" if $. == 13' pom.xml
-for s in `ls -d */ `; do
-    repertoire=$s
-	name=`perl -e '"'$repertoire'"=~/([a-zA-Z0-9.]*)/; print $1';`
-  	res="<module>$name</module>"
-  	echo $repertoire $name $res
-	perl -pi -le 'print "'$res'" if $. == 13' pom.xml
-done
-echo "superpom patched"
-echo "Maven deploy start"
-mvn clean deploy -e -P public > log_maven.log
-echo "Maven deploy done"
-
-sleep 5m
-
-tail -100 log_maven.log
-
-if grep "BUILD SUCCESSFUL" log_maven.log ; then
-	echo "Maven deploy success"
 else
-
-	if grep "ERROR" log_maven.log ; then	
-		echo "Maven deploy failed"
-		exit 1
-	else
-		echo "Maven deploy no error"
-	fi
-	
+  echo "Usage: launch_maven_subProcess must exist"
 fi
 
-exit 0
+
+
+
+
+
