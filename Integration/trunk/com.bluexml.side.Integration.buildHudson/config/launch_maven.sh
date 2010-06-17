1fa783e6d057694460a4bb56e87c35d0f51b7db1
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-06-17 12:10:14 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@18400 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
@@ -12,22 +12,20 @@ else
 fi
 
 if [ -f launch_maven_subProcess.sh ]; then
-chmod +x launch_maven_subProcess.sh
-cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
-mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
-chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
-echo "deploy mojos"
-./launch_maven_subProcess.sh $WORKSPACE maven_mojo_workspace
-
-
-
-
-cp -R $REPOSITORY_COPY/S-IDE/FrameworksModules/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
-cp -R $REPOSITORY_COPY/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/../buildAuto/Ankle/maven_workspace
-mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_workspace
-chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_workspace
-echo "deploy other m2 projects"
-./launch_maven_subProcess.sh $WORKSPACE maven_workspace
+	chmod +x launch_maven_subProcess.sh
+	mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+	chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+	cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
+	
+	echo "deploy mojos"
+	./launch_maven_subProcess.sh $WORKSPACE maven_mojo_workspace
+	
+	mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	cp -R $REPOSITORY_COPY/S-IDE/FrameworksModules/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	cp -R $REPOSITORY_COPY/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	echo "deploy other m2 projects"
+	./launch_maven_subProcess.sh $WORKSPACE maven_workspace
 
 else
   echo "Usage: launch_maven_subProcess must exist"
