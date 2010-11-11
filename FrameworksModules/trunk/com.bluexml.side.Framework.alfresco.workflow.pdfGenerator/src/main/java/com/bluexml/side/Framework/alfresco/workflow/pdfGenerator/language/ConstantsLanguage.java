d5e7c27e7fa39487f1e3c33a6cab8a70d920b77f
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-11-11 16:02:53 +0000

    Export PDF: prise en compte association 1..n
    git-svn-id: http://svn.bluexml.com/private/S-IDE@21611 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/language/ConstantsLanguage.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/language/ConstantsLanguage.java
@@ -16,6 +16,7 @@ public class ConstantsLanguage {
 	public static final String ACTION_KEY = "parameter.action";
 	public static final String[] ACTION_VALUES = {"IMPORT_FILLED_PDF","EXPORT_FILLED_PDF"};
 	
+	public static final String INPUT_PDF_NAME = "template.pdf";
 	public static final String[] INPUT_PDF_KEYS = {"parameter.pdf.uuid","parameter.pdf.path"};
 	public static final String[] OUTPUT_CONTENT_KEYS = {"parameter.output.uuid","parameter.output.path"};
 	public static final String OUTPUT_PDF_KEY = "parameter.output.path";
