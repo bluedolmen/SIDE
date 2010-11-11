12b137fa866189e1f172fd3723cda3a2c4ecb48f
dchevrier <dchevrier@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2010-11-11 16:01:42 +0000

    Export PDF: prise en compte association 1..n
    git-svn-id: http://svn.bluexml.com/private/S-IDE@21607 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/exception/InvalidValueOfParameterException.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.workflow.pdfGenerator/src/main/java/com/bluexml/side/Framework/alfresco/workflow/pdfGenerator/exception/InvalidValueOfParameterException.java
@@ -12,6 +12,7 @@ public class InvalidValueOfParameterException extends Exception {
 	private static final long serialVersionUID = -2263390281328107327L;
 	
 	public static final String BAD_FORMAT = "";
+	public static final String BAD_INDICATED_VALUE = "The parameter does not support the indicated value.";
 	
 	public InvalidValueOfParameterException(String message){
 		super(message);
