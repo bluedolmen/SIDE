d7df9bdc431a13e625d47991fd8c7f4d86335d3b
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-03 16:33:47 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@27988 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
@@ -6,7 +6,9 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.jdom.Attribute;
@@ -17,150 +19,125 @@ import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
 
+import com.bluexml.side.build.tools.DependencyTree;
+import com.bluexml.side.build.tools.componants.Module;
+import com.bluexml.side.build.tools.reader.ComponantsRegisters;
 import com.bluexml.side.util.dependencies.ModuleConstraint;
 import com.bluexml.side.util.dependencies.PluginsUtils;
 
 public class Builder {
 
-	public static List<ModuleConstraint> getModulesConstraint(File rootPlugins) throws Exception {
-
-		List<ModuleConstraint> moduleIds = new ArrayList<ModuleConstraint>();
-
-		// search in folder to get plugin.xml files
-
-		List<File> lPlug = PluginsUtils.getPluginXML(rootPlugins);
-		for (File file : lPlug) {
-			// search <moduleDependency>.*<moduleDependency>
-			try {
-				Document pluginXML = PluginsUtils.buildJdomDocument(file);
-				System.out.println("Scanning File :" + file);
-				XPath xpa = XPath.newInstance("//moduleDependence");
-				List<?> lmd = xpa.selectNodes(pluginXML.getRootElement());
-				for (Object object : lmd) {
-					Element el = (Element) object;
-					String id = null;
-					String tech_version = null;
-					String moduleType = null;
-					String versionNumMin = null;
-					String versionNumMax = null;
-
-					Attribute moduleId = el.getAttribute("moduleId");
-					if (moduleId != null) {
-						id = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("moduleType");
-					if (moduleId != null) {
-						moduleType = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("technologyVersion");
-					if (moduleId != null) {
-						tech_version = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("versionMax");
-					if (moduleId != null) {
-						versionNumMax = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("versionMin");
-					if (moduleId != null) {
-						versionNumMin = moduleId.getValue();
-					}
-
-					ModuleConstraint mc = new ModuleConstraint(id, tech_version, moduleType, versionNumMin, versionNumMax);
-					System.out.println("found :" + mc);
-					moduleIds.add(mc);
-				}
-
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-		return moduleIds;
+	private static void displayUsage() {
+		System.out.println("Usage : repositoryBuilderForSIDE.jar <action> <pluginsSRCHome> [<action arg>]*");
+		System.out.println(" <action> : [patchPom | makeDot]?");
+		System.out.println("          * patchPom :");
+		System.out.println("               - <pluginsSRCHome> : directory containing Eclipse plugins");
+		System.out.println("               - pom file path");
+		System.out.println("               - [product file path] to use to search modules following product/feature/plugin/ext");
+		System.out.println("          * makeDot :");
+		System.out.println("               - <pluginsSRCHome> : directory containing Eclipse plugins");
+		System.out.println("               - dot file path to output");
+		System.out.println("               - [product file path] to use to search modules following product/feature/plugin/ext");
 	}
 
 	public static void main(String[] args) {
-		if (args == null || args.length == 0) {
-			displayUsage();
-		}
-		File rootPlugins = getPluginRepo(args);
 		try {
-			switch (args.length) {
-			case 5:
-				// two option activated
-				String option2 = args[3];
-				String output2 = args[4];
-				applyOptionalJob(rootPlugins, option2, output2);
-			case 3:
-				// only one option activated
-				String option = args[1];
-				String output = args[2];
-				applyOptionalJob(rootPlugins, option, output);
-				break;
-			default:
+			boolean argsError = false;
+			if (args == null || args.length == 0) {
 				displayUsage();
-			}
+			} else {
+				String action = args[0];
+				if (action.equals("patchPom")) {
+					if (args.length > 3) {
+						File src = getFile(args[1]);
+						File pomPath = getFile(args[2]);
 
+						if (args.length > 3) {
+							File product = getFile(args[3]);
+							File properties = null;
+							if (args.length == 5) {
+								properties = getFile(args[4]);
+							}
+							pathPom(src, pomPath, product, properties);
+						} else {
+							pathPom(src, pomPath);
+						}
+
+					} else {
+						argsError = true;
+					}
+
+				} else if (action.equals("makeDot")) {
+					File src = getFile(args[1]);
+					String filePath = args[2];
+					writeDotFile(src, filePath);
+					System.out.println("graph dot file generated :" + filePath);
+				}
+
+				if (argsError) {
+					displayUsage();
+				}
+
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
+			displayUsage();
 		}
-
 	}
 
-	private static File getPluginRepo(String[] args) {
-		File rootPlugins = new File(args[0]);
+	private static File getFile(String path) throws FileNotFoundException {
+		File rootPlugins = new File(path);
 		if (!rootPlugins.exists()) {
-			System.err.println("File do not exist :" + rootPlugins);
+			throw new FileNotFoundException("File do not exist :" + rootPlugins);
 		}
 		return rootPlugins;
 	}
 
-	private static void applyOptionalJob(File rootPlugins, String option, String filePath) throws Exception, IOException {
-		if (option.equals("-makeDot")) {
-			writeDotFile(rootPlugins, filePath);
-			System.out.println("graph dot file generated :" + filePath);
-		} else if (option.equals("-patchPom")) {
-			List<ModuleConstraint> res = Builder.getModulesConstraint(rootPlugins);
-			// add dependency plugin used by SIDE
-			ModuleConstraint McForMavenDependeciesPlugins = new ModuleConstraint("org.apache.maven.plugins.maven-dependency-plugin", null, "maven-plugin", "2.2", "2.2");
-			res.add(McForMavenDependeciesPlugins);
-			// amp archetype and ampMojo
-			ModuleConstraint McForArchetype_amp = new ModuleConstraint("com.bluexml.side.Framework.maven.ampArchetypeForSide", null, "jar", "1.0.5", "1.0.5");
-			res.add(McForArchetype_amp);
-			ModuleConstraint McMojo_amp = new ModuleConstraint("com.bluexml.side.Integration.m2.ampMojo", null, "jar", "1.0.15", "1.0.15");
-			res.add(McMojo_amp);
-
-			// zip (share) archetype
-			ModuleConstraint McForArchetype_zip = new ModuleConstraint("com.bluexml.side.Framework.maven.warPatchArchetypeForSide", null, "jar", "1.0.5", "1.0.5");
-			res.add(McForArchetype_zip);
-			ModuleConstraint McMojo_zip = new ModuleConstraint("com.bluexml.side.Integration.m2.zipPackage", null, "jar", "1.0.19", "1.0.19");
-			res.add(McMojo_zip);
-
-			File file = new File(filePath);
-			if (!file.exists()) {
-				System.err.println("File do not exist :" + file);
-			}
-			List<File> poms = new ArrayList<File>();
-			for (ModuleConstraint moduleConstraint : res) {
-				File destFile = new File(file.getParent(), res.indexOf(moduleConstraint) + "_.pom");
-				FileUtils.copyFile(file, destFile);
-				List<ModuleConstraint> res_ = new ArrayList<ModuleConstraint>();
-				res_.add(moduleConstraint);
-				writePomFile(destFile, res_);
-				System.out.println("pom Patched :" + destFile);
-				poms.add(destFile);
-			}
-			// write pom.sh script that call go-offline for each patched poms
-			Builder.writeShellScript(new File(file.getParent(), "pom.sh"), poms);
-		}
+	private static void pathPom(File rootPlugins, File pomFile, File product, File properties) throws Exception, IOException, JDOMException, FileNotFoundException {
+		System.out.println("rootPlugins :" + rootPlugins);
+		System.out.println("pomFile :" + pomFile);
+		System.out.println("product :" + product);
+		System.out.println("properties :" + properties);
+		List<ModuleConstraint> res = Builder.getModulesConstraint(rootPlugins, product, properties);
+		writeScript(pomFile, res);
 	}
 
-	private static void displayUsage() {
-		System.out.println("Usage : repositoryBuilderForSIDE.jar <pluginsSRCHome> [-patchPom <pom.xml>] [-makeDot <output dot file>]");
-		System.out.println(" <pluginsSRCHome> : directory containing Eclipse plugins");
-		System.out.println(" <pom.xml>        : pom file to patch");
-		System.out.println(" -makeDot         : option to generate a graph dot file");
-		System.out.println(" <output dot file>: the output dot file (created or overriten)");
+	private static void writeScript(File pomFile, List<ModuleConstraint> res) throws IOException, Exception, JDOMException, FileNotFoundException {
+		// add dependency plugin used by SIDE
+		ModuleConstraint McForMavenDependeciesPlugins = new ModuleConstraint("org.apache.maven.plugins.maven-dependency-plugin", null, "maven-plugin", "2.2", "2.2");
+		res.add(McForMavenDependeciesPlugins);
+		// amp archetype and ampMojo
+		ModuleConstraint McForArchetype_amp = new ModuleConstraint("com.bluexml.side.Framework.maven.ampArchetypeForSide", null, "jar", "1.0.5", "1.0.5");
+		res.add(McForArchetype_amp);
+		ModuleConstraint McMojo_amp = new ModuleConstraint("com.bluexml.side.Integration.m2.ampMojo", null, "jar", "1.0.15", "1.0.15");
+		res.add(McMojo_amp);
+
+		// zip (share) archetype
+		ModuleConstraint McForArchetype_zip = new ModuleConstraint("com.bluexml.side.Framework.maven.warPatchArchetypeForSide", null, "jar", "1.0.5", "1.0.5");
+		res.add(McForArchetype_zip);
+		ModuleConstraint McMojo_zip = new ModuleConstraint("com.bluexml.side.Integration.m2.zipPackage", null, "jar", "1.0.19", "1.0.19");
+		res.add(McMojo_zip);
+
+		if (!pomFile.exists()) {
+			System.err.println("File do not exist :" + pomFile);
+		}
+		List<File> poms = new ArrayList<File>();
+		for (ModuleConstraint moduleConstraint : res) {
+			File destFile = new File(pomFile.getParent(), res.indexOf(moduleConstraint) + "_.pom");
+			FileUtils.copyFile(pomFile, destFile);
+			List<ModuleConstraint> res_ = new ArrayList<ModuleConstraint>();
+			res_.add(moduleConstraint);
+			writePomFile(destFile, res_);
+			System.out.println("pom Patched :" + destFile);
+			poms.add(destFile);
+		}
+		// write pom.sh script that call go-offline for each patched poms
+		Builder.writeShellScript(new File(pomFile.getParent(), "pom.sh"), poms);
+	}
+
+	private static void pathPom(File rootPlugins, File pomFile) throws Exception, IOException, JDOMException, FileNotFoundException {
+		List<ModuleConstraint> res = Builder.getModulesConstraint(rootPlugins);
+		writeScript(pomFile, res);
 	}
 
 	private static void writeDotFile(File rootPlugins, String output) throws Exception, IOException {
@@ -242,5 +219,78 @@ public class Builder {
 		// save close
 		fw.flush();
 		fw.close();
+	}
+
+	public static List<ModuleConstraint> getModulesConstraint(File sources, File product, File properties) throws Exception {
+		List<ModuleConstraint> moduleIds = new ArrayList<ModuleConstraint>();
+
+		List<File> repos = new ArrayList<File>();
+		repos.add(sources);
+		DependencyTree dtree = new DependencyTree(product, repos, properties);
+		ComponantsRegisters componantsRegisters = dtree.getComponantsRegisters();
+		Map<String, Module> modulesRegister = componantsRegisters.getModulesRegister();
+		Collection<Module> values = modulesRegister.values();
+		for (Module module : values) {
+			ModuleConstraint mc = new ModuleConstraint(module.getModuleID(), null, module.getType(), module.getVersion(), module.getVersion());
+			moduleIds.add(mc);
+		}
+		return moduleIds;
+	}
+
+	public static List<ModuleConstraint> getModulesConstraint(File rootPlugins) throws Exception {
+
+		List<ModuleConstraint> moduleIds = new ArrayList<ModuleConstraint>();
+
+		// search in folder to get plugin.xml files
+
+		List<File> lPlug = PluginsUtils.getPluginXML(rootPlugins);
+		for (File file : lPlug) {
+			// search <moduleDependency>.*<moduleDependency>
+			try {
+				Document pluginXML = PluginsUtils.buildJdomDocument(file);
+				System.out.println("Scanning File :" + file);
+				XPath xpa = XPath.newInstance("//moduleDependence");
+				List<?> lmd = xpa.selectNodes(pluginXML.getRootElement());
+				for (Object object : lmd) {
+					Element el = (Element) object;
+					String id = null;
+					String tech_version = null;
+					String moduleType = null;
+					String versionNumMin = null;
+					String versionNumMax = null;
+
+					Attribute moduleId = el.getAttribute("moduleId");
+					if (moduleId != null) {
+						id = moduleId.getValue();
+					}
+					moduleId = el.getAttribute("moduleType");
+					if (moduleId != null) {
+						moduleType = moduleId.getValue();
+					}
+					moduleId = el.getAttribute("technologyVersion");
+					if (moduleId != null) {
+						tech_version = moduleId.getValue();
+					}
+					moduleId = el.getAttribute("versionMax");
+					if (moduleId != null) {
+						versionNumMax = moduleId.getValue();
+					}
+					moduleId = el.getAttribute("versionMin");
+					if (moduleId != null) {
+						versionNumMin = moduleId.getValue();
+					}
+
+					ModuleConstraint mc = new ModuleConstraint(id, tech_version, moduleType, versionNumMin, versionNumMax);
+					System.out.println("found :" + mc);
+					moduleIds.add(mc);
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return moduleIds;
 	}
 }
--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/test.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE/test.sh
@@ -1,14 +1,17 @@
 echo "Build SIDE Labs RCP"
 
 # configuration
-WORKSPACE=/Users/davidabad/Workspace2.0
-SIDEHOME=$WORKSPACE/S-IDE
-BUILDERHOME=$WORKSPACE/buildAllMaven
-POM_PATCHER_PROJECT=$SIDEHOME/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE
-POM_IN=$WORKSPACE/superpom/pom.xml
-MAVENREPO_ARCHIVE_DIR=$SIDEHOME/Util/trunk/com.bluexml.side.Util.dependencies.repository/src/com/bluexml/side/util/dependencies/repository/mavenRepository
-MAVENREPO_ARCHIVE=$MAVENREPO_ARCHIVE_DIR/m2repositoryForSIDE.zip
+WORKSPACE=/Users/davidabad/.hudson/jobs/SIDEPRODUCTFULLBuild/workspace
+SIDEHOME_DEV=/Users/davidabad/workspaces/Workspace2.0/S-IDE
 
+SIDEHOME=$SIDEHOME_DEV
+#SIDEHOME=$WORKSPACE/S-IDE
+
+BUILDERHOME=$WORKSPACE/buildAllMaven
+POM_PATCHER_PROJECT=$SIDEHOME_DEV/Integration/trunk/com.bluexml.side.Integration.buildHudson/config/repositoryBuilderForSIDE
+POM_IN=$WORKSPACE/superpom/pom.xml
+MAVENREPO_ARCHIVE=m2repositoryForSIDE.zip
+PROPERTIES=$WORKSPACE/buildHundson/config/build.davidabad.build_RCP_All.properties
 
 # set constantes
 POM_PATCHER=$POM_PATCHER_PROJECT/target/repositoryBuilderForSIDE-0.0.1-jar-with-dependencies.jar
@@ -27,14 +30,15 @@ echo POM_OUT=$POM_OUT
 echo POM_IN=$POM_IN
 echo MAVENREPO=$MAVENREPO
 echo MAVENREPO_ARCHIVE=$MAVENREPO_ARCHIVE
+echo PROPERTIES=$PROPERTIES
 echo ==================
 
 # install superpom
 cd $WORKSPACE/superpom
-mvn install
+#mvn install
 # build patcher jar
 cd $POM_PATCHER_PROJECT
-mvn clean package
+#mvn clean package
 
 
 # clean working directories
@@ -43,11 +47,37 @@ mkdir -p $WORKDIR
 mkdir -p $MAVENREPO
 # copy ressources
 cp $POM_PATCHER $WORKDIR
-cp $POM_IN $POM_OUT
-cp $POM_PATCHER_PROJECT/scripts/patcher.sh $WORKDIR
-cd $WORKDIR;
-# start to include in repo some requiremenets that can't be included in same project
-sh patcher.sh $MAVENREPO
+
 # patch superpom file (add as dependencies all extension used by side components)
-echo java -jar $POM_PATCHER $SIDEHOME -patchPom $POM_OUT -makeDot $MAVENREPO_ARCHIVE_DIR/dependencies.dot
-java -jar $POM_PATCHER $SIDEHOME -patchPom $POM_OUT -makeDot $MAVENREPO_ARCHIVE_DIR/dependencies.dot
+echo "search products and build archives for each"
+for f in `find $SIDEHOME -type f -name '*.product'`; do
+  echo "process $f"
+  cd $WORKDIR
+  filename=`basename ${f}`
+  parentname=`dirname ${f}`
+  extension=${filename##*.}
+  filename=${filename%.*}
+  targetDir=$parentname/src/com/bluexml/side/Integration/eclipse/branding/$filename/repository
+  if [ -d $targetDir ] ; then 
+    mkdir -p $filename
+    cd $filename
+    echo "do job for $f"
+    cp $POM_IN .
+    echo "java -jar $POM_PATCHER patchPom $SIDEHOME pom.xml $f $PROPERTIES"
+    java -jar $POM_PATCHER patchPom $SIDEHOME pom.xml $f $PROPERTIES
+    echo "== build the archive=="
+    echo "=== call maven ==="
+    mkdir -p $MAVENREPO
+    touch $MAVENREPO/fake.text
+#    sh pom.sh $MAVENREPO
+    echo "zip repository"
+    target=$targetDir/$MAVENREPO_ARCHIVE
+    cd $MAVENREPO
+    if [ -f $target ] ; then
+    	rm $target	
+    fi
+    zip -r $target .
+  else
+  	echo "no repipository package found $targetDir"
+  fi
+done
