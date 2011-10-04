1aac3da6301123e74a3d2b60707ec22fb5b22daf
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 12:44:16 +0000

    merge to S-IDE
    git-svn-id: http://svn.bluexml.com/private/S-IDE@28133 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Workflow/trunk/com.bluexml.side.Workflow.generator.alfresco.enterprise333/src/com/bluexml/side/workflow/generator/alfresco/enterprise333/WorkflowGeneratorEnterprise.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.workflow.generator.alfresco.enterprise333;
+
+import com.bluexml.side.workflow.generator.alfresco.enterprise.WorkflowGenerator;
+
+public class WorkflowGeneratorEnterprise extends WorkflowGenerator {
+
+}
