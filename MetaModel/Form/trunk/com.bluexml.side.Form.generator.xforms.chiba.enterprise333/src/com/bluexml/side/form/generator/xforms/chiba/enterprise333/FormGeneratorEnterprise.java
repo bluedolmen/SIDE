df95d7f8cc832a86310e37dec8ee4cbc1bfe5d3a
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 12:43:07 +0000

    merge to S-IDE
    git-svn-id: http://svn.bluexml.com/private/S-IDE@28106 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Form/trunk/com.bluexml.side.Form.generator.xforms.chiba.enterprise333/src/com/bluexml/side/form/generator/xforms/chiba/enterprise333/FormGeneratorEnterprise.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.form.generator.xforms.chiba.enterprise333;
+
+import com.bluexml.side.forms.generator.alfresco.chiba.enterprise.FormGenerator;
+
+public class FormGeneratorEnterprise extends FormGenerator {
+
+}
