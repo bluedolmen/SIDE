2273fb61de56eb4b46476356b1c59ac015a2eb24
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 12:31:04 +0000

    merge to S-IDE
    git-svn-id: http://svn.bluexml.com/private/S-IDE@28002 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Deployer/trunk/com.bluexml.side.Deployer.alfresco.enterprise333/src/com/bluexml/side/deployer/alfresco/enterprise333/Deployer.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.deployer.alfresco.enterprise333;
+
+import com.bluexml.side.deployer.alfresco.enterprise.AlfrescoEnterpriseDeployer;
+
+public class Deployer extends AlfrescoEnterpriseDeployer {
+
+}
