6a1f352eccf822d265ecdb742faeaaa15910c7fe
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 13:58:31 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@28145 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 # Prepare maven workspace and launch maven deploy
 # 1st parameter =  workspace path of the buildHuson project
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ]; then
   WORKSPACE=$1
-  REPOSITORY_COPY=$2
 else
-  echo "Usage: launch_maven.sh WORKSPACE REPOSITORY_COPY"
+  echo "Usage: launch_maven.sh WORKSPACE"
   echo "       with WORKSPACE =  workspace path of the buildHuson project"
-  echo "       with REPOSITORY_COPY =  workspace path of SIDE sources (certainly equals to WORKSPACE)"
   exit -2
 fi
 
 if [ -f launch_maven_subProcess.sh ]; then
 	chmod +x launch_maven_subProcess.sh
-	mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
-	cp -R $REPOSITORY_COPY/S-IDE/Integration/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace
-	chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_mojo_workspace	
+	mkdir -p $WORKSPACE/work/maven_mojo_workspace
+	cp -R $WORKSPACE/S-IDE/Integration/trunk/* $WORKSPACE/work/maven_mojo_workspace
+	chmod -R 755 $WORKSPACE/work/maven_mojo_workspace	
 	
 	echo "deploy mojos"
 	./launch_maven_subProcess.sh $WORKSPACE maven_mojo_workspace
 	
-	mkdir -p $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	mkdir -p $WORKSPACE/work/maven_workspace
 
-	cp -R $REPOSITORY_COPY/S-IDE/FrameworksModules/trunk/* $WORKSPACE/../buildAuto/Ankle/maven_workspace
-	cp -R $REPOSITORY_COPY/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/../buildAuto/Ankle/maven_workspace
-	chmod -R 755 $WORKSPACE/../buildAuto/Ankle/maven_workspace
+	cp -R $WORKSPACE/S-IDE/FrameworksModules/trunk/* $WORKSPACE/work/maven_workspace
+	cp -R $WORKSPACE/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/work/maven_workspace
+	chmod -R 755 $WORKSPACE/work/maven_workspace
 	
 	echo "deploy other m2 projects"	
 	./launch_maven_subProcess.sh $WORKSPACE maven_workspace
 
 else
   echo "Usage: launch_maven_subProcess must exist"
+  exit -2
 fi
-
-
-
-
-
-
--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven_subProcess.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven_subProcess.sh
@@ -11,42 +11,45 @@ else
   exit -2
 fi
 
-
-cp $WORKSPACE/../buildAuto/Ankle/delete_folder.pl $WORKSPACE/../buildAuto/Ankle/$WORKING_DIR
-cd $WORKSPACE/../buildAuto/Ankle/$WORKING_DIR
-perl delete_folder.pl pom.xml
-cp $WORKSPACE/superpom/pom.xml .
-
-echo "update superpom"
-perl -pi -le 'print "<modules>" if $. == 12' pom.xml
-perl -pi -le 'print "</modules>" if $. == 13' pom.xml
-for s in `ls -d */ `; do
-    repertoire=$s
-	name=`perl -e '"'$repertoire'"=~/([a-zA-Z0-9._]*)/; print $1';`
-  	res="<module>$name</module>"
-  	echo $repertoire $name $res
-	perl -pi -le 'print "'$res'" if $. == 13' pom.xml
-done
-echo "superpom patched"
-echo "Maven deploy start"
-mvn clean deploy -e -P public > log_maven.log
-echo "Maven deploy done"
-
-sleep 5m
-
-tail -100 log_maven.log
-
-if grep "BUILD SUCCESSFUL" log_maven.log ; then
-	echo "Maven deploy success"
-else
-
-	if grep "ERROR" log_maven.log ; then	
-		echo "Maven deploy failed"
-		exit 1
-	else
-		echo "Maven deploy no error"
-	fi
+if [ -f delete_folder.pl ]; then
+	cp delete_folder.pl $WORKSPACE/work/$WORKING_DIR
+	cd $WORKSPACE/work/$WORKING_DIR
+	perl delete_folder.pl pom.xml
+	cp $WORKSPACE/superpom/pom.xml .
 	
-fi
+	echo "update superpom"
+	perl -pi -le 'print "<modules>" if $. == 12' pom.xml
+	perl -pi -le 'print "</modules>" if $. == 13' pom.xml
+	for s in `ls -d */ `; do
+	    repertoire=$s
+		name=`perl -e '"'$repertoire'"=~/([a-zA-Z0-9._]*)/; print $1';`
+	  	res="<module>$name</module>"
+	  	echo $repertoire $name $res
+		perl -pi -le 'print "'$res'" if $. == 13' pom.xml
+	done
+	echo "superpom patched"
+	echo "Maven deploy start"
+	mvn clean deploy -e -P public > log_maven.log
+	echo "Maven deploy done"
+	
+	sleep 5m
+	
+	tail -100 log_maven.log
+	
+	if grep "BUILD SUCCESSFUL" log_maven.log ; then
+		echo "Maven deploy success"
+	else
+	
+		if grep "ERROR" log_maven.log ; then	
+			echo "Maven deploy failed"
+			exit -2
+		else
+			echo "Maven deploy no error"
+		fi		
+	fi	
+	exit 0
 
-exit 0
+else
+   echo "Usage: delete_folder.pl must exist"
+   exit -2
+fi
