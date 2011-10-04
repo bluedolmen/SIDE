26c0debe8a07b62ec3ead97bc8713f64927d096e
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 12:42:02 +0000

    merge to S-IDE
    git-svn-id: http://svn.bluexml.com/private/S-IDE@28089 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Class/trunk/com.bluexml.side.Class.generator.alfresco.enterprise333/src/com/bluexml/side/clazz/generator/alfresco/enterprise333/AlfrescoEnterpriseGenerator.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.clazz.generator.alfresco.enterprise333;
+
+import com.bluexml.side.clazz.generator.alfresco.enterprise.AlfrescoGenerator;
+
+public class AlfrescoEnterpriseGenerator extends AlfrescoGenerator {
+
+}
