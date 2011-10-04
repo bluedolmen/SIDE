b0e3acf49df344e74040fca370430375d0b2e1ae
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 14:06:55 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@28147 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Extension/MetaModel/Class/trunk/Alfresco/Reverse/src/models/Alfresco/3.2Labs/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cat *.xml > allModels.xml
-echo '<models>' | cat - allModels.xml > allModels.xml.1
-cat allModels.xml.1 - | echo '</models>'  > allModels.xml.2
-mv allModels.xml.2 allModels.xml
-rm allModels.xml.1
