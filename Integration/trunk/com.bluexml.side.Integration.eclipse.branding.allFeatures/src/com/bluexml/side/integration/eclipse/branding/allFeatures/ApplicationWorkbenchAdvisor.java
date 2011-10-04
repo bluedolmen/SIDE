e75a64802e14ae88bb65edbf73e67c9738af5c44
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-04 12:40:36 +0000

    merge to S-IDE
    git-svn-id: http://svn.bluexml.com/private/S-IDE@28080 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Integration/trunk/com.bluexml.side.Integration.eclipse.branding.allFeatures/src/com/bluexml/side/integration/eclipse/branding/allFeatures/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.bluexml.side.integration.eclipse.branding.allFeatures;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.bluexml.side.Integration.eclipse.allFeatures.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ Integration/trunk/com.bluexml.side.Integration.eclipse.branding.allFeatures/src/com/bluexml/side/integration/eclipse/branding/allFeatures/Perspective.java
@@ -0,0 +1,10 @@
+package com.bluexml.side.integration.eclipse.branding.allFeatures;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
