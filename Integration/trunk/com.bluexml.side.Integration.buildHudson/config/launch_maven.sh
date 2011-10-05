dbff56611d2c1dcd9d65401ead54f6df0def1100
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-10-05 14:29:59 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@28795 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/launch_maven.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Prepare maven workspace and launch maven deploy
-# 1st parameter =  workspace path of the buildHuson project
-if [ $# -eq 1 ]; then
+# 1st parameter =  directory where do the jobs
+# 2nd parameter = directory where found SIDE sources (workspace/S-IDE)
+if [ $# -eq 2 ]; then
   WORKSPACE=$1
+  SIDE_SRC=$2
 else
   echo "Usage: launch_maven.sh WORKSPACE"
   echo "       with WORKSPACE =  workspace path of the buildHuson project"
@@ -11,18 +13,18 @@ fi
 
 if [ -f launch_maven_subProcess.sh ]; then
 	chmod +x launch_maven_subProcess.sh
-	mkdir -p $WORKSPACE/work/maven_mojo_workspace
-	cp -R $WORKSPACE/S-IDE/Integration/trunk/* $WORKSPACE/work/maven_mojo_workspace
-	chmod -R 755 $WORKSPACE/work/maven_mojo_workspace	
+	mkdir -p $WORKSPACE/maven_mojo_workspace
+	cp -R $SIDE_SRC/Integration/trunk/* $WORKSPACE/maven_mojo_workspace
+	chmod -R 755 $WORKSPACE/maven_mojo_workspace	
 	
 	echo "deploy mojos"
 	./launch_maven_subProcess.sh $WORKSPACE maven_mojo_workspace
 	
-	mkdir -p $WORKSPACE/work/maven_workspace
+	mkdir -p $WORKSPACE/maven_workspace
 
-	cp -R $WORKSPACE/S-IDE/FrameworksModules/trunk/* $WORKSPACE/work/maven_workspace
-	cp -R $WORKSPACE/S-IDE/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/work/maven_workspace
-	chmod -R 755 $WORKSPACE/work/maven_workspace
+	cp -R $SIDE_SRC/FrameworksModules/trunk/* $WORKSPACE/maven_workspace
+	cp -R $SIDE_SRC/MetaModel/Form/trunk/com.bluexml.side.Form.XFormsUtils $WORKSPACE/maven_workspace
+	chmod -R 755 $WORKSPACE/maven_workspace
 	
 	echo "deploy other m2 projects"	
 	./launch_maven_subProcess.sh $WORKSPACE maven_workspace
