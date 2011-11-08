9407b9e62de0338039e3f48aebf5b7412eb3e948
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-11-08 14:25:48 +0000

    SIDE Labs update : only include alfresco 3.4d support
    git-svn-id: http://svn.bluexml.com/private/S-IDE@29706 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Class/trunk/com.bluexml.side.Class.alfresco.models.library/src/com/bluexml/side/clazz/alfresco/models/library/AlfrescoModelBaseVersionChooser.java
@@ -0,0 +1,32 @@
+package com.bluexml.side.clazz.alfresco.models.library;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.swt.widgets.Composite;
+
+import com.bluexml.side.clazz.alfresco.models.library.ModelLibrary.Libraries;
+import com.bluexml.side.util.libs.eclipse.wizards.AbstractFieldsPage;
+
+public abstract class AlfrescoModelBaseVersionChooser extends AbstractFieldsPage {
+
+	protected AlfrescoModelBaseVersionChooser(String pageName) {
+		super(pageName);
+	}
+
+	protected void createAlfrescoLibComboBox(Composite composite,String fieldId) {
+		Map<String, String> libs = new TreeMap<String, String>();
+		Libraries[] values2 = ModelLibrary.Libraries.values();
+		for (Libraries libraries : values2) {
+			String l1 = libraries.toString();
+			libs.put(l1, l1);			
+		}
+		libs.put("", "");
+		
+		ArrayList<String> allowedValues = new ArrayList<String>();
+		allowedValues.addAll(libs.values());
+
+		createComboControl(composite, "model library", fieldId, allowedValues);
+	}
+}
