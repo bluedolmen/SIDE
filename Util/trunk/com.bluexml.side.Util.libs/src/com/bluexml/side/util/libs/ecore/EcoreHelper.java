9a12f7c246bd368c87097b111e9077c99f5bb4eb
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-11-28 17:40:25 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@30562 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Util/trunk/com.bluexml.side.Util.libs/src/com/bluexml/side/util/libs/ecore/EcoreHelper.java
+++ Util/trunk/com.bluexml.side.Util.libs/src/com/bluexml/side/util/libs/ecore/EcoreHelper.java
@@ -1,13 +1,19 @@
 package com.bluexml.side.util.libs.ecore;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.Diagnostician;
 
+import com.bluexml.side.util.libs.IFileHelper;
+
 public class EcoreHelper {
 
-	
 	/**
 	 * Launch validation on given EObject
 	 * 
@@ -23,5 +29,28 @@ public class EcoreHelper {
 			result = false;
 		}
 		return result;
+	}
+
+	public static String getFileContent(EObject o, String filePath) {
+		String content = "";
+		try {
+			// try to load from FS (not a good idea to use absolute path ....)
+			File f = new File(filePath);
+			if (!f.exists()) {
+				// try to get File from eclipse workspace
+				IFile iFile = IFileHelper.getIFile(filePath);
+				f = IFileHelper.convertIRessourceToFile(iFile);
+				if (!f.exists()) {
+					// mmm
+					f = IFileHelper.getFile(iFile);
+				}
+			}
+			content = FileUtils.readFileToString(f);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return content;
 	}
 }
