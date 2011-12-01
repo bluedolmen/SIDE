4084ff3112d9463dd750f99d04af8181e0a35e9f
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-12-01 10:36:33 +0000

    Fix filter configuration loading
    git-svn-id: http://svn.bluexml.com/private/S-IDE@30662 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.shareDoclist34d/src/main/resources/WEB-INF/classes/alfresco/web-extension/site-webscripts/com/bluexml/components/sesamvitaleFilter1/sesamvitaleFilter1.get.js
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.shareDoclist34d/src/main/resources/WEB-INF/classes/alfresco/web-extension/site-webscripts/com/bluexml/components/sesamvitaleFilter1/sesamvitaleFilter1.get.js
@@ -5,18 +5,25 @@
  */
 function main() {
 	var filters = [];
-	 
-	      logger.log("filter componant arg :" + args);
-	      logger.log("filter componant template :" + template);
-	 
+
+	logger.log("filter componant arg :" + args);
+	logger.log("filter componant template :" + template);
+
 	if (args.config != null) {
 		var conf = eval('(' + unescape(args.config) + ')');
-		
-		for ( var c = 0; c < conf.length; c++) {			
-			filters.push(conf[c]);
+
+		for ( var c = 0; c < conf.length; c++) {
+			var conf = conf[c];
+
+			filters.push({
+				id : conf.id,
+				label : conf.label,
+				data : conf.data.toSource()
+			});
 		}
 	}
 
+	logger.log("filters :" + filters.toSource());
 	model.filters = filters;
 }
 
