208d0803a87b2fbad6e7d1174a07e318fc1165d9
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2011-12-14 14:10:50 +0000

    replace sysout by logger
    git-svn-id: http://svn.bluexml.com/private/S-IDE@31039 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.share.formExtension34d/src/main/java/com/bluexml/side/Framework/alfresco/share/formExtension/MyConfigBootStrap.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.share.formExtension34d/src/main/java/com/bluexml/side/Framework/alfresco/share/formExtension/MyConfigBootStrap.java
@@ -2,12 +2,14 @@ package com.bluexml.side.Framework.alfresco.share.formExtension;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.extensions.config.ConfigBootstrap;
 import org.springframework.extensions.config.ConfigDeployment;
 import org.springframework.extensions.config.source.UrlConfigSource;
 
 public class MyConfigBootStrap extends ConfigBootstrap {
-
+	static Log logger = LogFactory.getLog(MyConfigBootStrap.class);
 	UrlConfigSource urlConfigSource = null;
 
 	public UrlConfigSource getUrlConfigSource() {
@@ -19,7 +21,9 @@ public class MyConfigBootStrap extends ConfigBootstrap {
 	}
 
 	public List<ConfigDeployment> initConfig() {
-		System.out.println("MyConfigBootStrap.initConfig()");
+		if (logger.isDebugEnabled()) {
+			logger.debug("MyConfigBootStrap.initConfig()");
+		}
 		return configService.appendConfig(urlConfigSource);
 	}
 }
