58bf858e8fe7b2a7e53855db0a802cb82e6fcbae
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-06 09:44:35 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=2054 (alfresco 3.4.6 EE)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@31586 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Workflow/com.bluexml.side.Workflow.generator.alfresco346E/src/com/bluexml/side/workflow/generator/alfresco346e/WorkflowGenerator346e.java
@@ -0,0 +1,8 @@
+package com.bluexml.side.workflow.generator.alfresco346e;
+
+import com.bluexml.side.workflow.generator.alfresco34d.WorkflowGeneratorCommunity;
+
+public class WorkflowGenerator346e extends WorkflowGeneratorCommunity {
+
+	
+}
