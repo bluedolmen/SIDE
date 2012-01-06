a865a2ff10a19e51fd4ddba8878af073d4d8f8a9
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-06 09:41:04 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=2054 (alfresco 3.4.6 EE)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@31580 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Form/trunk/com.bluexml.side.Form.generator.xforms.chiba346E/src/com/bluexml/side/form/generator/xforms/chiba346e/FormGenerator346E.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.form.generator.xforms.chiba346e;
+
+import com.bluexml.side.forms.generator.alfresco.chiba.FormGenerator;
+
+public class FormGenerator346E extends FormGenerator {
+
+}
