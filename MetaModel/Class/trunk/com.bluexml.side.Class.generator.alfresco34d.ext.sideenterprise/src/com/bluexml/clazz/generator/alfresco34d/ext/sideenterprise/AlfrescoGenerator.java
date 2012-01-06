f4b26c910e35960f0495c0976c2a4fe513a99742
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-06 12:35:04 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=2054 (alfresco 3.4.6 EE)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@31643 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- MetaModel/Class/trunk/com.bluexml.side.Class.generator.alfresco34d.ext.sideenterprise/src/com/bluexml/clazz/generator/alfresco34d/ext/sideenterprise/AlfrescoGenerator.java
+++ MetaModel/Class/trunk/com.bluexml.side.Class.generator.alfresco34d.ext.sideenterprise/src/com/bluexml/clazz/generator/alfresco34d/ext/sideenterprise/AlfrescoGenerator.java
@@ -1,6 +1,8 @@
 package com.bluexml.clazz.generator.alfresco34d.ext.sideenterprise;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.bluexml.side.util.security.SecurityHelper;
 import com.bluexml.side.util.security.preferences.SidePreferences;
@@ -31,20 +33,13 @@ public class AlfrescoGenerator extends com.bluexml.side.clazz.generator.alfresco
 		// define configuration for advancedSearch
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/custom-share-config.xml.mt"); //$NON-NLS-1$
 
-		// 3.4.d specific
-		templates.remove("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model.mt");
-		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/model/alfrescoGenerator_model.mt");
-
-		templates.remove("/com.bluexml.side.Class.generator.alfresco/templates/alfrescoshare/uploadForm/flash-upload.get.html.ftl.mt");
-		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/upload/flash-upload.get.html.ftl.mt");
-
 		// i18n messages
-		templates.remove("/com.bluexml.side.Class.generator.alfresco/templates/alfrescoshare/defaultdocListView/documentlist.get.properties.mt"); //$NON-NLS-1$
+
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get_de.properties.mt"); //$NON-NLS-1$
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get_es.properties.mt"); //$NON-NLS-1$
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get_fr.properties.mt"); //$NON-NLS-1$
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get_it.properties.mt"); //$NON-NLS-1$
-		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get.properties.mt"); //$NON-NLS-1$
+
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/changetype/change-type.get_de.properties.mt");
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/changetype/change-type.get_es.properties.mt");
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/changetype/change-type.get_fr.properties.mt");
@@ -54,6 +49,22 @@ public class AlfrescoGenerator extends com.bluexml.side.clazz.generator.alfresco
 		templates.add("/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/messages/slingshot.properties.mt");
 
 		return templates;
+	}
+
+	@Override
+	protected List<Map<String, String>> getTemplatesSubstitution() {
+		List<Map<String, String>> templatesSubstitution = super.getTemplatesSubstitution();
+		Map<String, String> map = new HashMap<String, String>();
+		// 3.4.d specific
+		map.put("/com.bluexml.side.Class.generator.alfresco/templates/Model/alfrescoGenerator_model.mt", "/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/model/alfrescoGenerator_model.mt");
+
+		map.put("/com.bluexml.side.Class.generator.alfresco/templates/alfrescoshare/uploadForm/flash-upload.get.html.ftl.mt", "/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/upload/flash-upload.get.html.ftl.mt");
+
+		// i18n messages
+		map.put("/com.bluexml.side.Class.generator.alfresco/templates/alfrescoshare/defaultdocListView/documentlist.get.properties.mt", "/com.bluexml.side.Class.generator.alfresco34d/com/bluexml/side/clazz/generator/alfresco34d/templates/alfrescoshare/doclist/documentlist.get.properties.mt"); //$NON-NLS-1$
+		
+		templatesSubstitution.add(map);
+		return templatesSubstitution;
 	}
 
 }
