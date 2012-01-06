2ba0c8b89429009a0aa1035c3edd0eee7afb46dc
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-06 09:35:56 +0000

    http://bugs.bluexml.net/show_bug.cgi?id=2054 (alfresco 3.4.6 EE)
    git-svn-id: http://svn.bluexml.com/private/S-IDE@31576 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ Deployer/trunk/com.bluexml.side.Deployer.alfrescoDirectCopy346E/src/com/bluexml/side/deployer/alfrescodirectcopy346e/DirectDeployer.java
@@ -0,0 +1,18 @@
+package com.bluexml.side.deployer.alfrescodirectcopy346e;
+
+import com.bluexml.side.deployer.alfresco.directcopy.AlfrescoHotDeployer;
+
+public class DirectDeployer extends AlfrescoHotDeployer {
+
+	public DirectDeployer() {
+		super();
+		this.cleanKey = "com.bluexml.side.Deployer.alfrescoDirectCopy346e.clean";
+		this.logChangesKey = "";
+	}
+
+
+	@Override
+	public boolean check() {
+		return true;
+	}
+}
