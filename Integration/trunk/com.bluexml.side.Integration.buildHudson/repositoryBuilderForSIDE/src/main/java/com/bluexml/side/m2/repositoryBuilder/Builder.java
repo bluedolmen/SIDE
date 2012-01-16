bd96b39d0ae959477abfbe90dc290f8da9993de9
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-16 10:35:53 +0000

    Fix pom writing using maven namespace
    git-svn-id: http://svn.bluexml.com/private/S-IDE@32057 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/repositoryBuilderForSIDE/src/main/java/com/bluexml/side/m2/repositoryBuilder/Builder.java
@@ -11,10 +11,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
-import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
+import org.jdom.Namespace;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
@@ -26,6 +26,7 @@ import com.bluexml.side.util.dependencies.ModuleConstraint;
 import com.bluexml.side.util.dependencies.PluginsUtils;
 
 public class Builder {
+	static final String mavenNS = "http://maven.apache.org/POM/4.0.0";
 
 	private static void displayUsage() {
 		System.out.println("Usage : repositoryBuilderForSIDE.jar <action> <pluginsSRCHome> [<action arg>]*");
@@ -159,18 +160,18 @@ public class Builder {
 		XPath xpa = XPath.newInstance("/project/dependencies");
 		Element deps = (Element) xpa.selectSingleNode(pom.getRootElement());
 		if (deps == null) {
-			deps = new Element("dependencies");
+			deps = createMavenElement("dependencies");
 			pom.getRootElement().addContent(deps);
 		}
 		// add dependencies
 		for (ModuleConstraint moduleConstraint : res) {
-			Element dep = new Element("dependency");
+			Element dep = createMavenElement("dependency");
 
-			Element groupId = new Element("groupId").setText(moduleConstraint.getGroupId());
-			Element artifactId = new Element("artifactId").setText(moduleConstraint.getArtifactId());
-			Element version = new Element("version").setText(moduleConstraint.getVersionRange());
-			Element type = new Element("type").setText(moduleConstraint.getModuleType());
-			Element scope = new Element("scope").setText("compile");
+			Element groupId = createMavenElement("groupId").setText(moduleConstraint.getGroupId());
+			Element artifactId = createMavenElement("artifactId").setText(moduleConstraint.getArtifactId());
+			Element version = createMavenElement("version").setText(moduleConstraint.getVersionRange());
+			Element type = createMavenElement("type").setText(moduleConstraint.getModuleType());
+			Element scope = createMavenElement("scope").setText("compile");
 
 			dep.addContent(groupId);
 			dep.addContent(artifactId);
@@ -182,7 +183,7 @@ public class Builder {
 		}
 
 		// change artifactId, so superpom can be resolved
-		Element artifactId = pom.getRootElement().getChild("artifactId");
+		Element artifactId = getMavenChild(pom.getRootElement(), "artifactId");
 		artifactId.setText("sideOfflineRepository");
 
 		// save changes
@@ -254,32 +255,11 @@ public class Builder {
 				List<?> lmd = xpa.selectNodes(pluginXML.getRootElement());
 				for (Object object : lmd) {
 					Element el = (Element) object;
-					String id = null;
-					String tech_version = null;
-					String moduleType = null;
-					String versionNumMin = null;
-					String versionNumMax = null;
-
-					Attribute moduleId = el.getAttribute("moduleId");
-					if (moduleId != null) {
-						id = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("moduleType");
-					if (moduleId != null) {
-						moduleType = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("technologyVersion");
-					if (moduleId != null) {
-						tech_version = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("versionMax");
-					if (moduleId != null) {
-						versionNumMax = moduleId.getValue();
-					}
-					moduleId = el.getAttribute("versionMin");
-					if (moduleId != null) {
-						versionNumMin = moduleId.getValue();
-					}
+					String id = getAttributeValue(el, "moduleId");
+					String tech_version = getAttributeValue(el, "technologyVersion");
+					String moduleType = getAttributeValue(el, "moduleType");
+					String versionNumMin = getAttributeValue(el, "versionMin");
+					String versionNumMax = getAttributeValue(el, "versionMax");
 
 					ModuleConstraint mc = new ModuleConstraint(id, tech_version, moduleType, versionNumMin, versionNumMax);
 					System.out.println("found :" + mc);
@@ -293,5 +273,17 @@ public class Builder {
 
 		}
 		return moduleIds;
+	}
+
+	public static Element createMavenElement(String tag) {
+		return new Element(tag, mavenNS);
+	}
+
+	public static Element getMavenChild(Element parent, String tag) {
+		return parent.getChild(tag, Namespace.getNamespace(mavenNS));
+	}
+
+	public static String getAttributeValue(Element node, String attributeName) {
+		return node.getAttributeValue(attributeName, Namespace.getNamespace(mavenNS));
 	}
 }
