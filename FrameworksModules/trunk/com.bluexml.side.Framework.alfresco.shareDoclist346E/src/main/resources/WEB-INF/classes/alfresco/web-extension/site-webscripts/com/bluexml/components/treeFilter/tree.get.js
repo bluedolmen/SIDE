0331d329232be2cec50fbf15cc089dacea95123d
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-01-30 08:53:19 +0000

    fix default QName
    git-svn-id: http://svn.bluexml.com/private/S-IDE@32324 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.shareDoclist346E/src/main/resources/WEB-INF/classes/alfresco/web-extension/site-webscripts/com/bluexml/components/treeFilter/tree.get.js
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.shareDoclist346E/src/main/resources/WEB-INF/classes/alfresco/web-extension/site-webscripts/com/bluexml/components/treeFilter/tree.get.js
@@ -13,12 +13,12 @@ function main() {
 	if (paramValide("rootName")) {
 		// use built-in SIDE model Aspect if not provided
 
-		model.assoType = paramValide("assoType") ? args.assoType : "sideBuildInLibraryAlfresco:TreeFilter_AssociationTree_TreeFilter";
-		model.nodeType = paramValide("nodeType") ? args.nodeType : "sideBuildInLibraryAlfresco:TreeFilter";
-		model.nodeTypeDocument = paramValide("nodeTypeDocument") ? args.nodeTypeDocument : "sideBuildInLibraryAlfresco:HasTreeFilter";
-		model.assoTypeDocument = paramValide("assoTypeDocument") ? args.assoTypeDocument : "sideBuildInLibraryAlfresco:HasTreeFilter_hasTreeFilter_TreeFiltersearch";
+		model.assoType = paramValide("assoType") ? args.assoType : "buildInLibraryAlfresco:TreeFilter_AssociationTree_TreeFilter";
+		model.nodeType = paramValide("nodeType") ? args.nodeType : "buildInLibraryAlfresco:TreeFilter";
+		model.nodeTypeDocument = paramValide("nodeTypeDocument") ? args.nodeTypeDocument : "buildInLibraryAlfresco:HasTreeFilter";
+		model.assoTypeDocument = paramValide("assoTypeDocument") ? args.assoTypeDocument : "buildInLibraryAlfresco:HasTreeFilter_hasTreeFilter_TreeFiltersearch";
 		model.selectableTypeIsAspect = paramValide("selectableTypeIsAspect") ? args.selectableTypeIsAspect : "true";
-		model.rootProperty = paramValide("rootProperty") ? args.rootProperty : "sideBuildInLibraryAlfresco:TreeFilter_root";
+		model.rootProperty = paramValide("rootProperty") ? args.rootProperty : "buildInLibraryAlfresco:TreeFilter_root";
 		model.rootName = args.rootName;
 	} else {
 		// error
