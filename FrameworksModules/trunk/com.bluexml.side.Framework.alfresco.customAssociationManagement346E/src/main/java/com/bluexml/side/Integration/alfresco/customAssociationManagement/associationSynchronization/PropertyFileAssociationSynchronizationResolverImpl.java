71ddafc572fc0ed8be4c49c0e2b499adf962a997
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-02-21 18:06:20 +0000

    Enhance AssociationSynchronization now configuration is namespace aware
    git-svn-id: http://svn.bluexml.com/private/S-IDE@33025 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.customAssociationManagement346E/src/main/java/com/bluexml/side/Integration/alfresco/customAssociationManagement/associationSynchronization/PropertyFileAssociationSynchronizationResolverImpl.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.customAssociationManagement346E/src/main/java/com/bluexml/side/Integration/alfresco/customAssociationManagement/associationSynchronization/PropertyFileAssociationSynchronizationResolverImpl.java
@@ -1,14 +1,26 @@
 package com.bluexml.side.Integration.alfresco.customAssociationManagement.associationSynchronization;
 
+import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.namespace.QName;
 
 import com.bluexml.side.framework.alfresco.commons.configurations.PropertiesConfiguration;
 
 public class PropertyFileAssociationSynchronizationResolverImpl extends PropertiesConfiguration implements AssociationSynchronizationResolver {
 
+	ServiceRegistry serviceRegistry;
+
+	public ServiceRegistry getServiceRegistry() {
+		return serviceRegistry;
+	}
+
+	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
+	}
+
 	public QName resolve(QName associationQName) {
-		if (dictionary.containsKey(associationQName.getLocalName())) {
-			return QName.createQName(associationQName.getNamespaceURI(), dictionary.get(associationQName.getLocalName()));
+		String prefixString = associationQName.toPrefixString(this.serviceRegistry.getNamespaceService());
+		if (dictionary.containsKey(prefixString)) {
+			return QName.createQName(dictionary.get(prefixString), this.serviceRegistry.getNamespaceService());
 		} else {
 			return null;
 		}
