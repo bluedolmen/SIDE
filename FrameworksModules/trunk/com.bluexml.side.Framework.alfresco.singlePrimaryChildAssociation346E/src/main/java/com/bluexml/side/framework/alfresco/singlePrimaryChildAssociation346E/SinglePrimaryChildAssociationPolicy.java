0cd7e02f662548abdfc2c8f277dfa5751fd6fca5
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-04-03 16:45:16 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@34792 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.singlePrimaryChildAssociation346E/src/main/java/com/bluexml/side/framework/alfresco/singlePrimaryChildAssociation346E/SinglePrimaryChildAssociationPolicy.java
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.singlePrimaryChildAssociation346E/src/main/java/com/bluexml/side/framework/alfresco/singlePrimaryChildAssociation346E/SinglePrimaryChildAssociationPolicy.java
@@ -6,6 +6,8 @@ import org.alfresco.repo.policy.Behaviour;
 import org.alfresco.repo.policy.Behaviour.NotificationFrequency;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.model.FileInfo;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -47,42 +49,66 @@ public class SinglePrimaryChildAssociationPolicy implements OnCreateChildAssocia
 	}
 
 	public void onCreateChildAssociation(ChildAssociationRef childAssocRef, boolean isNewNode) {
-		NodeRef childRef = childAssocRef.getChildRef();
-		NodeRef parentRef = childAssocRef.getParentRef();
-		QName typeC = getNodeService().getType(childRef);
-		QName typeP = getNodeService().getType(parentRef);
-		boolean hasValue = false;
-		boolean hasValueC = configuration.hasValue(typeC.toPrefixString(this.getNamespaceService()));
-		boolean hasValueP = configuration.hasValue(typeP.toPrefixString(this.getNamespaceService()));
-		if (hasValueC || hasValueP) {
-			// check value
-			if (hasValueC) {
-				hasValue = configuration.getAsBooleanValue(typeC.toPrefixString(this.getNamespaceService()));
-			} else {
-				hasValue = configuration.getAsBooleanValue(typeP.toPrefixString(this.getNamespaceService()));
-			}
-		}
-		QName typeQName = childAssocRef.getTypeQName();
-		if (!isNewNode && hasValue && typeQName.equals(ContentModel.ASSOC_CONTAINS)) {
-
-			if (!childAssocRef.isPrimary()) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Policy move primary to the new child-association ...");
+		try {
+			logger.debug("SinglePrimaryChildAssociationPolicy.onCreateChildAssociation()");
+			NodeRef childRef = childAssocRef.getChildRef();
+			NodeRef parentRef = childAssocRef.getParentRef();
+			QName typeC = getNodeService().getType(childRef);
+			QName typeP = getNodeService().getType(parentRef);
+			logger.debug("\tisNewNode :" + isNewNode);
+			logger.debug("\tchildRef :" + childRef);
+			logger.debug("\tparentRef :" + parentRef);
+			logger.debug("\ttypeC :" + typeC);
+			logger.debug("\ttypeP" + typeP);
+			boolean hasValue = false;
+			boolean hasValueC = configuration.hasValue(typeC.toPrefixString(this.getNamespaceService()));
+			boolean hasValueP = configuration.hasValue(typeP.toPrefixString(this.getNamespaceService()));
+			if (hasValueC || hasValueP) {
+				// check value
+				if (hasValueC) {
+					hasValue = configuration.getAsBooleanValue(typeC.toPrefixString(this.getNamespaceService()));
+				} else {
+					hasValue = configuration.getAsBooleanValue(typeP.toPrefixString(this.getNamespaceService()));
 				}
-				ChildAssociationRef primaryParent = getNodeService().getPrimaryParent(childRef);
-
-				// to avoid constraints error (two same association between parent -> child) wee need to remove the current childAssociation before to move
-				getNodeService().removeChildAssociation(childAssocRef);
-
-				// need to move the primary association and remove the current childAssociation
-
-				getNodeService().moveNode(childRef, parentRef, ContentModel.ASSOC_CONTAINS, primaryParent.getQName());
 			}
+			QName typeQName = childAssocRef.getTypeQName();
+			logger.debug("\tassoType :" + typeQName);
+			if (!isNewNode && hasValue && typeQName.equals(ContentModel.ASSOC_CONTAINS)) {
 
-		} else {
-			// do nothing this is the call that create the initial primary association
+				if (!childAssocRef.isPrimary()) {
+					if (logger.isDebugEnabled()) {
+						logger.debug("Policy move primary to the new child-association ...");
+					}
+					ChildAssociationRef primaryParent = getNodeService().getPrimaryParent(childRef);
+					logger.debug("OLD primary Parent :" + primaryParent.getParentRef());
+					// to avoid constraints error (two same association between parent -> child) wee need to remove the current childAssociation before to move
+					FileInfo fileInfo = getServiceRegistry().getFileFolderService().getFileInfo(childRef);
+					boolean linkB = fileInfo.isLink();
+					logger.debug("new ref isLink ? " + linkB);
+
+					getNodeService().removeChildAssociation(childAssocRef);
+					FileInfo moveFrom = getServiceRegistry().getFileFolderService().moveFrom(childRef, primaryParent.getParentRef(), parentRef, null);
+					boolean linkA = moveFrom.isLink();
+					logger.debug("new ref isLink ? " + linkA);
+					// need to move the primary association and remove the current childAssociation
+					ChildAssociationRef newPrimaryParent = getNodeService().getPrimaryParent(childRef);
+					logger.debug("NEW primary Parent :" + newPrimaryParent.getParentRef());
+					//					getNodeService().moveNode(childRef, parentRef, ContentModel.ASSOC_CONTAINS, primaryParent.getQName());
+					if (primaryParent.getParentRef().toString().equals(newPrimaryParent.getParentRef())) {
+						// primary parent not changed
+						logger.error("Very Bad the node is in the temp folder, move do nothing ...");
+					}
+				} else {
+					logger.debug("\tnothing to do notPrimary asso");
+				}
+
+			} else {
+				logger.debug("\tnothing to do");
+				// do nothing this is the call that create the initial primary association
+			}
+		} catch (Throwable e) {
+			logger.error(e.getMessage(), e);
 		}
-
 	}
 
 	/*
@@ -118,6 +144,16 @@ public class SinglePrimaryChildAssociationPolicy implements OnCreateChildAssocia
 
 	public void setConfiguration(PropertiesConfiguration configuration) {
 		this.configuration = configuration;
+	}
+
+	ServiceRegistry serviceRegistry;
+
+	public ServiceRegistry getServiceRegistry() {
+		return serviceRegistry;
+	}
+
+	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
 	}
 
 }
