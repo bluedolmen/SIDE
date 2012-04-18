efff25c1d46b931944572d22f8bbfda06624f2eb
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2012-04-18 16:56:15 +0000

    SIDE.Upload js component copy file name into cm:name filed if exists in form
    git-svn-id: http://svn.bluexml.com/private/S-IDE@35165 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.share.formExtension346E/src/main/resources/side/components/form/upload.js
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.share.formExtension346E/src/main/resources/side/components/form/upload.js
@@ -4,12 +4,10 @@
  * @namespace SIDE
  */
 // Ensure Alfresco root object exists
-if (typeof SIDE == "undefined" || !SIDE)
-{
+if (typeof SIDE == "undefined" || !SIDE) {
    var SIDE = {};
 }
 
- 
 /**
  * Content control component with FileUpload.
  * 
@@ -19,8 +17,7 @@ if (typeof SIDE == "undefined" || !SIDE)
  * @class SIDE.Upload
  */
 
-(function()
-{
+(function() {
    /**
     * YUI Library aliases
     */
@@ -29,135 +26,155 @@ if (typeof SIDE == "undefined" || !SIDE)
    /**
     * Upload constructor.
     * 
-    * @param {String} htmlId The HTML id of the parent element
+    * @param {String}
+    *           htmlId The HTML id of the parent element
     * @return {Alfresco.TextControl} The new Upload instance
     * @constructor
     */
-   SIDE.Upload = function(htmlId)
-   {
+   SIDE.Upload = function(htmlId) {
       return SIDE.Upload.superclass.constructor.call(this, htmlId, "SIDE.Upload");
    };
-   
-   YAHOO.extend(SIDE.Upload, Alfresco.RichTextControl,
-   {
+
+   YAHOO.extend(SIDE.Upload, Alfresco.RichTextControl, {
       /**
        * Object container for initialization options
-       *
+       * 
        * @property options
        * @type object
        */
-      options:
-      {
+      options : {
          /**
           * Current Form Mode: edit or create
           * 
           * @property formMode
           * @type string
           */
-         formMode: "edit",
-         
+         formMode : "edit",
+
          /**
           * NodeRef of the item the form is for
           * 
           * @property nodeRef
           * @type string
           */
-         nodeRef: null,
-         
+         nodeRef : null,
+
          /**
           * The mimetype of the content being created
           * 
           * @property mimeType
           * @type string
           */
-         mimeType: null,
-         
+         mimeType : null,
+
          /**
-          * Comma separated list of mime types that will be shown
-          * in a textarea
+          * Comma separated list of mime types that will be shown in a textarea
           * 
           * @property plainMimeTypes
           * @type string
           */
-         plainMimeTypes: "text/plain,text/xml",
-         
+         plainMimeTypes : "text/plain,text/xml",
+
          /**
-          * Comma separated list of mime types that will be shown
-          * in the TinyMCE editor
+          * Comma separated list of mime types that will be shown in the TinyMCE
+          * editor
           * 
           * @property richMimeTypes
           * @type string
           */
-         richMimeTypes: "text/html,text/xhtml",
-         
+         richMimeTypes : "text/html,text/xhtml",
+
          /**
-          * Comma separated list of mime types that will be shown
-          * using the img tag and allow upload of new versions
+          * Comma separated list of mime types that will be shown using the img
+          * tag and allow upload of new versions
           * 
           * @property imageMimeTypes
           * @type string
           */
-         imageMimeTypes: "image/jpeg,image/jpg,image/png",
-         
+         imageMimeTypes : "image/jpeg,image/jpg,image/png",
+
          /**
-          * Whether the (plain text) editor should be forced visible, e.g. mimetype unrecognized
+          * Whether the (plain text) editor should be forced visible, e.g.
+          * mimetype unrecognized
           * 
           * @property forceEditor
           * @type boolean
           */
-         forceEditor: false,
+         forceEditor : false,
 
          /**
-          * Whether (empty) content should be created when editor is hidden, e.g. mimetype unrecognized.
-          * Note: Only relevant when forceEditor = false
+          * Whether (empty) content should be created when editor is hidden,
+          * e.g. mimetype unrecognized. Note: Only relevant when forceEditor =
+          * false
           * 
           * @property forceContent
           * @type boolean
           */
-         forceContent: false
+         forceContent : false
       },
 
       /**
-       * Fired by YUI when parent element is available for scripting.
-       * Component initialisation, including instantiation of YUI widgets and event listener binding.
-       *
+       * Fired by YUI when parent element is available for scripting. Component
+       * initialisation, including instantiation of YUI widgets and event
+       * listener binding.
+       * 
        * @method onReady
        */
-      onReady: function Upload_onReady()
-      {
-         if (Alfresco.logger.isDebugEnabled())
-         {
-            Alfresco.logger.debug("Rendering content control for element '" + this.id + "', value = '" + this.options.currentValue + 
-                  "', nodeRef = '" + this.options.nodeRef + "', mimetype = '" + this.options.mimeType + "'");
+      onReady : function Upload_onReady() {
+         if (Alfresco.logger.isDebugEnabled()) {
+            Alfresco.logger.debug("Rendering content control for element '" + this.id + "', value = '" + this.options.currentValue + "', nodeRef = '" + this.options.nodeRef + "', mimetype = '"
+                  + this.options.mimeType + "'");
             Alfresco.logger.debug("Configured plain mimetypes for element '" + this.id + "': " + this.options.plainMimeTypes);
             Alfresco.logger.debug("Configured rich mimetypes for element '" + this.id + "': " + this.options.richMimeTypes);
             Alfresco.logger.debug("Configured image mimetypes for element '" + this.id + "': " + this.options.imageMimeTypes);
-            Alfresco.logger.debug("Editor parameters for element '" + this.id + "': " + 
-                  YAHOO.lang.dump(this.options.editorParameters));
+            Alfresco.logger.debug("Editor parameters for element '" + this.id + "': " + YAHOO.lang.dump(this.options.editorParameters));
          }
-         
-      var subButton = document.getElementsByName("-")[0];
-      var previousAction = subButton.onclick;
-      
+
+         var subButton = document.getElementsByName("-")[0];
+         var previousAction = subButton.onclick;
+
+         this.widgets.inputFile = Dom.get(this.id);
+
+         // get the cm_field if any
+         var cm_name_field = this._getFileNameField();
+         var me = this;
+         YAHOO.util.Event.addListener(this.widgets.inputFile, "change", function(p_sType, p_aArgs) {
+            if (cm_name_field) {
+
+               var inputFileValue = me.widgets.inputFile.value;
+               var index = inputFileValue.lastIndexOf('/');
+               var index2 = inputFileValue.lastIndexOf('\\');
+               if (index != -1 || index2 != -1) {
+                  index = index > index2 ? index : index2;
+                  cm_name_field.value = inputFileValue.substring(index, inputFileValue.length - 1);
+               } else {
+                  cm_name_field.value = inputFileValue;
+               }
+            }
+         });
       },
-      
-      
+
       /**
        * Hides the field, used when a content property can not be shown.
        * 
        * @method _hideField
        * @private
        */
-      _hideField: function Upload__hideField()
-      {
-         if (!this.options.forceContent)
-         {
-            // change the name of the textarea so it is not submitted as new content!
+      _hideField : function Upload__hideField() {
+         if (!this.options.forceContent) {
+            // change the name of the textarea so it is not submitted as new
+            // content!
             Dom.get(this.id).name = "-";
          }
-         
+
          // hide the whole field
          Dom.get(this.id + "-field").style.display = "none";
+      },
+      _getFileNameField : function Upload__getFileNameField() {
+         var formEl = this.widgets.inputFile.form;
+         var fileNameElement = formEl.elements['prop_cm_name'];
+         return fileNameElement;
       }
+
    });
 })();
