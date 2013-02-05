9d338ad86334334fd206281980dcde9e4b53adfd
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-05 17:42:40 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43189 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.maven.ampArchetypeForSide_42c/src/main/resources/archetype-resources/src/main/java/DemoComponent.java
@@ -0,0 +1,28 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+package ${package};
+
+import java.util.logging.Logger;
+
+import org.alfresco.repo.module.AbstractModuleComponent;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A basic component that will be started for this module.
+ * 
+ */
+public class DemoComponent extends AbstractModuleComponent
+{
+	Log log = LogFactory.getLog(DemoComponent.class);
+	
+    @Override
+    protected void executeInternal() throws Throwable
+    {
+        System.out.println("DemoComponent has been executed");
+        log.debug("Test debug logging is working");
+        log.info("This should not be outputted by default");
+    }
+}
