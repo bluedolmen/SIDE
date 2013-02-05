a716185fa0a500d5687739cd261902cd43d13cfd
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-05 15:59:53 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43140 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.formProcessor42c/src/main/java/com/bluexml/side/framework/alfresco/formProcessor/MyDbNodeServiceImpl.java
@@ -0,0 +1,55 @@
+package com.bluexml.side.framework.alfresco.formProcessor;
+
+import org.alfresco.repo.node.db.DbNodeServiceImpl;
+import org.alfresco.service.cmr.repository.AssociationExistsException;
+import org.alfresco.service.cmr.repository.AssociationRef;
+import org.alfresco.service.cmr.repository.InvalidNodeRefException;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MyDbNodeServiceImpl extends DbNodeServiceImpl {
+
+	private static Log logger = LogFactory.getLog(MyDbNodeServiceImpl.class);
+	
+	
+	
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.alfresco.repo.node.AbstractNodeServiceImpl#init()
+	 */
+	@Override
+	public void init() {
+		logger.info("[init] MyDBNodeServiceImpl initialization");
+		super.init();
+	}
+
+
+
+
+
+
+
+	/* (non-Javadoc)
+	 * @see org.alfresco.repo.node.db.DbNodeServiceImpl#createAssociation(org.alfresco.service.cmr.repository.NodeRef, org.alfresco.service.cmr.repository.NodeRef, org.alfresco.service.namespace.QName)
+	 * 
+	 */
+	/**
+	 * override createAssociation to call invokeOnUpdateNode,
+	 * this allow to define NodeServicePolicies aware of global node modification
+	 * some actions need to have a view on properties AND associations at the same time 
+	 */
+	@Override
+	public AssociationRef createAssociation(NodeRef sourceRef, NodeRef targetRef, QName assocTypeQName) throws InvalidNodeRefException, AssociationExistsException {
+		AssociationRef createAssociation = super.createAssociation(sourceRef, targetRef, assocTypeQName);
+		this.invokeOnUpdateNode(sourceRef);
+		return createAssociation; 
+	}
+
+	
+
+}
