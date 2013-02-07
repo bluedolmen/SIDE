b2f7c1550bd9a29c37e0ea235109f1c009fb1b7a
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-07 11:24:03 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43336 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.maven.ampArchetypeForSide_4029E/src/main/resources/archetype-resources/src/main/java/Demo.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+package ${package};
+
+/**
+ * This class does nothing except dump some output to <i>system.out</i>.
+ * 
+ * @author Derek Hulley
+ */
+public class Demo
+{
+	public void init()
+	{
+		System.out.println("SDK Demo AMP class has been loaded");
+	}
+}
