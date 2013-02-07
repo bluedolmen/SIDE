76aec90e2da347e8e1d6e31c272721511152a0a1
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-07 11:13:24 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43288 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.commons4029E/src/main/java/com/bluexml/side/framework/alfresco/commons/configurations/MultiValuesConfigurationXMLFile.java
@@ -0,0 +1,88 @@
+package com.bluexml.side.framework.alfresco.commons.configurations;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.springframework.core.io.Resource;
+/**
+ * SIDE Extension
+ * 
+ * @author davidabad
+ */
+public abstract class MultiValuesConfigurationXMLFile<K, V> extends AbstractConfigurationFile<K, List<V>> {
+	protected String entryTag;
+	protected String entryKeyAttribute;
+	protected String valueTag;
+
+	public MultiValuesConfigurationXMLFile(String entryTag, String keyAttributeId, String valueTag) throws Exception {
+		super();
+		this.entryTag = entryTag;
+		this.entryKeyAttribute = keyAttributeId;
+		this.valueTag = valueTag;
+
+	}
+
+	public enum ContentType {
+		ATTRIBUTE, TEXTNODE
+	}
+
+	protected abstract V getValueObject(Element value);
+
+	protected abstract K getKeyObject(String keyString);
+
+	@Override
+	protected void loadResource(Resource r) throws Exception {
+		loadXMLResource(r);
+	}
+
+	protected Document getDocument(Resource r) throws Exception {
+		InputStream in = r.getInputStream();
+		return getDocument(in);
+	}
+
+	protected Document getDocument(InputStream in) throws JDOMException, IOException {
+		SAXBuilder builder = new SAXBuilder();
+		Document nodeRefXbel = builder.build(in);
+		return nodeRefXbel;
+	}
+
+	protected void loadXMLResource(Resource r) throws Exception {
+		Document document = getDocument(r);
+		loadXML(document);
+	}
+
+	protected void loadXML(Document document) {
+		Element root = document.getRootElement();
+		for (Object keyElement : root.getChildren(entryTag)) {
+			String key = ((Element) keyElement).getAttributeValue(entryKeyAttribute);
+			dictionary.put(getKeyObject(key), new ArrayList<V>());
+			readValues((Element) keyElement, key);
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("Loaded dictionary entries :");
+			for (Map.Entry<K, List<V>> entry : dictionary.entrySet()) {
+				List<V> value = entry.getValue();
+				logger.debug(entry.getKey() + " :");
+				for (V v : value) {
+					logger.debug("value :" + v);
+				}
+			}
+		}
+	}
+
+	protected void readValues(Element type, String key) {
+		for (Object attribute : type.getChildren(valueTag)) {
+			Element attribute2 = (Element) attribute;
+
+			dictionary.get(getKeyObject(key)).add(getValueObject(attribute2));
+		}
+	}
+
+}
---
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.commons4029E/src/main/java/com/bluexml/side/framework/alfresco/commons/services/SIDERulesService.java
@@ -0,0 +1,220 @@
+package com.bluexml.side.framework.alfresco.commons.services;
+
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.action.evaluator.IsSubTypeEvaluator;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.action.Action;
+import org.alfresco.service.cmr.action.ActionCondition;
+import org.alfresco.service.cmr.action.CompositeAction;
+import org.alfresco.service.cmr.repository.ContentWriter;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.rule.Rule;
+import org.alfresco.service.cmr.rule.RuleType;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.QName;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ * SIDE Extension
+ * 
+ * @author davidabad
+ */
+public class SIDERulesService {
+	static Log logger = LogFactory.getLog(SIDERulesService.class);
+	private ServiceRegistry serviceRegistry;
+
+	public static final String RULETYPE_INBOUND = "inbound";
+	public static final String RULETYPE_OUTBOUND = "outbound";
+	public static final String RULETYPE_UPDATE = "update";
+
+	private String scriptsPath;
+
+	public ServiceRegistry getServiceRegistry() {
+		return serviceRegistry;
+	}
+
+	public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+		this.serviceRegistry = serviceRegistry;
+	}
+
+	/**
+	 * Method to add a -based rule on a space to apply on children space
+	 * 
+	 * @param the
+	 *            java file containing the code of the rule to
+	 *            apply on the targeted space
+	 * @param targetSpace
+	 *            the targeted space
+	 * @param ruleType
+	 *            the rule type: either "inbound", "outbound" or "update"
+	 */
+	public void addRule(NodeRef node, NodeRef targetSpace, String ruleType) {
+		addRuleWithTypeCondition(node, targetSpace, ruleType, null);
+	}
+
+	/**
+	 * Method to add a -based rule on a space to apply on children space
+	 * 
+	 * @param the
+	 *            java file containing the code of the rule to
+	 *            apply on the targeted space
+	 * @param targetSpace
+	 *            the targeted space
+	 * @param ruleType
+	 *            the rule type: either "inbound", "outbound" or "update"
+	 * @param contentType
+	 * @param uri
+	 */
+	public void addRuleWithTypeCondition(NodeRef node, NodeRef targetSpace, String ruleType, QName nodeType) {
+		String title = "rule__execution";
+		String description = "Run a  when an event occurs on the targetSpace";
+		String Name = (String) serviceRegistry.getNodeService().getProperty(node, ContentModel.PROP_NAME);
+		String targetSpaceName = (String) serviceRegistry.getNodeService().getProperty(targetSpace, ContentModel.PROP_NAME);
+		title = Name + "_" + ruleType + "_" + targetSpaceName;
+		if (serviceRegistry.getNodeService().getProperty(node, ContentModel.PROP_TITLE) != null) {
+			if (nodeType != null) description = serviceRegistry.getNodeService().getProperty(node, ContentModel.PROP_TITLE) + " for type " + nodeType + " on space " + targetSpaceName;
+			else description = (String) serviceRegistry.getNodeService().getProperty(node, ContentModel.PROP_TITLE) + " for all types on space " + targetSpaceName;
+		}
+		boolean setExecuteAsynchronously = true;
+		boolean applyToChildren = false;
+		boolean setRuleDisabled = false;
+		addRuleWithTypeCondition(node, targetSpace, ruleType, nodeType, applyToChildren, setExecuteAsynchronously, setRuleDisabled, title, description);
+	}
+
+	/**
+	 * Method to add a -based rule with full setting
+	 * 
+	 * @param
+	 * @param targetSpace
+	 * @param ruleType
+	 * @param contentType
+	 * @param uri
+	 * @param applyToChildren
+	 * @param setExecuteAsynchronously
+	 * @param setRuleDisabled
+	 * @param title
+	 * @param description
+	 */
+	public void addRuleWithTypeCondition(NodeRef node, NodeRef targetSpace, String ruleType, QName nodeType, boolean applyToChildren, boolean setExecuteAsynchronously, boolean setRuleDisabled, String title, String description) {
+		if (logger.isDebugEnabled()) logger.debug("Add the  rule " + node + " on the space " + targetSpace);
+		Rule rule = new Rule();
+
+		rule.setTitle(title);
+		rule.setDescription(description);
+		rule.applyToChildren(applyToChildren);
+		rule.setExecuteAsynchronously(setExecuteAsynchronously);
+		rule.setRuleDisabled(setRuleDisabled);
+		if (ruleType.equals("update")) {
+			rule.setRuleType(RuleType.UPDATE);
+		} else if (ruleType.equals("outbound")) {
+			rule.setRuleType(RuleType.OUTBOUND);
+		} else {
+			rule.setRuleType(RuleType.INBOUND);
+		}
+
+		CompositeAction compositeAction = serviceRegistry.getActionService().createCompositeAction();
+		rule.setAction(compositeAction);
+
+		ActionCondition condition = serviceRegistry.getActionService().createActionCondition(IsSubTypeEvaluator.NAME);
+		Map<String, Serializable> conditionParameters = new HashMap<String, Serializable>(1);
+		if (nodeType != null) {
+			conditionParameters.put(IsSubTypeEvaluator.PARAM_TYPE, nodeType);
+		} else {
+			conditionParameters.put(IsSubTypeEvaluator.PARAM_TYPE, ContentModel.TYPE_FOLDER);
+		}
+		condition.setParameterValues(conditionParameters);
+		compositeAction.addActionCondition(condition);
+
+		Action action = serviceRegistry.getActionService().createAction("script");
+		action.setParameterValue("script-ref", node);
+		compositeAction.addAction(action);
+
+		// save the rule on the node
+		List<Rule> myRules = serviceRegistry.getRuleService().getRules(targetSpace, false);
+		for (Rule ruleTest : myRules) {
+			if (ruleTest.getTitle().equals(title)) {
+				serviceRegistry.getRuleService().removeRule(targetSpace, ruleTest);
+			}
+		}
+		serviceRegistry.getRuleService().saveRule(targetSpace, rule);
+	}
+
+	/**
+	 * @param
+	 * @param title
+	 *            (optional return all if null)
+	 * @param ruleTypeName
+	 *            (optional return all if null)
+	 * @param includeInhertiedRuleType
+	 */
+	public void disableRules(NodeRef node, String title, String ruleTypeName, boolean includeInhertiedRuleType) {
+		List<Rule> rules = serviceRegistry.getRuleService().getRules(node, includeInhertiedRuleType, ruleTypeName);
+		for (Rule rule : rules) {
+			if (title == null || rule.getTitle().equals(title)) {
+				serviceRegistry.getRuleService().disableRule(rule);
+			}
+
+		}
+	}
+
+	/**
+	 * @param
+	 * @param title
+	 *            (optional return all if null)
+	 * @param ruleTypeName
+	 *            (optional return all if null)
+	 * @param includeInhertiedRuleType
+	 */
+	public void enableRules(NodeRef node, String title, String ruleTypeName, boolean includeInhertiedRuleType) {
+		List<Rule> rules = serviceRegistry.getRuleService().getRules(node, includeInhertiedRuleType, ruleTypeName);
+		for (Rule rule : rules) {
+			if (title == null || rule.getTitle().equals(title)) {
+				serviceRegistry.getRuleService().enableRule(rule);
+			}
+		}
+	}
+
+	/**
+	 * Disable all rule in the current thread
+	 */
+	public void disableAllRules() {
+		serviceRegistry.getRuleService().disableRules();
+	}
+
+	/**
+	 * Enable all rule in the current thread
+	 */
+	public void enableAllRules() {
+		serviceRegistry.getRuleService().enableRules();
+	}
+
+	public NodeRef saveRuleScript(InputStream in, String name) {
+		String query = "PATH:\"" + scriptsPath + "\"";
+		NodeRef scripts = serviceRegistry.getSearchService().query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, SearchService.LANGUAGE_LUCENE, query).getNodeRef(0);
+		NodeRef script = serviceRegistry.getNodeService().getChildByName(scripts, ContentModel.ASSOC_CONTAINS, name);
+		if (script == null) {
+			script = serviceRegistry.getFileFolderService().create(scripts, name, ContentModel.TYPE_CONTENT).getNodeRef();
+		}
+
+		ContentWriter writerHTML = serviceRegistry.getContentService().getWriter(script, ContentModel.PROP_CONTENT, true);
+		String mimetype = serviceRegistry.getMimetypeService().getMimetype("js");
+		writerHTML.setMimetype(mimetype);
+		writerHTML.putContent(in);
+		return script;
+	}
+
+	public String getScriptsPath() {
+		return scriptsPath;
+	}
+
+	public void setScriptsPath(String scriptsPath) {
+		this.scriptsPath = scriptsPath;
+	}
+}
