ebe93dbf8ac9edde15fdbcd30f010565b9f6f693
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-11 16:45:37 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43543 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/View/trunk/com.bluexml.side.View.generator.alfresco4029E/src/com/bluexml/side/view/generator/alfresco4029E/ViewGenerator4029E.java
@@ -0,0 +1,9 @@
+package com.bluexml.side.view.generator.alfresco4029E;
+
+import com.bluexml.side.view.generator.alfresco40d.ViewGenerator40d;
+
+public class ViewGenerator4029E extends ViewGenerator40d {
+	public final static String TEMPLATE_HOME_4029E = "/com.bluexml.side.View.generator.alfresco4029E/com/bluexml/side/view/generator/alfresco4029E/templates";
+
+	
+}
