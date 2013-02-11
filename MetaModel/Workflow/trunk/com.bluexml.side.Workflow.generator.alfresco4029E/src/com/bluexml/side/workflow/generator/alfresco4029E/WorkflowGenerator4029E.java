328d3c8defce2ec15857d276fac95e40112ce7c6
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-11 16:46:02 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43545 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Workflow/trunk/com.bluexml.side.Workflow.generator.alfresco4029E/src/com/bluexml/side/workflow/generator/alfresco4029E/WorkflowGenerator4029E.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.workflow.generator.alfresco4029E;
+
+import com.bluexml.side.workflow.generator.alfresco34d.WorkflowGeneratorCommunity;
+
+public class WorkflowGenerator4029E extends WorkflowGeneratorCommunity {
+
+}
