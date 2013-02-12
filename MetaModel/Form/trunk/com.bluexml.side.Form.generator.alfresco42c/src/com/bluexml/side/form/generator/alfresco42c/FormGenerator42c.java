cc8ea6569285a1ddc27e84c4eba7c2f7290f37af
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-12 15:07:06 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43694 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Form/trunk/com.bluexml.side.Form.generator.alfresco42c/src/com/bluexml/side/form/generator/alfresco42c/FormGenerator42c.java
@@ -0,0 +1,11 @@
+package com.bluexml.side.form.generator.alfresco42c;
+
+import com.bluexml.side.form.generator.alfresco34d.FormGenerator;
+
+public class FormGenerator42c extends FormGenerator {
+
+	
+	public FormGenerator42c() {
+		versionProperty = "com.bluexml.side.Form.generator.alfresco42c.module.version";
+	}
+}
