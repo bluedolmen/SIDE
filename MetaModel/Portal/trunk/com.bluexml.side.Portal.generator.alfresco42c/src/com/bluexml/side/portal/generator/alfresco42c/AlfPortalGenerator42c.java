bbc6e83943e122b8bdd08b948f9800b03ecfb5b1
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-12 15:07:14 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43696 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Portal/trunk/com.bluexml.side.Portal.generator.alfresco42c/src/com/bluexml/side/portal/generator/alfresco42c/AlfPortalGenerator42c.java
@@ -0,0 +1,7 @@
+package com.bluexml.side.portal.generator.alfresco42c;
+
+import com.bluexml.side.portal.generator.alfresco40d.AlfPortalGenerator40d;
+
+public class AlfPortalGenerator42c extends AlfPortalGenerator40d {
+
+}
