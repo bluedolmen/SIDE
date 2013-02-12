11974e905c0337e65e0b90a50ea47dbe150633de
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-02-12 15:06:27 +0000

    Initial import.
    git-svn-id: http://svn.bluexml.com/private/S-IDE@43684 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

---
+++ MetaModel/Class/trunk/com.bluexml.side.Class.generator.alfresco42c.ext.labs/src/com/bluexml/side/clazz/generator/alfresco42c/ext/labs/AlfClassGenerator42c.java
@@ -0,0 +1,9 @@
+package com.bluexml.side.clazz.generator.alfresco42c.ext.labs;
+
+import com.bluexml.side.clazz.generator.alfresco40d.ext.labs.AlfClassGenerator40d;
+
+public class AlfClassGenerator42c extends AlfClassGenerator40d {
+	protected final static String templateBase42c = com.bluexml.side.clazz.generator.alfresco42c.Activator.templateBase42c;
+	
+	
+}
