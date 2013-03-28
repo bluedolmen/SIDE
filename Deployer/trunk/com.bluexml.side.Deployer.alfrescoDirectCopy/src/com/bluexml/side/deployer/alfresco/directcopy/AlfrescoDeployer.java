fd11a6907cd3a1d85f9186879390344f5b157ba7
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-03-28 15:34:37 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@45142 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Deployer/trunk/com.bluexml.side.Deployer.alfrescoDirectCopy/src/com/bluexml/side/deployer/alfresco/directcopy/AlfrescoDeployer.java
+++ Deployer/trunk/com.bluexml.side.Deployer.alfrescoDirectCopy/src/com/bluexml/side/deployer/alfresco/directcopy/AlfrescoDeployer.java
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2007-20013  BlueXML - www.bluexml.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.bluexml.side.deployer.alfresco.directcopy;
 
 import java.io.File;
@@ -35,7 +51,6 @@ public class AlfrescoDeployer extends AbstractMultiDeployer {
 	}
 
 	public boolean check() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
