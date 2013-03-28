27b2ad25dd0240299572866211af14b39c926b3c
dabad <dabad@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-03-28 13:30:40 +0000

    git-svn-id: http://svn.bluexml.com/private/S-IDE@45133 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- Integration/trunk/com.bluexml.side.Integration.buildHudson/config/labs/add_licence.sh
+++ Integration/trunk/com.bluexml.side.Integration.buildHudson/config/labs/add_licence.sh
@@ -8,7 +8,7 @@ var=`cat $licence`
 
 
 # avoid webscript js files
-for f in `find $chemin -type f -and \( -name "*.java" -or -name "*.js" \) -and ! \( -path "*/resources/alfresco/*" -or -path "*web-extension*" \) -and -path "*com.bluexml*" `; do
+for f in `find $chemin -type f -name "*.java" `; do
 	filein=$f
 	num=1
 	perl -pi -le 'print "/*" if $. == "'$num'"' $filein
@@ -21,41 +21,41 @@ for f in `find $chemin -type f -and \( -name "*.java" -or -name "*.js" \) -and !
 	perl -pi -le 'print "*/" if $. == "'$num'"' $filein
 done
 
-for f in `find $chemin -type f -name "*.xml" `; do
-	filein=$f
-	num=2
-	perl -pi -le 'print "<!--" if $. == "'$num'"' $filein
-	num=$(($num+1))
-	for i in $var; do
-        	perl -pi -le 'print "'$i'" if $. == "'$num'"' $filein
-		num=$(($num+1))
-	done
+#for f in `find $chemin -type f -name "*.xml" `; do
+#	filein=$f
+#	num=2
+#	perl -pi -le 'print "<!--" if $. == "'$num'"' $filein
+#	num=$(($num+1))
+#	for i in $var; do
+#        	perl -pi -le 'print "'$i'" if $. == "'$num'"' $filein
+#		num=$(($num+1))
+#	done
+#
+#	perl -pi -le 'print "-->" if $. == "'$num'"' $filein
+#done
 
-	perl -pi -le 'print "-->" if $. == "'$num'"' $filein
-done
+#for f in `find $chemin -type f -name "*.ecore" -o -name "*.ecore_diagram" -o -name "*.ecorediag" -o -name "*.genmodel" -o -name "*.chain" -o -name "*.exsd" `; do
+#	filein=$f
+#	num=1
+#	perl -pi -le 'print "<!--" if $. == "'$num'"' $filein
+#	num=$(($num+1))
+#	for i in $var; do
+#       	perl -pi -le 'print "'$i'" if $. == "'$num'"' $filein
+#		num=$(($num+1))
+#	done
+#
+#	perl -pi -le 'print "-->" if $. == "'$num'"' $filein
+#done
 
-for f in `find $chemin -type f -name "*.ecore" -o -name "*.ecore_diagram" -o -name "*.ecorediag" -o -name "*.genmodel" -o -name "*.chain" -o -name "*.exsd" `; do
-	filein=$f
-	num=1
-	perl -pi -le 'print "<!--" if $. == "'$num'"' $filein
-	num=$(($num+1))
-	for i in $var; do
-        	perl -pi -le 'print "'$i'" if $. == "'$num'"' $filein
-		num=$(($num+1))
-	done
-
-	perl -pi -le 'print "-->" if $. == "'$num'"' $filein
-done
-
-for f in `find $chemin -type f -name "*.properties"`; do
-	filein=$f
-	num=1
-	for i in $var; do
-        	perl -pi -le 'print "#  '$i'" if $. == "'$num'"' $filein
-		num=$(($num+1))
-	done
-
-done
+#for f in `find $chemin -type f -name "*.properties"`; do
+#	filein=$f
+#	num=1
+#	for i in $var; do
+#        	perl -pi -le 'print "#  '$i'" if $. == "'$num'"' $filein
+#		num=$(($num+1))
+#	done
+#
+#done
 
 for f in `find $chemin -type f -name "*.mt" `; do
 	filein=$f
