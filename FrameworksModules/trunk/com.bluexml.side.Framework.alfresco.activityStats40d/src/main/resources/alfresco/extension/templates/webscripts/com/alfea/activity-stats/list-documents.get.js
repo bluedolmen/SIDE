a8c4d7c3251db3d7a57b1d4e224180efd45d023f
rbaudu <rbaudu@c4e9bb49-730a-0410-b52d-bc9a0859fdfe>
2013-04-04 11:00:43 +0000

    exclude thumbnail, topic, ...
    git-svn-id: http://svn.bluexml.com/private/S-IDE@46404 c4e9bb49-730a-0410-b52d-bc9a0859fdfe

--- FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.activityStats40d/src/main/resources/alfresco/extension/templates/webscripts/com/alfea/activity-stats/list-documents.get.js
+++ FrameworksModules/trunk/com.bluexml.side.Framework.alfresco.activityStats40d/src/main/resources/alfresco/extension/templates/webscripts/com/alfea/activity-stats/list-documents.get.js
@@ -3,5 +3,5 @@ var site = url.templateArgs.siteName;
 var type = args.type;
 if (type == null) type = "cm:content";
 
-var nodes = search.luceneSearch("PATH:\"/app:company_home/st:sites/cm:"+site+"/cm:documentlibrary//*\" +TYPE:\""+type+"\"");
+var nodes = search.luceneSearch("+PATH:\"/app:company_home/st:sites/cm:"+site+"/cm:documentLibrary//*\" +TYPE:\""+type+"\" -TYPE:\"cm:thumbnail\" -TYPE:\"cm:failedThumbnail\" -TYPE:\"cm:systemfolder\" -TYPE:\"fm:forums\" -TYPE:\"fm:forum\" -TYPE:\"fm:topic\" -TYPE:\"fm:post\"");
 model.number = nodes.length;
